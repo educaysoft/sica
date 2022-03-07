dab7ec225db74c170c504ffcc7ef1b4765b44e6b
Stalin Francis <educaysoft@gmail.com>
2022-03-07 17:05:16 -0500

    Desde mi latop ver tesis

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,4 +53,4 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['index.php/login']='login';
-$route['tesis/(:any)']='documento/listarxtipodocu/$1';
+$route['tesiscti']='documento/listarxtipodocu/11';
