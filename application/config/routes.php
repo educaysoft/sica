f902f44295f5dbeadc42aa13b12bc56f60d0690b
Stalin Francis <educaysoft@gmail.com>
2022-03-07 16:31:45 -0500

    Desde mi laptop

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,4 +53,4 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['index.php/login']='login';
-$route['tesis']='login/index';
+$route['tesiscti']='documento/listarxtipodocu/11';
