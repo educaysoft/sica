d21667d9b00300b7951fac017370720b38899739
Stalin Francis <sfrancis@192.168.1.13>
2022-04-18 02:11:25 -0400

    otravea para los certificados

--- application/views/cursos/FundamentosDeProgramacion_clases.php
+++ application/views/cursos/FundamentosDeProgramacion_clases.php
@@ -252,7 +252,7 @@ $idrespueta=array(array(7,8,9),array(10,11,12));
 
 </div>
 	<div id="certificado" style="padding:10px; width:80%; margin:auto; border:1px solid blue; text-align: center;">
-			<?php echo '<button id="evaluar" onclick="get_certificado('.$idevaluacion.','.site_url('evento/listar_participantes').','.$evento["idevento"].');" >En hora buena! Ya puedes imprimir tu certificado</button>'; ?> 
+			<?php echo '<button id="evaluar" onclick="get_certificado('.$idevaluacion.','.'evento/listar_participantes'.','.$evento["idevento"].');" >En hora buena! Ya puedes imprimir tu certificado</button>'; ?> 
 	</div>
 </div>
 
@@ -263,12 +263,12 @@ $idrespueta=array(array(7,8,9),array(10,11,12));
 
 
 
-function get_certificado(idevaluacion,url, idevento)
+function get_certificado(idevaluacion,miurl, idevento)
 {
 alert("hola");
 alert(idevaluacion);
 alert(idevento);
-alert(url);
+alert(miurl);
 
 
 window.location.href = url+'/'+idevento;
