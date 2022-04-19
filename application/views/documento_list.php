15301500de4f6f6a536d8714578e436a99669ab6
Stalin Francis <sfrancis@192.168.1.13>
2022-04-18 23:46:53 -0400

    hola otra modificaion zzzxxxx

--- application/views/documento_list.php
+++ application/views/documento_list.php
@@ -122,13 +122,23 @@ body {font-family: Arial, Helvetica, sans-serif;}
         };
 
 
+var idtipodocu=0;
+function filtra_documento()
+{
+
+idtipodocu = $('select[name=idtipodocu]').val();
+alert(idtipodocu);
+var mytabla= $('#mydatac').DataTable({destroy: true,"ajax": {url: '<?php echo site_url('documento/documento_dataxtipodocu')?>', type: 'GET',data:{idtipodocu:idtipodocu}},});
+};
+
 
 $(document).ready(function(){
 
 	var iddocumento=<?php echo  $filtro ?>;
 	alert(iddocumento);
-
-	var mytabla= $('#mydatac').DataTable({"ajax": {url: '<?php echo site_url('documento/documento_data')?>', type: 'GET',data:{iddocumento:iddocumento}},});
+          idtipodocu=iddocumento;
+var mytabla= $('#mydatac').DataTable({destroy: true,"ajax": {url: '<?php echo site_url('documento/documento_dataxtipodocu')?>', type: 'GET',data:{idtipodocu:idtipodocu}},});
+//	var mytabla= $('#mydatac').DataTable({"ajax": {url: '<?php echo site_url('documento/documento_data')?>', type: 'GET',data:{iddocumento:iddocumento}},});
 
 $('#show_data').on('click','.item_ver',function(){
  
