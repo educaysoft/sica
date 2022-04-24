007d3b59c2d9bb4d84d80d06737f75b4bfefb425
Stalin Francis <sfrancis@192.168.1.9>
2022-04-23 22:45:15 -0400

    Asistencia

--- application/views/asistencia_form.php
+++ application/views/asistencia_form.php
@@ -112,208 +112,193 @@ echo form_textarea("comentario","",$textarea_options);
 
 
 
-function get_participantes() {
-	var idevento = $('select[name=idevento]').val();
-    $.ajax({
-        url: "<?php echo site_url('asistencia/get_participantes') ?>",
-        data: {idevento:idevento},
-        method: 'POST',
-	async : true,
-        dataType : 'json',
-        success: function(data){
-        var html = '';
-        var i;
-        for(i=0; i<data.length; i++){
-        html += '<option value='+data[i].idpersona+'>'+data[i].nombres+'</option>';
-        }
-        $('#idpersona').html(html);
-
-	var select = document.getElementById('idpersona');
-	select.size = select.length;  
-
-        },
-      error: function (xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
-      }
-
-    })
-
-}
-
-
-
-function get_participantes2() {
-	var idevento = $('select[name=idevento]').val();
-	var f = document.getElementById("idfechaevento");
-  	var arrtmp=f.options[f.selectedIndex].text;
-	const x=arrtmp.split(" - ");
-	var fecha=x[0];
-//alert(idevento);
-//alert(fecha);
-    if(fecha=="--Select--"){
-      alert("debe seleccionar una fecha");
-   }else{
-    $.ajax({
-        url: "<?php echo site_url('asistencia/get_participantes2') ?>",
-        data: {idevento:idevento,fecha:fecha},
-        method: 'POST',
-	async : false,
-        dataType : 'json',
-        success: function(data){
-        var html = '';
-        var i;
-        var l=data.length+1;
-        document.getElementById('idpersona').setAttribute('size',"'"+l+"'");
-        for(i=0; i<data.length; i++){
-		if(data[i].tipoasistencia!=" "){
-			html += '<option style="color:green;"  value='+data[i].idpersona+'>'+data[i].idpersona+'- '+data[i].nombres+' - '+data[i].tipoasistencia+'</option>';
-		}else{
-			html += '<option style="color:red;" value='+data[i].idpersona+'>'+data[i].idpersona+'- '+data[i].nombres+' - '+data[i].tipoasistencia+'</option>';
+	function get_participantes() {
+		var idevento = $('select[name=idevento]').val();
+	    $.ajax({
+		url: "<?php echo site_url('asistencia/get_participantes') ?>",
+		data: {idevento:idevento},
+		method: 'POST',
+		async : true,
+		dataType : 'json',
+		success: function(data){
+		var html = '';
+		var i;
+		for(i=0; i<data.length; i++){
+		html += '<option value='+data[i].idpersona+'>'+data[i].nombres+'</option>';
 		}
-        }
-        $('#idpersona').html(html);
+		$('#idpersona').html(html);
 
+		var select = document.getElementById('idpersona');
+		select.size = select.length;  
 
-        var select = document.getElementById('idpersona');
-        select.size = select.length;  
-        },
-      error: function (xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
-      }
-
-    })
-}
-}
-
-
-
-
-
-
-
-
-function get_asistencia() {
-	var fecha = document.getElementById("fecha").value;
-	var idevento=document.getElementById("idevento").value;
-//	var idpersona= $('select[name=idpersona]').val();
-	var idpersona=document.getElementById("idpersona").value;
-	alert(idpersona);
-	
-//	element.replaceChild(newNode, element.childNodes[xx]);
-
-
-
-    $.ajax({
-        url: "<?php echo site_url('asistencia/get_asistenciap') ?>",
-        data: {idevento:idevento,fecha:fecha,idpersona:idpersona},
-        method: 'POST',
-	async : true,
-        dataType : 'json',
-        success: function(data){
-        var html = '';
-	var comentario="";
-        var i;
-	if(data.length>1){
-	var xx=document.getElementById("idpersona").selectedIndex;
-	
-	
-	 element = document.getElementById("idpersona")[xx];
-       	 element.style.color="red";
-
-	}else{
-	var xx=document.getElementById("idpersona").selectedIndex;
-	
-	
-	 element = document.getElementById("idpersona")[xx];
-       	 element.style.color="green";
-
+		},
+	      error: function (xhr, ajaxOptions, thrownError) {
+		alert(xhr.status);
+		alert(thrownError);
+	      }
+	    })
 	}
 
-        for(i=0; i<data.length; i++){
-        html += '<option value='+data[i].idtipoasistencia+'>'+data[i].tipoasistencia+'</option>';
-	document.getElementById("comentario").value=data[i].comentario;
-        }
-        $('#idtipoasistencia').html(html);
 
 
-        },
-      error: function (xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
-      }
+	function get_participantes2() {
+		var idevento = $('select[name=idevento]').val();
+		var f = document.getElementById("idfechaevento");
+		var arrtmp=f.options[f.selectedIndex].text;
+		const x=arrtmp.split(" - ");
+		var fecha=x[0];
+	    if(fecha=="--Select--"){
+	      alert("debe seleccionar una fecha");
+	   }else{
+	    $.ajax({
+		url: "<?php echo site_url('asistencia/get_participantes2') ?>",
+		data: {idevento:idevento,fecha:fecha},
+		method: 'POST',
+		async : false,
+		dataType : 'json',
+		success: function(data){
+		var html = '';
+		var i;
+		var l=data.length+1;
+		document.getElementById('idpersona').setAttribute('size',"'"+l+"'");
+		for(i=0; i<data.length; i++){
+			if(data[i].tipoasistencia!="undefined"){
+				html += '<option style="color:green;"  value='+data[i].idpersona+'>'+data[i].idpersona+'- '+data[i].nombres+' - '+data[i].tipoasistencia+'</option>';
+			}else{
+				html += '<option style="color:red;" value='+data[i].idpersona+'>'+data[i].idpersona+'- '+data[i].nombres+' - '+data[i].tipoasistencia+'</option>';
+			}
+		}
+		$('#idpersona').html(html);
 
-    })
+		var select = document.getElementById('idpersona');
+		select.size = select.length;  
+		},
+	      error: function (xhr, ajaxOptions, thrownError) {
+		alert(xhr.status);
+		alert(thrownError);
+	      }
 
-}
-
-
-
-
-function get_asistencia2() {
-	var fecha = document.getElementById("fecha").value;
-	var idevento=document.getElementById("idevento").value;
-	var idpersona= $('select[name=idpersona]').val();
-
-
-    $.ajax({
-        url: "<?php echo site_url('asistencia/get_asistencia') ?>",
-        data: {idevento : idevento, fecha : fecha, idpersona : idpersona},
-        method: 'POST',
-	async : true,
-        dataType : 'json',
-        success: function(data){
-        var html = '';
-        var i;
-        },
-      error: function (xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
-      }
-
-    })
-
-}
-
+	    })
+	}
+	}
 
 
 
 
 
-function save_asistencia() {
-	var f = document.getElementById("idfechaevento");
-  	var arrtmp=f.options[f.selectedIndex].text;
-	const x=arrtmp.split(" - ");
-	var fecha=x[0];
-	alert(fecha);
-	var idevento=document.getElementById("idevento").value;
-	var tipoasistencia=document.getElementById("idtipoasistencia").value;
-	var comentario=document.getElementById("comentario").value;
-	var idpersona= $('select[name=idpersona]').val();
-	var p = document.getElementById("idpersona");
-  	var idpersona=p.options[p.selectedIndex].value;
 
-    $.ajax({
-        url: "<?php echo site_url('asistencia/save_asistencia') ?>",
-        data: {idevento:idevento, fecha:fecha,idtipoasistencia:idtipoasistencia,comentario:comentario,idpersona:idpersona},
-        method: 'POST',
-        async : false,
-        dataType : 'json',
-        success: function(data){
-        var html = '';
-        var i;
-        get_asistencia2();
-        alert("Se guardo con exito");
-        },
-      error: function (xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
-      }
 
-    })
 
-}
+	function get_asistencia() {
+		var fecha = document.getElementById("fecha").value;
+		var idevento=document.getElementById("idevento").value;
+	//	var idpersona= $('select[name=idpersona]').val();
+		var idpersona=document.getElementById("idpersona").value;
+		alert(idpersona);
+	//	element.replaceChild(newNode, element.childNodes[xx]);
+
+	    $.ajax({
+		url: "<?php echo site_url('asistencia/get_asistenciap') ?>",
+		data: {idevento:idevento,fecha:fecha,idpersona:idpersona},
+		method: 'POST',
+		async : true,
+		dataType : 'json',
+		success: function(data){
+		var html = '';
+		var comentario="";
+		var i;
+		if(data.length>1){
+		var xx=document.getElementById("idpersona").selectedIndex;
+		
+		
+		 element = document.getElementById("idpersona")[xx];
+		 element.style.color="red";
+
+		}else{
+		var xx=document.getElementById("idpersona").selectedIndex;
+		
+		
+		 element = document.getElementById("idpersona")[xx];
+		 element.style.color="green";
+
+		}
+
+		for(i=0; i<data.length; i++){
+		html += '<option value='+data[i].idtipoasistencia+'>'+data[i].tipoasistencia+'</option>';
+		document.getElementById("comentario").value=data[i].comentario;
+		}
+		$('#idtipoasistencia').html(html);
+
+
+		},
+	      error: function (xhr, ajaxOptions, thrownError) {
+		alert(xhr.status);
+		alert(thrownError);
+	      }
+	    })
+	}
+
+
+
+
+	function get_asistencia2() {
+		var fecha = document.getElementById("fecha").value;
+		var idevento=document.getElementById("idevento").value;
+		var idpersona= $('select[name=idpersona]').val();
+
+	    $.ajax({
+		url: "<?php echo site_url('asistencia/get_asistencia') ?>",
+		data: {idevento : idevento, fecha : fecha, idpersona : idpersona},
+		method: 'POST',
+		async : true,
+		dataType : 'json',
+		success: function(data){
+		var html = '';
+		var i;
+		},
+	      error: function (xhr, ajaxOptions, thrownError) {
+		alert(xhr.status);
+		alert(thrownError);
+	      }
+	    })
+	}
+
+
+
+
+
+
+	function save_asistencia() {
+		var f = document.getElementById("idfechaevento");
+		var arrtmp=f.options[f.selectedIndex].text;
+		const x=arrtmp.split(" - ");
+		var fecha=x[0];
+		alert(fecha);
+		var idevento=document.getElementById("idevento").value;
+		var tipoasistencia=document.getElementById("idtipoasistencia").value;
+		var comentario=document.getElementById("comentario").value;
+		var idpersona= $('select[name=idpersona]').val();
+		var p = document.getElementById("idpersona");
+		var idpersona=p.options[p.selectedIndex].value;
+
+	    $.ajax({
+		url: "<?php echo site_url('asistencia/save_asistencia') ?>",
+		data: {idevento:idevento, fecha:fecha,idtipoasistencia:idtipoasistencia,comentario:comentario,idpersona:idpersona},
+		method: 'POST',
+		async : false,
+		dataType : 'json',
+		success: function(data){
+		var html = '';
+		var i;
+		get_asistencia2();
+		alert("Se guardo con exito");
+		},
+	      error: function (xhr, ajaxOptions, thrownError) {
+		alert(xhr.status);
+		alert(thrownError);
+	      }
+	    })
+	}
 
 
 
