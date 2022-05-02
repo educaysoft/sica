d9c7f7e3818e8af44c90ef3f30673cac68893a82
Stalin Francis <sfrancis@192.168.1.10>
2022-05-02 06:53:31 -0400

    female

--- application/views/template/page_header.php
+++ application/views/template/page_header.php
@@ -10,6 +10,7 @@ if (isset($this->session->userdata['logged_in'])) {
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
 
 <!---- Para manejar el datatable -->
