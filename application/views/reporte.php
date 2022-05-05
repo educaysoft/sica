2b72e3c7110d83c7e4581ac260df2d3b6aab1dbc
Stalin Francis <sfrancis@192.168.1.10>
2022-05-04 23:43:54 -0400

    creando el reporte de participacion en pdf

--- application/views/reporte.php
+++ application/views/reporte.php
@@ -3,7 +3,7 @@
 //	require 'conexion.php';
 	
 //	$query = "SELECT e.estado, m.id_municipio, m.municipio FROM t_municipio AS m INNER JOIN t_estado AS e ON m.id_estado=e.id_estado";
-	$resultado = $mysqli->query($query);
+//	$resultado = $mysqli->query($query);
 	
 	$pdf = new PDF();
 	$pdf->AliasNbPages();
