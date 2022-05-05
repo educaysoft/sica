b5b4bbd0b367032d42e5c2f903964a5d9725a379
Stalin Francis <sfrancis@192.168.1.10>
2022-05-05 02:18:59 -0400

    creando el reporte de participacion en pdf

--- application/views/reporte.php
+++ application/views/reporte.php
@@ -14,7 +14,7 @@
 
 
  $pdf->Cell(8,6,'#',1,0,'C',1);
- $pdf->Cell(50,6,'Participante',1,0,'C',1);
+ $pdf->Cell(55,6,'Participante',1,0,'C',1);
 foreach ($fechaeventos as $row){
  $pdf->Cell(20,6,$row->tema,1,0,'C',1);
 }
@@ -36,17 +36,17 @@ $i=0;
 foreach ($participacion as $row){
   if($id!=$row->idpersona)
   {
-   if($id>0){
+   if($id>0
     $i=$i+1;
-    $pdf->Cell(8,6,$i,1,0,'R',1); 
-    $pdf->Cell(50,6,utf8_decode($arrparticipacion[$id]),1,0,'L',1);
+    $pdf->Cell(8,6,$i,1,0,'R',0); 
+    $pdf->Cell(55,6,utf8_decode($arrparticipacion[$id]),1,0,'L',0);
     foreach ($fechaeventos as $row1){
       if(isset($arrparticipacion[$row1->fecha])){
-         $pdf->Cell(20,6,round($arrparticipacion[$row1->fecha]/10,0),1,0,'R',1);
+         $pdf->Cell(20,6,round($arrparticipacion[$row1->fecha]/10,0),1,0,'R',0);
 	  $sum=$sum+ round($arrparticipacion[$row1->fecha]/10,0);
 	  $can=$can+1;
       }else{
-         $pdf->Cell(20,6,'0',1,0,'R',1);
+         $pdf->Cell(20,6,'0',1,0,'R',0);
 	  $sum=$sum+ 0;
 	  $can=$can+1;
 
@@ -54,9 +54,9 @@ foreach ($participacion as $row){
     }
 
       $resu=round(($sum/($can)),0);
-      $pdf->Cell(20,6,$sum,1,0,'R',1);
-      $pdf->Cell(20,6,$resu,1,0,'R',1);
-      $pdf->Cell(20,6,8,1,1,'R',1);
+      $pdf->Cell(20,6,$sum,1,0,'R',0);
+      $pdf->Cell(20,6,$resu,1,0,'R',0);
+      $pdf->Cell(20,6,8,1,1,'R',0);
 	$sum=0;
 	$can=0;
    }
@@ -69,23 +69,23 @@ foreach ($participacion as $row){
   }
 }
   $i=$i+1;
-    $pdf->Cell(8,6,$i,1,0,'R',1); 
-    $pdf->Cell(50,6,utf8_decode($arrparticipacion[$id]),1,0,'L',1);
+    $pdf->Cell(8,6,$i,1,0,'R',0); 
+    $pdf->Cell(55,6,utf8_decode($arrparticipacion[$id]),1,0,'L',0);
     foreach ($fechaeventos as $row1){
       if(isset($arrparticipacion[$row1->fecha])){
-         $pdf->Cell(20,6,round($arrparticipacion[$row1->fecha]/10,0),1,0,'R',1);
+         $pdf->Cell(20,6,round($arrparticipacion[$row1->fecha]/10,0),1,0,'R',0);
          $sum=$sum+round($arrparticipacion[$row1->fecha]/10,0);
 	 $can=$can+1;
       }else{
-         $pdf->Cell(20,6,'0',1,0,'R',1);
+         $pdf->Cell(20,6,'0',1,0,'R',0);
 	  $sum=$sum+ 0;
 	  $can=$can+1;
       }
     } 
       $resu=round(($sum/($can)), 0);
-      $pdf->Cell(20,6,$sum,1,0,'R',1);
-      $pdf->Cell(20,6,$resu,1,0,'R',1);
-      $pdf->Cell(20,6,8,1,1,'R',1);
+      $pdf->Cell(20,6,$sum,1,0,'R',0);
+      $pdf->Cell(20,6,$resu,1,0,'R',0);
+      $pdf->Cell(20,6,8,1,1,'R',0);
 	$sum=0;
 	$can=0;
 
