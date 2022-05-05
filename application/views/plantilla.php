9c3273b8933243d2cde584e52a8a6768e472e9bd
Stalin Francis <sfrancis@192.168.1.10>
2022-05-04 22:41:02 -0400

    creando el reporte de participacion en pdf

--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -1,12 +1,12 @@
 <?php
 use setasign\Fpdi\Fpdi;
 
-require_once('../../FPDI/vendor/autoload.php');
+require_once(base_url().'FPDI/vendor/autoload.php');
 class PDF extends FPDF
 {
 	function Header()
 	{
-		$this->Image('../../images/logo.png',5,5,30);
+		$this->Image(base_url().'images/logo.png',5,5,30);
 		$this->SetFont('Arial','B',15);
 		$this->Cell(30);
 		$this->Cell(120,10,'Reporte de Participación',0,0,'C');
