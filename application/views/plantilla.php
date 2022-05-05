057e360cb878465e8d8972e52bad2cebad6b20c2
Stalin Francis <sfrancis@192.168.1.10>
2022-05-05 01:33:10 -0400

    creando el reporte de participacion en pdf

--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -17,7 +17,7 @@ class PDF extends FPDF
 	function Footer()
 	{
 		$this->SetY(-15);
-		$this->SetFont('Ariral','I',8);
+		$this->SetFont('Arial','I',8);
 		$this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
 	}
 	}
