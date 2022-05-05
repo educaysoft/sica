382d4a97be8d73a6a5a40a1474c81646333f4695
Stalin Francis <sfrancis@192.168.1.10>
2022-05-05 02:01:24 -0400

    creando el reporte de participacion en pdf

--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -9,19 +9,19 @@ class PDF extends FPDF
 		$i=base_url().'images/logo.jpg';
 		$this->Image($i,5,5,20);
 		$this->SetFont('Arial','B',10);
-		$this->Cell(30);
+		$this->Cell(30,20);
 		$this->Cell(120,5,utf8_decode('UNIVERSIDAD TÉCNICA LUIS VARGAS TORRES DE ESMERALDAS'),0,1,'C');
 		$this->Cell(120,5,utf8_decode('FACULTAD DE INGENIERIAS (FACI)'),0,1,'C');
 		$this->Cell(120,5,utf8_decode('CARRERA EN TECNOLOGÍA DE LA INFORMACIÓN'),0,1,'C');
 		$this->Cell(120,5,utf8_decode('INFORME DE CÁTEDRA INTEGRADORA'),0,1,'C');
-		$this->Ln(10);
+		$this->Ln(8);
 		$this->Cell(40,5,utf8_decode('CÁTEDRA:'),0,0,'L');
-		$this->Cell(40,5,utf8_decode('Metodología de la Investigación'),0,2,'L');
+		$this->Cell(40,5,utf8_decode('Metodología de la Investigación'),0,1,'L');
 		$this->Cell(40,5,utf8_decode('PARALELO:'),0,0,'L');
 		$this->Cell(40,5,utf8_decode('B'),0,1,'L');
 		$this->Cell(40,5,utf8_decode('DOCENTE:'),0,0,'L');
 		$this->Cell(40,5,utf8_decode('Ing. Stalin Francis Q. M.Sc.'),0,1,'L');
-		$this->Ln(10);
+		$this->Ln(5);
 	}
 	function Footer()
 	{
