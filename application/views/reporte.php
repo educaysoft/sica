60bbbe0baf39692bedf23dccd140bd8088986a87
Stalin Francis <sfrancis@192.168.1.10>
2022-05-05 02:29:57 -0400

    creando el reporte de participacion en pdf

--- application/views/reporte.php
+++ application/views/reporte.php
@@ -14,13 +14,13 @@
 
 
  $pdf->Cell(8,6,'#',1,0,'C',1);
- $pdf->Cell(55,6,'Participante',1,0,'C',1);
+ $pdf->Cell(62,6,'Participante',1,0,'C',1);
 foreach ($fechaeventos as $row){
  $pdf->Cell(20,6,$row->tema,1,0,'C',1);
 }
- $pdf->Cell(20,6,'Total',1,0,'C',1);
- $pdf->Cell(20,6,'Prom',1,0,'C',1);
- $pdf->Cell(20,6,'Asis',1,1,'C',1);
+ $pdf->Cell(15,6,'Total',1,0,'C',1);
+ $pdf->Cell(15,6,'Prom',1,0,'C',1);
+ $pdf->Cell(15,6,'Asis',1,1,'C',1);
 $sum=0;
 $can=0;
 
@@ -39,7 +39,7 @@ foreach ($participacion as $row){
    if($id>0){
     $i=$i+1;
     $pdf->Cell(8,6,$i,1,0,'R',0); 
-    $pdf->Cell(55,6,utf8_decode($arrparticipacion[$id]),1,0,'L',0);
+    $pdf->Cell(62,6,utf8_decode($arrparticipacion[$id]),1,0,'L',0);
     foreach ($fechaeventos as $row1){
       if(isset($arrparticipacion[$row1->fecha])){
          $pdf->Cell(20,6,round($arrparticipacion[$row1->fecha]/10,0),1,0,'R',0);
@@ -54,9 +54,9 @@ foreach ($participacion as $row){
     }
 
       $resu=round(($sum/($can)),0);
-      $pdf->Cell(20,6,$sum,1,0,'R',0);
-      $pdf->Cell(20,6,$resu,1,0,'R',0);
-      $pdf->Cell(20,6,8,1,1,'R',0);
+      $pdf->Cell(15,6,$sum,1,0,'R',0);
+      $pdf->Cell(15,6,$resu,1,0,'R',0);
+      $pdf->Cell(15,6,8,1,1,'R',0);
 	$sum=0;
 	$can=0;
    }
@@ -70,7 +70,7 @@ foreach ($participacion as $row){
 }
   $i=$i+1;
     $pdf->Cell(8,6,$i,1,0,'R',0); 
-    $pdf->Cell(55,6,utf8_decode($arrparticipacion[$id]),1,0,'L',0);
+    $pdf->Cell(62,6,utf8_decode($arrparticipacion[$id]),1,0,'L',0);
     foreach ($fechaeventos as $row1){
       if(isset($arrparticipacion[$row1->fecha])){
          $pdf->Cell(20,6,round($arrparticipacion[$row1->fecha]/10,0),1,0,'R',0);
@@ -83,9 +83,9 @@ foreach ($participacion as $row){
       }
     } 
       $resu=round(($sum/($can)), 0);
-      $pdf->Cell(20,6,$sum,1,0,'R',0);
-      $pdf->Cell(20,6,$resu,1,0,'R',0);
-      $pdf->Cell(20,6,8,1,1,'R',0);
+      $pdf->Cell(15,6,$sum,1,0,'R',0);
+      $pdf->Cell(15,6,$resu,1,0,'R',0);
+      $pdf->Cell(15,6,8,1,1,'R',0);
 	$sum=0;
 	$can=0;
 
