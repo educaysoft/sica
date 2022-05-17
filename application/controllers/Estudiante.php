1375fffa762165ffefb4fbc8f2fe4a7d7b8e57fa
Stalin Francis <sfrancis@192.168.1.10>
2022-05-17 01:03:55 -0400

    errores en el modulo estudiante

--- application/controllers/Estudiante.php
+++ application/controllers/Estudiante.php
@@ -17,7 +17,7 @@ public function index(){
 		$data['estudiante']=$this->estudiante_model->elultimo();
 		$data['personas']= $this->persona_model->lista_personas()->result();
 		$data['departamentos']= $this->departamento_model->lista_departamentos()->result();
-		$data['estudios']= $this->estudio_model->estudios($data['estudiante']['idpersona']);
+		$data['estudios']= $this->estudio_model->estudios($data['estudiante']['idpersona'])->result();
 			
 		$data['title']="Lista de estudiantes";
 		$this->load->view('template/page_header');
