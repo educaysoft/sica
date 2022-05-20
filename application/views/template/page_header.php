c565513a697cd2d32bc7a0456a9e62d395df6bef
Stalin Francis <sfrancis@192.168.1.10>
2022-05-20 19:49:58 -0400

    title

--- application/views/template/page_header.php
+++ application/views/template/page_header.php
@@ -8,6 +8,7 @@ if (isset($this->session->userdata['logged_in'])) {
 }
 ?>
 <head>
+<title>sica</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
