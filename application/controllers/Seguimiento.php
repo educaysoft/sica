639cbb02c78f5a567d8413f06cc5e007239a78cb
Stalin Francis <sfrancis@192.168.1.10>
2022-05-29 18:48:38 -0400

    send mail

--- application/controllers/Seguimiento.php
+++ application/controllers/Seguimiento.php
@@ -436,22 +436,22 @@ public function send()
         echo lang('erro_no_js');
     }else{
 
-//	$this->load->database();
- //      $this->load->helper('form');
-      //  if($this->input->post('idpersona'))
-//	{
-//		$this->db-select('*');
-//		$this->db->where(array('idpersona'=>$this->input->post('idpersona'),'idcorreo_estado'=>1));
-//		$this->db->limit(1);
-//		$query=$this->db->get('correo');
-//		if($query->num_rows() >0) {
-//			$email=$query->result()[0]->nombre;
-//		}else{
+	$this->load->database();
+       $this->load->helper('form');
+       if($this->input->post('idpersona'))
+	{
+		$this->db-select('*');
+	$this->db->where(array('idpersona'=>$this->input->post('idpersona'),'idcorreo_estado'=>1));
+		$this->db->limit(1);
+		$query=$this->db->get('correo');
+		if($query->num_rows() >0) {
+			$email=$query->result()[0]->nombre;
+		}else{
         		$email = $this->input->post('email');
-//		}
-//	}else{
-//       		$email = $this->input->post('email');
-//	}
+		}
+	}else{
+       		$email = $this->input->post('email');
+	}
 
 
 
@@ -479,7 +479,7 @@ public function send()
 		$this->email->subject($this->input->post('tema'));
 	}else{
 		$this->email->subject('UTLVTE - MAESTRÍA EN TECNOLOGÍA DE LA INFORMACIÓN');
-	]	
+	}	
 
 	$this->email->message($msg);
         if ($this->email->send()){
