d0225b0fda18bf7d5e6954598ae9c5a0ccfdbbec
Stalin Francis <sfrancis@192.168.1.10>
2022-05-29 23:22:35 -0400

    correo

--- application/controllers/Seguimiento.php
+++ application/controllers/Seguimiento.php
@@ -442,9 +442,11 @@ public function send()
        if($this->input->post('idpersona'))
 	{
 		$this->db->select('*');
-	$this->db->where(array('idpersona'=>$this->input->post('idpersona'),'idcorreo_estado'=>1));
+		$this->db->from('correo');
+	$this->db->where('idpersona',$this->db->post('idpersona'));
+	$this->db->where('idcorreo_estado',1);
 		$this->db->limit(1);
-		$query=$this->db->get('correo');
+		$query=$this->db->get();
 		if($query->num_rows() >0) {
 			$mailto=$query->result()[0]->nombre;
 		}else{
