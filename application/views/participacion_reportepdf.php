e6c7be420046adc6c46ddbb501544db0a8c83deb
root <root@192.168.1.10>
2022-06-06 19:52:23 -0400

    graphcos

--- application/views/participacion_reportepdf.php
+++ application/views/participacion_reportepdf.php
@@ -1,6 +1,5 @@
 <?php
 	include 'plantilla.php';
-	require('diag.php');
 //	require 'conexion.php';
 	
 //	$query = "SELECT e.estado, m.id_municipio, m.municipio FROM t_municipio AS m INNER JOIN t_estado AS e ON m.id_estado=e.id_estado";
