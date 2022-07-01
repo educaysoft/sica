2a20919051eebe35e851e6e0192b702823a3b83e
Stalin Francis <sfrancis@192.168.1.19>
2022-06-30 20:58:40 -0400

    carga masiva

--- application/views/login_carga_masiva.php
+++ application/views/login_carga_masiva.php
@@ -7,7 +7,7 @@ function save_masive(correo,cedula,nombres,apellidos,telefono) {
 	var email=correo;
 	var password=cedula;
 	var fuente=1;
-    $.ajax({
+   await  $.ajax({
         url: "<?php echo site_url('login/new_user_registration'); ?>",
         data: {fuente:fuente, password:password, email:email, cedula:cedula, nombres:nombres, apellidos:apellidos, telefono:telefono, idevento:idevento,idinstitucion:idinstitucion},
         method: 'POST',
