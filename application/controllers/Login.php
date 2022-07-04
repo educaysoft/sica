6ba9441914e5da64511a176c7611d2ea4597b064
Stalin Francis <sfrancis@192.168.1.19>
2022-07-03 20:01:56 -0400

    cambiando

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -382,10 +382,10 @@ public function logout() {
 }
 
 public function carga_masiva(){
-	header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Credentials: true ");
-	header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
-	header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+//	header("Access-Control-Allow-Origin: *");
+////	header("Access-Control-Allow-Credentials: true ");
+//	header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+//	header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
 	$data['message_display'] = 'Successfully Logout';
 	$this->load->view('template/page_header.php');
 	$this->load->view('login_carga_masiva', $data);
