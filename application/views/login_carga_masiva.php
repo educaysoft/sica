375b61d6671ccd4098f91dc0b8878a31312274b5
Stalin Francis <sfrancis@192.168.1.4>
2022-07-06 06:38:13 -0400

    proceso de testing para la carga masiva

--- application/views/login_carga_masiva.php
+++ application/views/login_carga_masiva.php
@@ -75,13 +75,13 @@
         $movil1=str_replace("O","0",$movil0);
 	$movil2=str_pad($movil1,10,'0',STR_PAD_LEFT);
 
-       if($i>399){
+       if($i>1){
 	echo $row['correo']." - ".$cedula2." - ".$row['nombres']." - ".$row['apellidos']." - ".$movil2."<br>";	 
-    	echo "<script> save_masive('".$row['correo']."','".$cedula2."','".$row['nombres']."','".$row['apellidos']."','".$movil2."'); </script>";
+    	echo "<script> save_masive(".$row['correo'].",".$cedula2.",".$row['nombres'].",".$row['apellidos'].",".$movil2."); </script>";
 	sleep(1);
        }
 	$i=$i+1;
-	if($i==600)
+	if($i==220)
 	{
 	break;
 	}
