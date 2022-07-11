7fb7df1ce711208d3e2cd1d0155c25255b92cb7b
Stalin Francis <sfrancis@192.168.1.6>
2022-07-10 23:38:00 -0400

    mejorando la carga

--- application/views/login_carga_masiva.php
+++ application/views/login_carga_masiva.php
@@ -75,7 +75,7 @@
         $movil1=str_replace("O","0",$movil0);
 	$movil2=str_pad($movil1,10,'0',STR_PAD_LEFT);
 
-       if($i>199{
+       if($i>199){
 	echo $row['correo']." - ".$cedula2." - ".$row['nombres']." - ".$row['apellidos']." - ".$movil2."<br>";	 
     	echo "<script> save_masive(`".$row['correo']."`,`".$cedula2."`,`".$row['nombres']."`,`".$row['apellidos']."`,`".$movil2."`); </script>";
 	sleep(1);
