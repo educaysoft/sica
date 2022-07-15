b641b89cf956034b2e94be8d1b157399fec598e6
Stain Francis <educaysoft@gmail.com>
2022-07-14 21:04:08 -0400

    certiicado

--- application/views/certificado_record.php
+++ application/views/certificado_record.php
@@ -29,7 +29,6 @@ if(isset($certificado))
 
 
 <?php echo form_hidden('idcertificado',$certificado['idcertificado']) ?>
-<table>
 
 
 
@@ -83,119 +82,173 @@ if(isset($certificado))
 
 
 
-<tr>
-      <td>Evento:</td>
-      <td><?php
+<div class="form-group row">
+<label class="col-md-2 col-form-label">Ubicación:</label>
+<div class="col-md-10">
+<?php
   $eys_arrctl=array("name"=>'evento','value'=>$certificado['evento'],"disabled"=>"disabled",'placeholder'=>'Evento del certificado','style'=>'width:600px;');
- echo form_input($eys_arrctl) ?></td>
-</tr>
+ echo form_input($eys_arrctl); 
+?>
+</div>
+</div>
 
-<tr>
-     <td>Evento:</td>
-     <td><?php 
+
+
+<div class="form-group row">
+<label class="col-md-2 col-form-label">Evento:</label>
+<div class="col-md-10">
+<?php
+
 $options= array("NADA");
 foreach ($eventos as $row){
 	$options[$row->idevento]= $row->titulo;
 }
 
-echo form_input('idevento',$options[$certificado['idevento']],array("disabled"=>"disabled",'style'=>'width:500px;')) ?></td>
-  </tr>
+echo form_input('idevento',$options[$certificado['idevento']],array("disabled"=>"disabled",'style'=>'width:500px;'));
+?>
+</div>
+</div>
 
 
-<tr>
-     <td>Tipo documento:</td>
-     <td><?php 
+
+<div class="form-group row">
+<label class="col-md-2 col-form-label">Tipo documento:</label>
+<div class="col-md-10">
+<?php
+
 $options= array("NADA");
 foreach ($tipodocus as $row){
 	$options[$row->idtipodocu]= $row->descripcion;
 }
 
 echo form_input('idtipodocu',$options[$certificado['idtipodocu']],array("disabled"=>"disabled",'style'=>'width:500px;')) ?></td>
-  </tr>
+?>
+</div>
+</div>
 
 
-
-<tr>
-     <td>Documento:</td>
-     <td><?php 
+<div class="form-group row">
+<label class="col-md-2 col-form-label">Documento:</label>
+<div class="col-md-10">
+<?php
 $options= array("NADA");
 foreach ($documentos as $row){
 	$options[$row->iddocumento]= $row->asunto;
 }
 
-echo form_input('iddocumento',$options[$certificado['iddocumento']],array("disabled"=>"disabled",'style'=>'width:500px;')) ?></td>
-  </tr>
+echo form_input('iddocumento',$options[$certificado['iddocumento']],array("disabled"=>"disabled",'style'=>'width:500px;')); 
+?>
+</div>
+</div>
 
 
 
-<tr>
-      <td>Ancho x(296.67):</td>
-      <td><?php
+<div class="form-group row">
+<label class="col-md-2 col-form-label"> Ancho x(296.67):</label>
+<div class="col-md-10">
+<?php
   $eys_arrctl=array("name"=>'ancho_x','value'=>$certificado['ancho_x'],"disabled"=>"disabled",'placeholder'=>'Ancho del certificado','style'=>'width:600px;');
- echo form_input($eys_arrctl) ?></td>
-</tr>
+ echo form_input($eys_arrctl); 
+?>
+</div>
+</div>
 
-<tr>
-      <td>Alto  Y(210.56):</td>
-      <td><?php
+
+
+<div class="form-group row">
+<label class="col-md-2 col-form-label">Alto  Y(210.56): </label>
+<div class="col-md-10">
+<?php
+
   $eys_arrctl=array("name"=>'alto_y','value'=>$certificado['alto_y'],"disabled"=>"disabled",'placeholder'=>'Alto del certificado y','style'=>'width:600px;');
- echo form_input($eys_arrctl) ?></td>
-</tr>
+ echo form_input($eys_arrctl);
+
+?>
+</div>
+</div>
 
 
 
-
-
-
-<tr>
-      <td>posi nombre X:</td>
-      <td><?php
+<div class="form-group row">
+<label class="col-md-2 col-form-label"> posi nombre X: </label>
+<div class="col-md-10">
+<?php
   $eys_arrctl=array("name"=>'posi_nomb_x','value'=>$certificado['posi_nomb_x'],"disabled"=>"disabled",'placeholder'=>'Posicion de nombre en x','style'=>'width:600px;');
- echo form_input($eys_arrctl) ?></td>
-</tr>
+ echo form_input($eys_arrctl); 
 
-<tr>
-      <td>posi nombre Y(115 mm):</td>
-      <td><?php
+?>
+</div>
+</div>
+
+
+
+
+<div class="form-group row">
+<label class="col-md-2 col-form-label">posi nombre Y(115 mm) : </label>
+<div class="col-md-10">
+<?php
   $eys_arrctl=array("name"=>'posi_nomb_y','value'=>$certificado['posi_nomb_y'],"disabled"=>"disabled",'placeholder'=>'Posicion de nombre en y','style'=>'width:600px;');
- echo form_input($eys_arrctl) ?></td>
-</tr>
+ echo form_input($eys_arrctl);
+?>
+</div>
+</div>
 
 
 
-
-<tr>
-      <td>posi fecha X:</td>
-      <td><?php
+<div class="form-group row">
+<label class="col-md-2 col-form-label"> posi fecha X : </label>
+<div class="col-md-10">
+<?php
   $eys_arrctl=array("name"=>'posi_fecha_x','value'=>$certificado['posi_fecha_x'],"disabled"=>"disabled",'placeholder'=>'Posicion de fecha en x','style'=>'width:600px;');
- echo form_input($eys_arrctl) ?></td>
-</tr>
+ echo form_input($eys_arrctl);
+?>
+</div>
+</div>
 
-<tr>
-      <td>posi fecha Y(115 mm):</td>
-      <td><?php
+
+
+
+<div class="form-group row">
+<label class="col-md-2 col-form-label">posi fecha Y(115 mm): </label>
+<div class="col-md-10">
+<?php
+
   $eys_arrctl=array("name"=>'posi_fecha_y','value'=>$certificado['posi_fecha_y'],"disabled"=>"disabled",'placeholder'=>'Posicion de fecha en y','style'=>'width:600px;');
- echo form_input($eys_arrctl) ?></td>
-</tr>
+ echo form_input($eys_arrctl);
+?>
+</div>
+</div>
 
 
 
+<div class="form-group row">
+<label class="col-md-2 col-form-label">posi codigo X: </label>
+<div class="col-md-10">
+<?php
 
-
-
-<tr>
-      <td>posi codigo X:</td>
-      <td><?php
   $eys_arrctl=array("name"=>'posi_codigo_x','value'=>$certificado['posi_codigo_x'],"disabled"=>"disabled",'placeholder'=>'Posicion de codigo en x','style'=>'width:600px;');
- echo form_input($eys_arrctl) ?></td>
-</tr>
+ echo form_input($eys_arrctl);
 
-<tr>
-      <td>posi codigo Y(115 mm):</td>
-      <td><?php
+?>
+</div>
+</div>
+
+
+
+
+<div class="form-group row">
+<label class="col-md-2 col-form-label">posi codigo Y(115 mm): </label>
+<div class="col-md-10">
+<?php
+
   $eys_arrctl=array("name"=>'posi_codigo_y','value'=>$certificado['posi_codigo_y'],"disabled"=>"disabled",'placeholder'=>'Posicion de codigo en y','style'=>'width:600px;');
- echo form_input($eys_arrctl) ?></td>
-</tr>
+ echo form_input($eys_arrctl);
+
+?>
+</div>
+</div>
+
+
 
 
 
@@ -236,7 +289,6 @@ $textarea_options = array('class' => 'form-control','rows' => '4',   'cols' => '
 
 
 
-</table>
 <?php echo form_close(); ?>
 
 
