44609b5c45dbd0e441e8e565da8915095896a0d4
Stain Francis <educaysoft@gmail.com>
2022-07-17 17:25:45 -0400

    certificado con firma 2

--- FPDI/certificado.php
+++ FPDI/certificado.php
@@ -67,86 +67,81 @@ $pageCount=$pdf->setSourceFile($x);
 // import page 1
 echo $pageCount;
 for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++) {
+	$tplId = $pdf->importPage($pageNo);
+	//$pdf->useTemplate($tplId, 20, 10, 200);
 
-$tplId = $pdf->importPage($pageNo);
-//$pdf->useTemplate($tplId, 20, 10, 200);
+	$pdf->AddPage('L',[296.67,210.56]);
+	//El tamaño se acopa al tamaño del template
+	//$pdf->AddPage('L', array($size['w'], $size['h']));
+	// use the imported page and place it at point 10,10 with a width of 100 mm
 
-$pdf->AddPage('L',[296.67,210.56]);
-//El tamaño se acopa al tamaño del template
-//$pdf->AddPage('L', array($size['w'], $size['h']));
-// use the imported page and place it at point 10,10 with a width of 100 mm
+	//$pdf->useTemplate($tplId,null,null,$size['w'],$size['h'],FALSE);
+	//$size['w']=296.67 y $size['h']=210.56
+	//$pdf->useTemplate($tplId,null,null,210.56,296.67,FALSE);
+	$pdf->useTemplate($tplId);
 
-//$pdf->useTemplate($tplId,null,null,$size['w'],$size['h'],FALSE);
-//$size['w']=296.67 y $size['h']=210.56
-//$pdf->useTemplate($tplId,null,null,210.56,296.67,FALSE);
-$pdf->useTemplate($tplId);
+	$mid_x=148;
 
-$mid_x=148;
+	//write the code of de certificaod
+	$pdf->SetFont('Helvetica','B',15);
 
-//write the code of de certificaod
-$pdf->SetFont('Helvetica','B',15);
-
-if($posi_codigo_x==0)
-{
-	$pdf->SetTextColor(0, 0,0);
-}else{
-	$pdf->SetTextColor(255,255,255);
-}
-
-
-
-$pdf->SetXY($posi_codigo_x,$posi_codigo_y);
-if($posi_codigo_x==0)
-{
-	$espacio_impresion=$posif;  
-}else{
-	$espacio_impresion=$pdf->GetStringWidth($codigo);
-}
-$realposix=$posi_codigo_x+($espacio_impresion/2-($pdf->GetStringWidth($codigo)/2));
-$pdf->Text($realposix,$posi_codigo_y,$codigo);
-
-// now write some text above the imported page
-$pdf->SetFont('Helvetica','B',20);
-$pdf->SetTextColor(0, 0,255);
-$pdf->SetXY($pos_nomb_x,$posi_nomb_y);
-$espacio_impresion=$posif;  //-$posix;
-$realposix=$posi_nomb_x+($espacio_impresion/2-($pdf->GetStringWidth($participante)/2));
-$pdf->Text($realposix,$posi_nomb_y,utf8_decode($participante));
-
-//write the date of de certificaod
-$pdf->SetFont('Helvetica','B',15);
-if($posi_codigo_x==0)
-{
-$pdf->SetTextColor(0, 0,0);
-}else{
-
-$pdf->SetTextColor(255,255,255);
-
-
-if($firma2_x>0 && $firma2_y>0)
-{
-
-$pdf->Image('https://educaysoft.org/firmas/francisstalin.png',$firma1_x,$firma1_y);
-
-}
-
-
-
-
-if(intval($posi_fecha_y)>0)
-{
-
-	$pdf->SetXY($posi_fecha_x,$posi_fecha_y);
-	if(intval($posi_fecha_x)==0)
+	if($posi_codigo_x==0)
 	{
-	$espacio_impresion=$posif;  
+		$pdf->SetTextColor(0, 0,0);
 	}else{
-	$espacio_impresion=$pdf->GetStringWidth($fecha);
+		$pdf->SetTextColor(255,255,255);
 	}
 
-	$realposix=$posi_fecha_x+($espacio_impresion/2-($pdf->GetStringWidth($fecha)/2));
-	$pdf->Text($realposix,$posi_fecha_y,$espacio_impresion);
-}
+
+
+	$pdf->SetXY($posi_codigo_x,$posi_codigo_y);
+	if($posi_codigo_x==0)
+	{
+		$espacio_impresion=$posif;  
+	}else{
+		$espacio_impresion=$pdf->GetStringWidth($codigo);
+	}
+	$realposix=$posi_codigo_x+($espacio_impresion/2-($pdf->GetStringWidth($codigo)/2));
+	$pdf->Text($realposix,$posi_codigo_y,$codigo);
+
+	// now write some text above the imported page
+	$pdf->SetFont('Helvetica','B',20);
+	$pdf->SetTextColor(0, 0,255);
+	$pdf->SetXY($pos_nomb_x,$posi_nomb_y);
+	$espacio_impresion=$posif;  //-$posix;
+	$realposix=$posi_nomb_x+($espacio_impresion/2-($pdf->GetStringWidth($participante)/2));
+	$pdf->Text($realposix,$posi_nomb_y,utf8_decode($participante));
+
+	//write the date of de certificaod
+	$pdf->SetFont('Helvetica','B',15);
+	if($posi_codigo_x==0)
+	{
+		$pdf->SetTextColor(0, 0,0);
+	}else{
+		$pdf->SetTextColor(255,255,255);
+	}
+
+	if($firma2_x>0 && $firma2_y>0)
+	{
+		$pdf->Image('https://educaysoft.org/firmas/francisstalin.png',$firma1_x,$firma1_y);
+	}
+
+
+
+
+	if(intval($posi_fecha_y)>0)
+	{
+		$pdf->SetXY($posi_fecha_x,$posi_fecha_y);
+		if(intval($posi_fecha_x)==0)
+		{
+		$espacio_impresion=$posif;  
+		}else{
+		$espacio_impresion=$pdf->GetStringWidth($fecha);
+		}
+
+		$realposix=$posi_fecha_x+($espacio_impresion/2-($pdf->GetStringWidth($fecha)/2));
+		$pdf->Text($realposix,$posi_fecha_y,$espacio_impresion);
+	}
 
 }
 //$pdf->Output('I', 'generated.pdf');
