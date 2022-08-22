5957ef301a3bb0bdda01d5a9401759db89dc1641
Stain Francis <educaysoft@gmail.com>
2022-08-22 02:06:18 -0400

    mejorando modulo de tema

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -348,7 +348,7 @@ if ($result == TRUE) {
 			redirect('Principal'); 
 		}else{		
    date_default_timezone_set('America/Guayaquil');
-    $date = date("d-m-Y");
+    $date = date("Y-m-d");
     $asistencia=array('idpersona'=>$result[0]->idpersona,'idevento'=>$result[0]->idevento,'fecha'=>$date,'idtipoasistencia'=>1,'comentario'=>"INGRESO AL SISTEMA");
     $this->asistencia_model->save($asistencia);
 
