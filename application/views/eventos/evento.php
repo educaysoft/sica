dea42a1282e6ad72ef19b345d8585536e01667ee
Stain Francis <educaysoft@gmail.com>
2022-08-27 23:51:46 -0400

    eventos

--- application/views/eventos/evento.php
+++ application/views/eventos/evento.php
@@ -368,7 +368,7 @@ echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="curre
           </div>
 
           <div class="col-sm-4">	
-            <span>EVENTO</span> 
+            <span>TOTALES</span> 
           </div>
 
           <div class="col-sm-3">
@@ -376,21 +376,17 @@ echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="curre
           </div>
 
           <div class="col-sm-1">
-		<?php
-           echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-check" viewBox="0 0 16 16"> <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/><path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/></svg>';
-		?>
+            <span>Clases </span>
 	 </div>
 
           <div class="col-sm-1">
-            <span>Activ. y<br>Partici.</span>
+            <span>Calificacion</span>
           </div>
           <div class="col-sm-1">
-            <span>AYUDA</span>
+            <span>Ayuda</span>
           </div>
           <div class="col-sm-1">
-	<?php
-           echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16"><path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/></svg>';
-		?>
+            <span>Pago</span>
 
           </div>
 
