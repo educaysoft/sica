10c674151d26052685c6aa2d60b4c96b40714564
Stain Francis <educaysoft@gmail.com>
2022-09-05 18:23:10 -0400

    Mejoreas

--- application/views/evento_record.php
+++ application/views/evento_record.php
@@ -307,8 +307,8 @@ echo form_input($arrdatos) ?>
 <script type="text/javascript">
 
 $(document).ready(function(){
-	var idevento = 39; // $('select[name=idevento]').val();
-	var mytabla= $('#mydatac').DataTable({"ajax": {url: '<?php echo site_url('evento/evento_fechas')?>', type: 'GET',data:{idevento:idevento}},},});
+	var idevento =  $('select[name=idevento]').val();
+	var mytabla= $('#mydatac').DataTable({"ajax": {url: '<?php echo site_url('evento/evento_fechas')?>', type: 'GET',data:{idevento:idevento}},});
 });
 
 $('#show_data').on('click','.item_ver',function(){
