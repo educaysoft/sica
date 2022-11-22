1ee20a350bd4c7c6385a7c8ec091c9ddc22f630d
Stain Francis <educaysoft@gmail.com>
2022-11-22 14:55:16 -0500

    mejoras a evento

--- application/controllers/Evento.php
+++ application/controllers/Evento.php
@@ -329,7 +329,7 @@ public function index(){
 			$data=array();
 			foreach($data0->result() as $r){
 				$data[]=array($r->idparticipante,$r->nombres,$r->archivopdf,
-				$r->href='<a href="javascript:void(0);" class="btn btn-info btn-sm item_ver"  data-retorno="'.site_url('participante/actual').'"    data-idevento="'.$r->idparticipante.'">Ver</a>');
+				$r->href='<a href="javascript:void(0);" class="btn btn-info btn-sm item_ver"  data-retorno="'.site_url('participante/actual').'"    data-idparticipante="'.$r->idparticipante.'">Ver</a>');
 			}	
 			$output=array( "draw"=>$draw,
 				"recordsTotal"=> $data0->num_rows(),
