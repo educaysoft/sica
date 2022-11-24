48f87f86ba95deae2e79edcdc083a899f2d8aae9
Stain Francis <educaysoft@gmail.com>
2022-11-24 15:08:07 -0500

    mejoras

--- application/views/periodoacademico_edit.php
+++ application/views/periodoacademico_edit.php
@@ -6,13 +6,24 @@
  
     
   <tr>
-      <td>Nombre:</td>
+      <td>Nombre largo:</td>
       <td><?php
  
-$eys_arrinput=array('name'=>'nombre','value'=>$periodoacademico['nombre'], "style"=>"width:500px");
+$eys_arrinput=array('namelargo'=>'nombrelargo','value'=>$periodoacademico['nombrelargo'], "style"=>"width:500px");
  echo form_input($eys_arrinput); ?></td>
   </tr>
+
+<tr>
+      <td>Nombre corto:</td>
+      <td><?php
  
+$eys_arrinput=array('namecorto'=>'nombrecorto','value'=>$periodoacademico['nombrecorto'], "style"=>"width:500px");
+ echo form_input($eys_arrinput); ?></td>
+  </tr>
+
+
+
+
  <tr>
  <td colspan="2"> <hr><?php echo form_submit('submit', 'Guardar'); ?> <?php echo anchor('periodoacademico','Atras') ?></td>
  </tr>
