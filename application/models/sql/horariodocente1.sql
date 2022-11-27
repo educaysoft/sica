2ca65746291ed41e1950fc222595a6e208a4f5d3
Stain Francis <educaysoft@gmail.com>
2022-11-27 08:04:28 -0500

    mejoras

--- application/models/sql/horariodocente1.sql
+++ application/models/sql/horariodocente1.sql
@@ -1,4 +1,3 @@
 
 use educayso_facae;
-drop view horariodocente1;
-create view horariodocente1 as select hd.idhorariodocente, hd.iddocente, concat(COALESCE(pe.apellidos,''),"  ",COALESCE(pe.nombres,'')) as eldocente, hd.idperiodoacademico, pa.nombrecorto as elperiodoacademico  from horariodocente hd, periodoacademico pa, docente do, persona pe where hd.iddocente=do.iddocente and do.idpersona=pe.idpersona and hd.idperiodoacademico=pa.idperiodoacademico ;
+create view horariodocente1 as select hd.idhorariodocente, hd.iddocente, concat(COALESCE(pe.apellidos,''),"  ",COALESCE(pe.nombres,'')) as eldocente, hd.idperiodoacademico, pa.nombrecorto as elperiodoacademico, concat(COALESCE(pa.nombrecorto,'')," - ", COALESCE(pe.apellidos,''),"  ",COALESCE(pe.nombres,'')) as elhorariodocente   from horariodocente hd, periodoacademico pa, docente do, persona pe where hd.iddocente=do.iddocente and do.idpersona=pe.idpersona and hd.idperiodoacademico=pa.idperiodoacademico ;
