b9ca6b51c82a567a136edf345a958c762130be6a
Stain Francis <educaysoft@gmail.com>
2022-11-26 19:21:27 -0500

    mejoras

--- application/views/malla_edit.php
+++ application/views/malla_edit.php
@@ -3,16 +3,52 @@
 <h2> <?php echo $title; ?></h2>
 <hr />
 <table>
- 
+
+<tr>
+<td> Departamento/carrera:</td>
+<td><?php
+$options= array('--Select--');
+foreach ($departamentos as $row){
+	$options[$row->iddepartamento]= $row->nombre;
+}
+
+ echo form_dropdown("iddepartamento",$options, $malla['iddepartamento']);  ?></td>
+</tr>
+
+
+
     
   <tr>
-      <td>Nombre:</td>
+      <td>Nombre corto:</td>
       <td><?php
  
-$eys_arrinput=array('name'=>'nombre','value'=>$malla['nombre'], "style"=>"width:500px");
+$eys_arrinput=array('name'=>'nombrecorto','value'=>$malla['nombrecorto'], "style"=>"width:500px");
  echo form_input($eys_arrinput); ?></td>
   </tr>
+
+<tr>
+      <td>Nombre largo:</td>
+      <td><?php
  
+$eys_arrinput=array('name'=>'nombrelargo','value'=>$malla['nombrelargo'], "style"=>"width:500px");
+ echo form_input($eys_arrinput); ?></td>
+  </tr>
+
+
+<tr>
+<td> Periodo académico:</td>
+<td><?php
+$options= array('--Select--');
+foreach ($periodoacademicos as $row){
+	$options[$row->idperiodoacademico]= $row->nombrelargo;
+}
+
+ echo form_dropdown("idperidoacademico",$options, $malla['idperidoacademico']);  ?></td>
+</tr>
+
+
+
+
  <tr>
  <td colspan="2"> <hr><?php echo form_submit('submit', 'Guardar'); ?> <?php echo anchor('malla','Atras') ?></td>
  </tr>
