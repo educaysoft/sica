6ba23246a59b0255473d757058feb6988dd5e74e
Stain Francis <educaysoft@gmail.com>
2022-12-05 23:01:04 -0500

    mejoras

--- application/views/fechaevento_form.php
+++ application/views/fechaevento_form.php
@@ -106,7 +106,7 @@ $textarea_options = array('class' => 'form-control','rows' => '4',   'cols' => '
 <div class="col-md-10">
 <?php
 
- echo form_input(array("name"=>"horainicio","id"=>"horainicio","type"=>"time"));  
+ echo form_input(array("name"=>"horainicio","id"=>"horainicio","type"=>"time","value"=>$hora));  
 
 ?>
 </div>
