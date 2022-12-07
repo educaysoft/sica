f02649da94c9ababbad706950e50b7b313737686
Stain Francis <educaysoft@gmail.com>
2022-12-07 18:39:25 -0500

    mejoras

--- application/views/jornadadocente_record.php
+++ application/views/jornadadocente_record.php
@@ -31,47 +31,78 @@ if(isset($jornadadocente))
 
 <?php echo form_open('jornadadocente/save_edit') ?>
 <?php echo form_hidden('idjornadadocente',$jornadadocente['idjornadadocente']) ?>
-<table>
-  <tr>
-     <td>Id JornadaDocente:</td>
-     <td><?php echo form_input('idjornadadocente',$jornadadocente['idjornadadocente'],array("disabled"=>"disabled",'placeholder'=>'Idjornadadocentes')) ?></td>
-  </tr>
- 
- 
-<tr>
-     <td>AsignaturaDocente:</td>
-     <td><?php 
-$options= array("NADA");
-foreach ($asignaturadocentes as $row){
-	$options[$row->idasignaturadocente]= $row->laasignatura;
-}
 
-echo form_input('idasignaturadocente',$options[$jornadadocente['idasignaturadocente']],array("disabled"=>"disabled",'style'=>'width:500px;')) ?></td>
-  </tr>
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label">ID JornadaDOCENTE:</label>
+	<div class="col-md-10">
+		<?php
+
+
+     echo form_input('idjornadadocente',$jornadadocente['idjornadadocente'],array("disabled"=>"disabled",'placeholder'=>'Idjornadadocentes'));
+
+		?>
+	</div> 
+</div>
  
 
- <tr>
-     <td>Dia semana:</td>
-     <td><?php 
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Asignatura docente:</label>
+	<div class="col-md-10">
+		<?php
+	$options= array("NADA");
+	foreach ($asignaturadocentes as $row){
+		$options[$row->idasignaturadocente]= $row->laasignatura;
+	}
+echo form_input('idasignaturadocente',$options[$jornadadocente['idasignaturadocente']],array("disabled"=>"disabled",'style'=>'width:500px;'));  
+
+		?>
+	</div> 
+</div>
+ 
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Dia semana:</label>
+	<div class="col-md-10">
+		<?php
     $options= array("NADA");
     foreach ($diasemanas as $row){
 	      $options[$row->iddiasemana]= $row->nombre;
     }
-    echo form_input('iddiasemana',$options[$jornadadocente['iddiasemana']],array("disabled"=>"disabled",'style'=>'width:500px;')) ?></td>
- </tr>
+    echo form_input('iddiasemana',$options[$jornadadocente['iddiasemana']],array("disabled"=>"disabled",'style'=>'width:500px;'));
+		?>
+	</div> 
+</div>
+
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Hora inicio:</label>
+	<div class="col-md-10">
+		<?php
+       echo form_input('horainicio',$jornadadocente['horainicio'],array('placeholder'=>'número de sisión','style'=>'width:50px;'));
+		?>
+	</div> 
+</div>
+
   
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Duración:</label>
+	<div class="col-md-10">
+		<?php
+       echo form_input('duracionminutos',$jornadadocente['duracionminutos'],array('placeholder'=>'duración en minutos','style'=>'width:50px;'));
+		?>
+	</div> 
+</div>
 
 
-  
 
 
 
 
 
-
-
-
-</table>
 <?php echo form_close(); ?>
 
 
