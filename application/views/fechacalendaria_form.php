6aa4da35c86c8297fac5f3d1cfb5a02f057f8302
Stain Francis <educaysoft@gmail.com>
2022-12-07 05:54:59 -0500

    mejoras

--- application/views/fechacalendaria_form.php
+++ application/views/fechacalendaria_form.php
@@ -3,36 +3,26 @@
 <?php echo form_open("fechacalendaria/save") ?>
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Silabo:</label>
+    <label class="col-md-2 col-form-label"> Periodo académico:</label>
 	<div class="col-md-10">
 		<?php
 $options= array('--Select--');
-foreach ($silabos as $row){
-	$options[$row->idsilabo]= $row->nombre;
+foreach ($periodoacademicos as $row){
+	$options[$row->idperiodoacademico]= $row->nombre;
 }
 
- echo form_dropdown($name="idsilabo",$options, set_select('--Select--','default_value'),array('id'=>'idsilabo','onchange'=>'get_periodoacademico()'));  
+ echo form_dropdown($name="idperiodoacademico",$options, set_select('--Select--','default_value'),array('id'=>'idperiodoacademico','onchange'=>'get_periodoacademico()'));  
 		?>
 	</div> 
 </div>
 
 
 
-<div class="form-group row">
-<label class="col-md-2 col-form-label">Unidad silabo:</label>
-<div class="col-md-10">
-    <div class="form-group">
-         <select class="form-control" id="idperiodoacademico" name="idperiodoacademico" required>
-                 <option>No Selected</option>
-          </select>
-    </div>
 
-</div>
-</div>
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Nombre corto:</label>
+    <label class="col-md-2 col-form-label"> Actividad realizada:</label>
 	<div class="col-md-10">
 		<?php
  echo form_input("actividad","", array("placeholder"=>"Nombre de fechacalendaria",'style'=>'width:500px;'));
@@ -41,7 +31,7 @@ foreach ($silabos as $row){
 </div> 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Nombre largo:</label>
+    <label class="col-md-2 col-form-label"> Detalle de actividad:</label>
 	<div class="col-md-10">
 		<?php
  echo form_input("detalle","", array("placeholder"=>"Nombre de fechacalendaria",'style'=>'width:500px;'));
@@ -50,40 +40,26 @@ foreach ($silabos as $row){
 </div>
 
 <div class="form-group row">
-<label class="col-md-2 col-form-label">Fecha impartida:</label>
+<label class="col-md-2 col-form-label">Fecha calendaria:</label>
 <div class="col-md-10">
 <?php
 
 
- echo form_input(array("name"=>"fechaimpartida","id"=>"fechaimpartida","type"=>"date"));  
+ echo form_input(array("name"=>"fechacalendaria","id"=>"fechacalendaria","type"=>"date"));  
 
 ?>
 </div>
 </div>
 
 
-<div class="form-group row">
-    <label class="col-md-2 col-form-label"> Duración (minutos):</label>
-	<div class="col-md-10">
-		<?php
- echo form_input("duracionminutos","", array("placeholder"=>"Duracion en minutos",'style'=>'width:100px;'));
-		?>
-	</div> 
-</div>
+
+
+
+
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Número de sesión:</label>
-	<div class="col-md-10">
-		<?php
- echo form_input("numerosesion","", array("placeholder"=>"número de sesión",'style'=>'width:50px;'));
-		?>
-	</div> 
-</div>
-
-
-<div class="form-group row">
-    <label class="col-md-2 col-form-label"> Videotutorial:</label>
+    <label class="col-md-2 col-form-label"> Institucion:</label>
 	<div class="col-md-10">
 	<?php
 	$options= array('--Select--');
@@ -115,10 +91,10 @@ foreach ($silabos as $row){
 
 
 function get_periodoacademico() {
-	var idsilabo = $('select[name=idsilabo]').val();
+	var idperiodoacademico = $('select[name=idperiodoacademico]').val();
     $.ajax({
         url: "<?php echo site_url('fechacalendaria/get_periodoacademico') ?>",
-        data: {idsilabo: idsilabo},
+        data: {idperiodoacademico: idperiodoacademico},
         method: 'POST',
 	async : true,
         dataType : 'json',
