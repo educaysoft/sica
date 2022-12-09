952ff82012c067c06c81601b663d9b770c902864
Stain Francis <educaysoft@gmail.com>
2022-12-08 20:36:59 -0500

    mejoras

--- application/views/asignatura_list.php
+++ application/views/asignatura_list.php
@@ -82,7 +82,7 @@ $(document).ready(function(){
 
 
 $('#show_data').on('click','.item_ver',function(){
-	var id= $(this).data('idiasignatura');
+	var id= $(this).data('idasignatura');
 	var retorno= $(this).data('retorno');
 	window.location.href = retorno+'/'+id;
 
