f3758164b2d9834c6f867792208f7e615c85762b
Stain Francis <educaysoft@gmail.com>
2022-12-10 10:20:21 -0500

    mejoras

--- application/views/docente_record.php
+++ application/views/docente_record.php
@@ -145,7 +145,7 @@ echo form_input('iddepartamento',$options[$docente['iddepartamento']],array("dis
       	<!-- Page Heading -->
  	<div class="row">
   	<div class="col-12">
-	<table class="table table-striped table-bordered table-hover" id="mydataie">
+	<table class="table table-striped table-bordered table-hover" id="mydatae">
 	 <thead>
 	 <tr>
 	 <th>idpersona</th>
