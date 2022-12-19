02163b6e2bd828c05f63ef7770576b2e2912ce13
Stain Francis <educaysoft@gmail.com>
2022-12-18 23:56:48 -0500

    mejoras

--- application/models/Documento_model.php
+++ application/models/Documento_model.php
@@ -93,7 +93,7 @@ class Documento_model extends CI_model {
 		if($evento->num_rows()>0)
 		{
 
- 		echo json_encode(json_decode($evento->first_row('array').toString()),JSON_PRETTY_PRINT);
+ 		echo json_encode($evento->first_row('array'),JSON_PRETTY_PRINT);
 		}else{
 
  		echo json_encode(array(),JSON_PRETTY_PRINT);
