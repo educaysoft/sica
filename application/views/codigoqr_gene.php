e481a51dd274dc72ad17965361a35bc124220b17
Stain Francis <educaysoft@gmail.com>
2022-12-19 23:26:26 -0500

    jha

--- application/views/codigoqr_gene.php
+++ application/views/codigoqr_gene.php
@@ -3,7 +3,7 @@
 require('QR/phpqrcode/qrlib.php') ;
 $archivo=  'QR/codigosqr/'.$codigoqr['filename'];
 echo $archivo;
-QRcode::png($codigoqr['contenido'],$archivo,$codigoqr['level'],$codigoqr['tamanio'],$codigoqr['framesize']);
+QRcode::png($codigoqr['contenido'],$codigosqr['filename'],$codigoqr['level'],$codigoqr['tamanio'],$codigoqr['framesize']);
 
 
 
