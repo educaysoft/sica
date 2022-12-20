b1b74ec056b3c9111651d37e4574668b532af1d9
Stain Francis <educaysoft@gmail.com>
2022-12-20 08:22:22 -0500

    recuperacion de certificdo

--- FPDI/certificado.php
+++ FPDI/certificado.php
@@ -1,9 +1,4 @@
 <?php
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Origin: https://educaysoft.org");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: OPTIONS,POST,GET");
-header('Set-Cookie: '.session_name().'='.session_id().'; SameSite=None; Secure');
 use setasign\Fpdi\Fpdi;
 // or for usage with TCPDF:
 // use setasign\Fpdi\Tcpdf\Fpdi;
