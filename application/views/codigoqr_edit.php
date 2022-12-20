d14bb378c2d42243878587bd28f8db622ed7f007
Stain Francis <educaysoft@gmail.com>
2022-12-19 22:47:53 -0500

    mejoras

--- application/views/codigoqr_edit.php
+++ application/views/codigoqr_edit.php
@@ -17,12 +17,42 @@ echo form_input($eys_arrinput); ?></td>
 
 
   <tr>
-      <td>Nombre:</td>
+      <td>filename:</td>
       <td><?php
-$eys_arrinput=array('name'=>'nombre','value'=>$codigoqr['nombre'], "style"=>"width:500px");
+$eys_arrinput=array('name'=>'filename','value'=>$codigoqr['filename'], "style"=>"width:500px");
  echo form_input($eys_arrinput); ?></td>
   </tr>
 
+
+<tr>
+      <td>Tamanio:</td>
+      <td><?php
+$eys_arrinput=array('name'=>'tamanio','value'=>$codigoqr['tamanio'], "style"=>"width:500px");
+ echo form_input($eys_arrinput); ?></td>
+  </tr>
+
+
+<tr>
+      <td>Levele:</td>
+      <td><?php
+$eys_arrinput=array('name'=>'level','value'=>$codigoqr['level'], "style"=>"width:500px");
+ echo form_input($eys_arrinput); ?></td>
+  </tr>
+
+<tr>
+      <td>framesize:</td>
+      <td><?php
+$eys_arrinput=array('name'=>'framesize','value'=>$codigoqr['framesize'], "style"=>"width:500px");
+ echo form_input($eys_arrinput); ?></td>
+  </tr>
+
+
+<tr>
+      <td>Contenido:</td>
+      <td><?php
+$eys_arrinput=array('name'=>'contenido','value'=>$codigoqr['contenido'], "style"=>"width:500px");
+ echo form_input($eys_arrinput); ?></td>
+  </tr>
 
 
 
