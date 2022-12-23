42337a1fce2559b4dca171354ff2dd9bfd5819ec
Stain Francis <educaysoft@gmail.com>
2022-12-22 23:04:05 -0500

    mejoras en asignatura docente

--- application/views/jornadadocente_form.php
+++ application/views/jornadadocente_form.php
@@ -53,7 +53,7 @@
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Duración:</label>
+    <label class="col-md-2 col-form-label"> Duración (minutos):</label>
 	<div class="col-md-10">
 	<?php
 
