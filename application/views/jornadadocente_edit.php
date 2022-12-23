8ec52d5728eaaeb0297a243c95870cbfa425ff42
Stain Francis <educaysoft@gmail.com>
2022-12-22 21:52:37 -0500

    mejoras en asignatura docente

--- application/views/jornadadocente_edit.php
+++ application/views/jornadadocente_edit.php
@@ -2,47 +2,78 @@
 <?php echo form_hidden('idjornadadocente',$jornadadocente['idjornadadocente']) ?>
 <h2> <?php echo $title; ?></h2>
 <hr />
-<table>
- 
-   <tr>
-     <td>Id jornadadocente</td>
-     <td><?php 
 
+
+
+<div class="form-group row">
+<label class="col-md-2 col-form-label"> Id joranada docente:</label>
+<div class="col-md-10">
+<?php
 
 $eys_arrinput=array('name'=>'idjornadadocente','value'=>$jornadadocente['idjornadadocente'],'readonly'=>'true', "style"=>"width:500px");
-echo form_input($eys_arrinput); ?></td>
-  </tr> 
+echo form_input($eys_arrinput); 
+?>
+</div>
+</div>
+
+
+<div class="form-group row">
+<label class="col-md-2 col-form-label">Asignatura docente:</label>
+<div class="col-md-10">
+<?php
+ 
+$options= array('--Select--');
+foreach ($asignaturadocentes as $row){
+	$options[$row->idasignaturadocente]= $row->laasignatura." - ".$row->paralelo;
+}
+ echo form_dropdown("idasignaturadocente",$options, $jornadadocente['idasignaturadocente']); 
+?>
+</div>
+</div>
+
+
+<div class="form-group row">
+<label class="col-md-2 col-form-label">Día de semana:</label>
+<div class="col-md-10">
+<?php
+
+$options= array('--Select--');
+foreach ($diasemanas as $row){
+	$options[$row->iddiasemana]= $row->nombre;
+}
+ echo form_dropdown("iddiasemana",$options, $jornadadocente['iddiasemana']);  
+?>
+</div>
+</div>
+
+<div class="form-group row">
+<label class="col-md-2 col-form-label">Hora inicio:</label>
+<div class="col-md-10">
+<?php
+
+ echo form_input(array("name"=>"horainicio","id"=>"horainicio","value"=>$jornadadocente['horainicio'],"type"=>"time"));  
+
+?>
+</div>
+</div>
+
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Duración (minutos):</label>
+	<div class="col-md-10">
+	<?php
+
+ echo form_input("duracionminutos", array("placeholder"=>"Duración del evento","value"=>$jornadadocente['duracionminutos'],'style'=>'width:500px;')); 
+		?>
+	</div> 
+</div>
+
+
+
 
 
 
  
- <tr>
-<td> Persona:</td>
-<td><?php
-$options= array('--Select--');
-foreach ($docentes as $row){
-	$options[$row->iddocente]= $row->eldocente;
-}
-
- echo form_dropdown("iddocente",$options, $jornadadocente['iddocente']);  ?></td>
-</tr>
-
-<tr>
-<td> Periodo académico:</td>
-<td><?php
-$options= array('--Select--');
-foreach ($periodoacademicos as $row){
-	$options[$row->idperiodoacademico]= $row->nombrecorto;
-}
-
- echo form_dropdown("idperiodoacademico",$options, $jornadadocente['idperiodoacademico']);  ?></td>
-</tr>
-
-
-
- 
- <tr>
- <td colspan="2"> <hr><?php echo form_submit('submit', 'Guardar'); ?> <?php echo anchor('jornadadocente','Atras') ?></td>
- </tr>
-</table>
+ <hr><?php echo form_submit('submit', 'Guardar'); ?> <?php echo anchor('jornadadocente','Atras') ?>
 <?php echo form_close(); ?>
