5e707f0e98f404589000ea64dd2ef2a51897ab6d
Stain Francis <educaysoft@gmail.com>
2022-12-28 22:54:30 -0500

    mejoras en participante

--- application/views/fechaevento_form.php
+++ application/views/fechaevento_form.php
@@ -1,10 +1,3 @@
-<div class="d-flex align-items-center p-3 my-3 text-white bg-purple rounded shadow-sm">
-    <img class="me-3" src="../assets/brand/bootstrap-logo-white.svg" alt="" width="48" height="38">
-    <div class="lh-1">
-      <h1 class="h6 mb-0 text-white lh-1">Bootstrap</h1>
-      <small>Since 2011</small>
-    </div>
-  </div>
 
 <div style="margin-top:5cm;">
 <h2> <?php echo $title; ?> </h2>
