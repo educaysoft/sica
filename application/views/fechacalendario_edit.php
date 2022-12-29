8ddda11eaf3defbb1bbc1cc217c451dc77dd56f0
Stain Francis <educaysoft@gmail.com>
2022-12-29 15:56:38 -0500

    craendo calendario academico

--- application/views/fechacalendario_edit.php
+++ application/views/fechacalendario_edit.php
@@ -2,12 +2,33 @@
 <?php echo form_hidden('idfechacalendario',$fechacalendario['idfechacalendario']) ?>
 <h2> <?php echo $title; ?></h2>
 <hr />
- 
+
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Nombre corto:</label>
+    <label class="col-md-2 col-form-label"> idfechacalendario:</label>
 	<div class="col-md-10">
 		<?php
-$eys_arrinput=array('name'=>'nombrecorto','value'=>$fechacalendario['nombrecorto'], "style"=>"width:500px");
+$eys_arrinput=array('name'=>'idfechacalendario','value'=>$fechacalendario['idfechacalendario'], "style"=>"width:100px");
+ echo form_input($eys_arrinput);
+		?>
+	</div> 
+</div>
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Fecha :</label>
+	<div class="col-md-10">
+
+      <?php echo form_input( array("name"=>'fechacalendario',"id"=>'fechacalendario',"value"=>$fechacalendario['fechacalendario'],'type'=>'date','placeholder'=>'fecha calendaria'));
+
+		?>
+	</div> 
+</div>
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Actividad:</label>
+	<div class="col-md-10">
+		<?php
+$eys_arrinput=array('name'=>'actividad','value'=>$fechacalendario['actividad'], "style"=>"width:500px");
  echo form_input($eys_arrinput);
 		?>
 	</div> 
@@ -15,10 +36,10 @@ $eys_arrinput=array('name'=>'nombrecorto','value'=>$fechacalendario['nombrecorto
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Nombre largo:</label>
+    <label class="col-md-2 col-form-label"> detalle:</label>
 	<div class="col-md-10">
 		<?php
-$eys_arrinput=array('name'=>'nombrelargo','value'=>$fechacalendario['nombrelargo'], "style"=>"width:500px");
+$eys_arrinput=array('name'=>'detalle','value'=>$fechacalendario['detalle'], "style"=>"width:500px");
  echo form_input($eys_arrinput);
 		?>
 	</div> 
@@ -27,70 +48,33 @@ $eys_arrinput=array('name'=>'nombrelargo','value'=>$fechacalendario['nombrelargo
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Unidad silabo:</label>
+    <label class="col-md-2 col-form-label"> Calendario académico:</label>
 	<div class="col-md-10">
 		<?php
 
 $options= array('--Select--');
-foreach ($unidadsilabos as $row){
-	$options[$row->idunidadsilabo]= $row->nombre;
+foreach ($calenarioacademicos as $row){
+	$options[$row->idcalenarioacademico]= $row->nombre;
 }
 
- echo form_dropdown("idunidadsilabo",$options, $fechacalendario['idunidadsilabo']);  
+ echo form_dropdown("idcalendarioacademico",$options, $fechacalendario['idcalendarioacademico']);  
 		?>
 	</div> 
 </div>
 
 
 
-<div class="form-group row">
-    <label class="col-md-2 col-form-label"> Fecha impartida:</label>
-	<div class="col-md-10">
-
-      <?php echo form_input( array("name"=>'fechaimpartida',"id"=>'fechaimpartida',"value"=>$fechacalendario['fechaimpartida'],'type'=>'date','placeholder'=>'fecha impartida'));
-
-		?>
-	</div> 
-</div>
-
-
-<div class="form-group row">
-    <label class="col-md-2 col-form-label"> Duración (minutos):</label>
-	<div class="col-md-10">
-		<?php
-$eys_arrinput=array('name'=>'duracionminutos','value'=>$fechacalendario['duracionminutos'], "style"=>"width:100px");
- echo form_input($eys_arrinput);
-		?>
-	</div> 
-</div>
-
-
-<div class="form-group row">
-    <label class="col-md-2 col-form-label"> Número de sesión:</label>
-	<div class="col-md-10">
-		<?php
-$eys_arrinput=array('name'=>'numerosesion','value'=>$fechacalendario['numerosesion'], "style"=>"width:50px");
- echo form_input($eys_arrinput);
-		?>
-	</div> 
-</div>
 
 
 
-<div class="form-group row">
-    <label class="col-md-2 col-form-label"> Video tutorial:</label>
-	<div class="col-md-10">
-		<?php
 
-$options= array('--Select--');
-foreach ($videotutoriales as $row){
-	$options[$row->idvideotutorial]= $row->nombre;
-}
 
- echo form_dropdown("idvideotutorial",$options, $fechacalendario['idvideotutorial']);  
-		?>
-	</div> 
-</div>
+
+
+
+
+
+
 
 
  
