022e07dcd756df575f2fcafda36919798877736a
Stain Francis <educaysoft@gmail.com>
2023-01-01 18:24:17 -0500

    iniciales en institucion

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -347,7 +347,7 @@ if ($result == TRUE) {
    			date_default_timezone_set('America/Guayaquil');
     			$date = date("Y-m-d");
     			$hora= date("H:i:s");
-    			$asistencia=array('idpersona'=>$result[0]->idpersona,'idevento'=>$result[0]->idevento,'fecha'=>$date,'hora'=>$hora,'idtipoasistencia'=>1,'comentario'=>"INGRESO AL SISTEMA");
+    			$asistencia=array('idpersona'=>$result[0]->idpersona,'fecha'=>$date,'hora'=>$hora,'idtipoasistencia'=>1,'comentario'=>"INGRESO AL SISTEMA");
     			$idasistencia=$this->asistencia_model->save($asistencia);
     		//	print_r($asistencia);
     		//	echo $idasistencia;
