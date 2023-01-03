1f60ac75dd9cbecd780e2dd11ce96fbee16368b4
Stain Francis <educaysoft@gmail.com>
2023-01-03 02:35:02 -0500

    optimizacion del modulo de evento

--- application/views/unidadsilabo_record.php
+++ application/views/unidadsilabo_record.php
@@ -100,7 +100,7 @@ echo form_input('idsilabo',$options[$unidadsilabo['idsilabo']],array("disabled"=
  
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Temas: </label>
+    <label class="col-md-2 col-form-label"> <?php echo anchor('tema/add', 'Tema:'); ?> </label>
 
 	<div class="col-md-10">
 	<div class="row justify-content-left">
