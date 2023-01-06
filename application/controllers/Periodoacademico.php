925b4c8630e61473de54c198567237d408bddfb9
ElProfe <educaysoft@gmail.com>
2023-01-05 19:50:05 -0500

    repoprate se sesionevento

--- application/controllers/Periodoacademico.php
+++ application/controllers/Periodoacademico.php
@@ -106,7 +106,7 @@ public function edit()
 	 	$data0 = $this->periodoacademico_model->lista_periodoacademicos();
 		$data=array();
 		foreach($data0->result() as $r){
-			$data[]=array($r->idperiodoacademico,$r->nombre,
+			$data[]=array($r->idperiodoacademico,$r->nombrecorto,
 				$r->href='<a href="javascript:void(0);" class="btn btn-info btn-sm item_ver"  data-idperiodoacademico="'.$r->idperiodoacademico.'">Ver</a>');
 		}	
 		$output=array( "draw"=>$draw,
