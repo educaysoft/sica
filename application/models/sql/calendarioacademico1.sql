6314d55d3108b5ac0d5828fff59022132ffee734
ElProfe <educaysoft@gmail.com>
2023-01-07 09:19:33 -0500

    docuemnto

--- application/models/sql/calendarioacademico1.sql
+++ application/models/sql/calendarioacademico1.sql
@@ -1,4 +1,4 @@
 
 use educayso_facae;
 drop view calendarioacademico1;
-create view calendarioacademico1 as select calendarioacademico.idcalendarioacademico,calendarioacademico.idperiodoacademico,concat(departamento.iniciales," - ",periodoacademico.nombrecorto) as elcalendarioacademico from calendarioacademico, periodoacademico,departamento where calendarioacademico.idperiodoacademico=periodoacademico.idperiodoacademico and calendarioacademico.iddepartamento=departamento.iddepartamento ;
+create view calendarioacademico1 as select calendarioacademico.idcalendarioacademico,calendarioacademico.idperiodoacademico,calendarioacademico.iddepartamento,concat(departamento.iniciales," - ",periodoacademico.nombrecorto) as elcalendarioacademico from calendarioacademico, periodoacademico,departamento where calendarioacademico.idperiodoacademico=periodoacademico.idperiodoacademico and calendarioacademico.iddepartamento=departamento.iddepartamento ;
