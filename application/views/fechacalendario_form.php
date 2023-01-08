c76252101185024e347e1a02ebab23b24bf86efb
ElProfe <educaysoft@gmail.com>
2023-01-08 09:17:50 -0500

    hito en fecha calendario

--- application/views/fechacalendario_form.php
+++ application/views/fechacalendario_form.php
@@ -53,7 +53,14 @@ foreach ($calendarioacademicos as $row){
 
 
 
-
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Hito:</label>
+	<div class="col-md-10">
+		<?php
+ echo form_input("hito","", array("placeholder"=>"Hito (0, 1)",'style'=>'width:500px;'));
+		?>
+	</div> 
+</div>
 
 
 
