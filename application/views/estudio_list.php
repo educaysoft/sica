5cd8b341b2f26f012b5368ba9f0bec305604bcf2
ElProfe <educaysoft@gmail.com>
2023-01-09 07:52:16 -0500

    hito en fecha calendario

--- application/views/estudio_list.php
+++ application/views/estudio_list.php
@@ -27,6 +27,13 @@ body {font-family: Arial, Helvetica, sans-serif;}
 </style>
 
 
+<div id="eys-nav-i">
+	<ul>
+		<li> <?php echo anchor('estudio', 'Home'); ?></li>
+	</ul>
+</div>
+
+
 <div class="row justify-content-center">
       <!-- Page Heading -->
  <div class="row">
