8e97ca58ddeca9f5b8bbc428416537db746a1554
ElProfe <educaysoft@gmail.com>
2023-01-11 01:37:13 -0500

    mejorando modulo de articulos parte de nuevo

--- application/views/prestamoarticulo_edit.php
+++ application/views/prestamoarticulo_edit.php
@@ -15,91 +15,83 @@ echo form_input($eys_arrinput); ?></td>
 
 
 <tr>
-<td> Evento:</td>
+<td> Articulo:</td>
 <td><?php
 $options= array('--Select--');
-foreach ($eventos as $row){
-	$options[$row->idevento]= $row->titulo;
+foreach ($articulos as $row){
+	$options[$row->idarticulo]= $row->nombre;
 }
 
- echo form_dropdown("idevento",$options, $prestamoarticulo['idevento']);  ?></td>
+ echo form_dropdown("idarticulo",$options, $prestamoarticulo['idarticulo']);  ?></td>
 </tr>
 
- 
+
+<tr>
+<td> Persona:</td>
+<td><?php
+$options= array('--Select--');
+foreach ($personas as $row){
+	$options[$row->idpersona]=$row->apellidos."  ".$row->nombres;
+}
+
+ echo form_dropdown("idpersona",$options, $prestamoarticulo['idpersona']);  ?></td>
+</tr>
+
+
+
+
  <tr>
-      <td>Fecha :</td>
-      <td><?php echo form_input( array("name"=>'fecha',"id"=>'fecha',"value"=>$prestamoarticulo['fecha'],'type'=>'date','placeholder'=>'fecha')); ?></td>
+      <td>Fecha prestamo :</td>
+      <td><?php echo form_input( array("name"=>'fechaprestamo',"id"=>'fechaprestamo',"value"=>$prestamoarticulo['fechaprestamo'],'type'=>'date','placeholder'=>'fecha')); ?></td>
   </tr>
  
 
 <tr>
-<td> Tema:</td>
-<td><?php
-$options= array('--Select--');
-foreach ($temas as $row){
-	$options[$row->idtema]="Unidad: ".$row->unidad." - Sesion: ".$row->numerosesion." - ".$row->nombrecorto;
-}
-
- echo form_dropdown("idtema",$options, $prestamoarticulo['idtema']);  ?></td>
+     <td>Hora prestamo:</td>
+     <td><?php 
+     $eys_arrinput=array('name'=>'horaprestamo','id'=>'horaprestamo',"type"=>"time",'value'=>$prestamoarticulo['horaprestamo'], "style"=>"width:500px");
+     echo form_input($eys_arrinput); ?></td>
 </tr>
 
 
 
 
+ <tr>
+      <td>Fecha devolucion :</td>
+      <td><?php echo form_input( array("name"=>'fechadevolucion',"id"=>'fechadevolucion',"value"=>$prestamoarticulo['fechadevolucion'],'type'=>'date','placeholder'=>'fecha')); ?></td>
+  </tr>
+ 
+
+<tr>
+     <td>Hora devolucion:</td>
+     <td><?php 
+     $eys_arrinput=array('name'=>'horadevolucion','id'=>'horadevolucion',"type"=>"time",'value'=>$prestamoarticulo['horadevolucion'], "style"=>"width:500px");
+     echo form_input($eys_arrinput); ?></td>
+</tr>
+
+
 
 
 <tr>
-  <td>Tema a tratar(nombre largo):</td>
+  <td>Detalle:</td>
   <td><?php 
   
-$textarea_options = array('class' => 'form-control','rows' => '4',   'cols' => '20', 'style'=> 'width:50%;height:100px;', "placeholder"=>"tema","id" =>"tema");    
-echo form_textarea('tema',$prestamoarticulo['tema'],$textarea_options ); ?></td>
+$textarea_options = array('class' => 'form-control','rows' => '4',   'cols' => '20', 'style'=> 'width:50%;height:100px;', "placeholder"=>"detalle","id" =>"detalle");    
+echo form_textarea('detalle',$prestamoarticulo['detalle'],$textarea_options ); ?></td>
  </tr>
 
 
-<tr>
-  <td>Tema a tratar(nombre corto):</td>
-  <td><?php 
-  
-$textarea_options = array('class' => 'form-control','rows' => '4',   'cols' => '20', 'style'=> 'width:50%;height:100px;', "placeholder"=>"tema","id" =>"temacorto");    
-echo form_textarea('temacorto',$prestamoarticulo['temacorto'],$textarea_options ); ?></td>
- </tr>
-
-
-<tr>
-     <td>Ponderacion:</td>
-     <td><?php 
-     $eys_arrinput=array('name'=>'ponderacion','value'=>$prestamoarticulo['ponderacion'], "style"=>"width:500px");
-     echo form_input($eys_arrinput); ?></td>
-</tr>
-
-
-<tr>
-     <td>Hora inicio:</td>
-     <td><?php 
-     $eys_arrinput=array('name'=>'horainicio','id'=>'horainicio',"type"=>"time",'value'=>$prestamoarticulo['horainicio'], "style"=>"width:500px");
-     echo form_input($eys_arrinput); ?></td>
-</tr>
-
-<tr>
-     <td>Hora fin:</td>
-     <td><?php 
-     $eys_arrinput=array('name'=>'horafin','id'=>'horafin',"type"=>"time",'value'=>$prestamoarticulo['horafin'], "style"=>"width:500px");
-     echo form_input($eys_arrinput); ?></td>
-</tr>
 
 
 
-<tr>
-<td> Modo de evaluacion:</td>
-<td><?php
-$options= array('--Select--');
-foreach ($modoevaluacions as $row){
-	$options[$row->idmodoevaluacion]= $row->nombre;
-}
 
- echo form_dropdown("idmodoevaluacion",$options, $prestamoarticulo['idmodoevaluacion']);  ?></td>
-</tr>
+
+
+
+
+
+
+
 
 
 
