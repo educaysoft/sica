36145b48eec4f551fcdb1796be531f2c55600da6
ElProfe <educaysoft@gmail.com>
2023-01-10 20:20:52 -0500

    mejorando modulo de evento ventana flotante para ingresa las fechas de sesion

--- application/views/docente_list_pdf.php
+++ application/views/docente_list_pdf.php
@@ -22,9 +22,9 @@
 
 
 
-	$pdf->SetFont('Arial','',14);
+	$pdf->SetFont('Arial','',12);
 	$pdf->SetTextColor(0, 0,0);
-    	$pdf->Text(20,40,"Docente:  ". $estudios[0]->elestudiante); 
+    	$pdf->Text(20,40,"Docente:  ".$estudios[0]->cedula." - " $estudios[0]->lapersona); 
 
 	$pdf->SetFillColor(232,232,232);
 	$pdf->SetFont('Arial','B',8);
