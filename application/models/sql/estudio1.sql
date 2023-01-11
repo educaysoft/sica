f612ca19500aab733e0e3fd0b0df6cba1ed2e519
ElProfe <educaysoft@gmail.com>
2023-01-10 20:25:14 -0500

    mejorando modulo de evento ventana flotante para ingresa las fechas de sesion

--- application/models/sql/estudio1.sql
+++ application/models/sql/estudio1.sql
@@ -1,4 +1,4 @@
 
 use educayso_facae;
-
+drop view estudio1;
 create view estudio1 as select estudio.idestudio,persona.idpersona,persona.cedula,concat(COALESCE(persona.apellidos,''),"  ",COALESCE(persona.nombres,'')) as lapersona, estudio.idinstitucion as idinstitucion, institucion.nombre as lainstitucion, estudio.nivel, estudio.titulo from estudio,persona,institucion  where estudio.idpersona=persona.idpersona and estudio.idinstitucion=institucion.idinstitucion ;
