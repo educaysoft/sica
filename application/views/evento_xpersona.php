8ad9560082b5f80d95c0024f46a06f9f2158870a
ElProfe <educaysoft@gmail.com>
2023-01-12 20:11:22 -0500

    mejorando una etiquete en el modulo Evento.php

--- application/views/evento_xpersona.php
+++ application/views/evento_xpersona.php
@@ -133,7 +133,7 @@ function filtra_evento()
 {
 //       var idevento_estado = $('select[name=idevento_estado]').val();
 
-//	var idpersona = document.getElementById("filtro").innerHTML;
+	var idpersona = document.getElementById("filtro").innerHTML;
        
 var mytabla= $('#mydatac').DataTable({destroy: true,"ajax": {url: '<?php echo site_url('evento/persona_data')?>', type: 'GET',data:{idpersona:idpersona}},});
 }
