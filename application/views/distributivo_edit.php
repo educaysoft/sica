8b4eb67f4c300e7730df9d9af739300c889859f5
Stain Francis <educaysoft@gmail.com>
2023-01-14 19:15:13 -0500

    mejoras al modulo de distributivo

--- application/views/distributivo_edit.php
+++ application/views/distributivo_edit.php
@@ -11,7 +11,7 @@
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Departamento/carrera:</label>
+    <label class="col-md-2 col-form-label"> Departamento o carrera:</label>
 	<div class="col-md-10">
 		<?php
 
