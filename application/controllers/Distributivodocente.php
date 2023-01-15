1e65202661e35eafae70bddad209107714fd535d
Stain Francis <educaysoft@gmail.com>
2023-01-14 22:42:23 -0500

    mejoras al modulo de distributivo

--- application/controllers/Distributivodocente.php
+++ application/controllers/Distributivodocente.php
@@ -15,7 +15,7 @@ public function index(){
 
   	if(isset($this->session->userdata['logged_in'])){
 			
-  	$data['distributivodocente']=$this->distributivodocente_model->distributivodocentes($this->uri->segment(3))->row_array();
+  	$data['distributivodocente']=$this->distributivodocente_model->distributivodocente($this->uri->segment(3))->row_array();
   	$data['docentes']= $this->docente_model->lista_docentesA()->result();
   	$data['distributivos']= $this->distributivo_model->lista_distributivos1(0)->result();
   		$data['departamentos']= $this->departamento_model->lista_departamentos()->result();
