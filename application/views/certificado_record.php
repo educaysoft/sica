1b2221af4647869661079018505daab6a60f060d
ElProfe <educaysoft@gmail.com>
2023-01-18 10:33:04 -0500

    mejorando el modulo de certificado

--- application/views/certificado_record.php
+++ application/views/certificado_record.php
@@ -95,7 +95,7 @@ if(isset($certificado))
 
 
 <div class="form-group row">
-<label class="col-md-2 col-form-label">Evento:</label>
+<label class="col-md-2 col-form-label"><?php echo anchor('evento/actual/'.$certificado['idevento'], 'Evento:'); ?></label>
 <div class="col-md-10">
 <?php
 
