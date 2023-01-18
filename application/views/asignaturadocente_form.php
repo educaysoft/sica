53ace878bdf54aadd57dc6b7c6f73d094401da41
ElProfe <educaysoft@gmail.com>
2023-01-18 14:33:00 -0500

    mejoras

--- application/views/asignaturadocente_form.php
+++ application/views/asignaturadocente_form.php
@@ -65,7 +65,7 @@ foreach ($mallas as $row){
 
 
 <div class="form-group row">
-<label class="col-md-2 col-form-label">Paralelo:</label>
+<label class="col-md-2 col-form-label">Asignatura:</label>
 <div class="col-md-10">
     <div class="form-group">
 
