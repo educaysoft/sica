cf987baa1c91469a9a32e31d2bbc53fa5be741d7
Stain Francis <educaysoft@gmail.com>
2023-01-18 03:44:25 -0500

    mejoraes ubiciacai

--- application/models/sql/ubicacionarticulo.sql
+++ application/models/sql/ubicacionarticulo.sql
@@ -1,9 +1,11 @@
 use educayso_facae;
+drop table ubicacionarticulo;
 CREATE TABLE `ubicacionarticulo` (
   `idubicacionarticulo` int(11) NOT NULL auto_increment primary key,
   `idarticulo` int(11) DEFAULT NULL,
   `idpersona` int(11) DEFAULT NULL,
   `fecha` date DEFAULT NULL,
-  `iddepartamento` int(11) DEFAULT NULL
+  `idunidaod` int(11) DEFAULT NULL,
+  `detalle` text DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
