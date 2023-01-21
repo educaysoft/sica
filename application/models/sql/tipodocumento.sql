5366a8422053f3caaaa8a3cfe457b5e710391b7d
Stain Francis <educaysoft@gmail.com>
2023-01-21 09:22:56 -0500

    mejoras para ces

---
+++ application/models/sql/tipodocumento.sql
@@ -0,0 +1,5 @@
+use educayso_facae;
+
+create table tipodocumento(idtipodocumento int(11) not null auto_increment primary key, nombre varchar(100));
+
+
---
+++ application/models/sql/tipodocumento1.sql
@@ -0,0 +1,2 @@
+use educayso_facae;
+create view tipodocumento1 as select idtipodocumento, nombre from tipodocumento;
