7e369fe43f11c8c2484c584be2a125aecd35dcf8
Stain Francis <educaysoft@gmail.com>
2023-01-21 22:38:25 -0500

    modulo pais

--- application/views/persona_edit.php
+++ application/views/persona_edit.php
@@ -2,68 +2,104 @@
 <?php echo form_hidden('idpersona',$persona['idpersona']) ?>
 <h2> <?php echo $title; ?></h2>
 <hr />
-<table>
- 
-   <tr>
-     <td>Id persona</td>
-     <td><?php 
 
 
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Id persona:</label>
+	<div class="col-md-10">
+	<?php
 $eys_arrinput=array('name'=>'idpersona','value'=>$persona['idpersona'],'readonly'=>'true', "style"=>"width:500px");
-echo form_input($eys_arrinput); ?></td>
-  </tr> 
+echo form_input($eys_arrinput); 
+	?>
+	</div> 
+</div> 
 
 
-<tr>
-      <td>Cédula:</td>
-      <td><?php
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Cedula:</label>
+	<div class="col-md-10">
+	<?php
        $eys_arrinput=array('name'=>'cedula','value'=>$persona['cedula'], "style"=>"width:500px");
-      echo form_input($eys_arrinput); ?></td>
-</tr>
+      echo form_input($eys_arrinput); 
+	?>
+	</div> 
+</div> 
 
 
-<tr>
-      <td>Nombres:</td>
-      <td><?php
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Nombres:</label>
+	<div class="col-md-10">
+	<?php
        $eys_arrinput=array('name'=>'nombres','value'=>$persona['nombres'], "style"=>"width:500px");
-      echo form_input($eys_arrinput); ?></td>
-</tr>
+      echo form_input($eys_arrinput); 
+	?>
+	</div> 
+</div> 
 
-<tr>
-      <td>Apellidos:</td>
-      <td><?php
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Apellidos:</label>
+	<div class="col-md-10">
+	<?php
        $eys_arrinput=array('name'=>'apellidos','value'=>$persona['apellidos'], "style"=>"width:500px");
-      echo form_input($eys_arrinput); ?></td>
-</tr>
- 
-
- <tr>
-      <td>Fecha Nacimiento:</td>
-      <td><?php echo form_input( array("name"=>'fechanacimiento',"id"=>'fechanacimiento',"value"=>$persona['fechanacimiento'],'type'=>'date','placeholder'=>'fechanacimiento')); ?></td>
-  </tr>
+      echo form_input($eys_arrinput); 
+	?>
+	</div> 
+</div> 
 
 
-<tr>
-      <td>Archivo de foto:</td>
-      <td><?php
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Fecha nacimiento:</label>
+	<div class="col-md-10">
+	<?php
+       echo form_input( array("name"=>'fechanacimiento',"id"=>'fechanacimiento',"value"=>$persona['fechanacimiento'],'type'=>'date','placeholder'=>'fechanacimiento')); 
+	?>
+	</div> 
+</div> 
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Archivo de foto:</label>
+	<div class="col-md-10">
+	<?php
        $eys_arrinput=array('name'=>'foto','value'=>$persona['foto'], "style"=>"width:500px");
-      echo form_input($eys_arrinput); ?></td>
-</tr>
- 
- <tr>
-<td> Genero:</td>
-<td><?php
+      echo form_input($eys_arrinput); 
+	?>
+	</div> 
+</div> 
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Sexo:</label>
+	<div class="col-md-10">
+	<?php
 $options= array('--Select--');
 foreach ($sexos as $row){
 	$options[$row->idsexo]= $row->nombre;
 }
+ echo form_dropdown("idsexo",$options, $persona['idsexo']);  
+	?>
+	</div> 
+</div> 
 
- echo form_dropdown("idsexo",$options, $persona['idsexo']);  ?></td>
-</tr>
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> <?php echo anchor('pais/add', 'Pais resicencia:'); ?>:</label>
+	<div class="col-md-10">
+	<?php
+ 	$options = array();
+  	foreach ($paispersonas as $row){
+		$options[$row->idpaispersona]=$row->elpais;
+	}
+ echo form_multiselect('paispersona[]',$options,(array)set_value('idpaispersona', ''), array('style'=>'width:500px')); 
+
+	?>
+	</div> 
+</div>
 
 
 
- 
+<table> 
  <tr>
  <td colspan="2"> <hr><?php echo form_submit('submit', 'Guardar'); ?> <?php echo anchor('persona','Atras') ?></td>
  </tr>
