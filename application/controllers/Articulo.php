ea4026eb1d1cc57ee4791c46923020dec34eaf6f
Stain Francis <educaysoft@gmail.com>
2023-01-23 14:21:39 -0500

    articulo

--- application/controllers/Articulo.php
+++ application/controllers/Articulo.php
@@ -7,6 +7,7 @@ class Articulo extends CI_Controller{
       $this->load->model('articulo_model');
       $this->load->model('prestamoarticulo_model');
   	  $this->load->model('institucion_model');
+  	  $this->load->model('ubicacion_model');
 }
 
 public function index(){
