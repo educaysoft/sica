f2447cb848c900fecf5bce68bccc299d2b6432c5
Stain Francis <educaysoft@gmail.com>
2023-01-23 23:09:59 -0500

    mejoras a documento

--- application/views/documentoportafolio_list.php
+++ application/views/documentoportafolio_list.php
@@ -51,8 +51,9 @@ body {font-family: Arial, Helvetica, sans-serif;}
  <thead>
  <tr>
  <th>ID</th>
- <th>documentoportafolio</th>
- <th>Periodo</th>
+ <th>iddocumento</th>
+ <th>Asunto</th>
+ <th>Fecha</th>
  <th style="text-align: right;">Actions</th>
  </tr>
  </thead>
