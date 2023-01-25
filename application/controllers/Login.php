897fc39d0a5c2c2e0a1fe45542efeaf5cea86fe9
Stain Francis <educaysoft@gmail.com>
2023-01-25 11:28:29 -0500

    errores en login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -23,7 +23,7 @@ parent::__construct();
 // Show login page
 public function index() {
 	
-	 $data['eventos']= $this->evento_model->lista_eventos_open()->result();
+	 $data['eventos']= $this->evento_model->lista_eventos_open(0)->result();
 	 $this->load->view('template/page_header.php');
 	 $this->load->view('login_form',$data);
 	 $this->load->view('template/page_footer.php');
