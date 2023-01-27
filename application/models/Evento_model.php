6535b18d2d25a88c94ecc5bb829016ee400f648a
Stain Francis <educaysoft@gmail.com>
2023-01-27 14:36:33 -0500

    mejoras al modulo de acceso

--- application/models/Evento_model.php
+++ application/models/Evento_model.php
@@ -173,12 +173,6 @@ class Evento_model extends CI_model {
 
  
 
-  //Retorna solamente un registro de el id pasado como parame
- 	function evento($id){
-	$evento = $this->db->query('select * from evento where idevento="'. $id.'" order by idevento');
- 		return $evento;
- 	}
-
 
 
 
