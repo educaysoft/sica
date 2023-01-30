d4169770ab97f8bebc5f3989058d7498ec2c7565
Stain Francis <educaysoft@gmail.com>
2023-01-29 19:33:20 -0500

    mejoras en distributivos docente se crea el reporte pdf

--- application/controllers/Distributivodocente.php
+++ application/controllers/Distributivodocente.php
@@ -10,6 +10,7 @@ class Distributivodocente extends CI_Controller{
   	  $this->load->model('asignaturadocente_model');
   	  $this->load->model('departamento_model');
   	  $this->load->model('evento_model');
+  	  $this->load->model('jornadadocente_model');
 }
 
 public function index(){
