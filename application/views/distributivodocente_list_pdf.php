c321bd4814910bb4727c8a76d159941727a47c81
Stain Francis <educaysoft@gmail.com>
2023-01-29 19:45:53 -0500

    mejoras en distributivos docente se crea el reporte pdf

--- application/views/distributivodocente_list_pdf.php
+++ application/views/distributivodocente_list_pdf.php
@@ -23,7 +23,7 @@
 
 	$pdf->SetFont('Arial','',12);
 	$pdf->SetTextColor(0, 0,0);
-    	$pdf->Text(20,40,"Distributivodocente:  ".$jornadadocente[O]->iddistributivodocente); 
+    	$pdf->Text(20,40,"Distributivodocente:  ".$jornadadocente[0]->iddistributivodocente); 
 
 	$pdf->SetFillColor(232,232,232);
 	$pdf->SetFont('Arial','B',8);
