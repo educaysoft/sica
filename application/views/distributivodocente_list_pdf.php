b6eba908df1d8dd1d36aab4db45e8c4b3bfe3587
Stain Francis <educaysoft@gmail.com>
2023-02-03 07:51:56 -0500

    modulo mejoras

--- application/views/distributivodocente_list_pdf.php
+++ application/views/distributivodocente_list_pdf.php
@@ -68,7 +68,7 @@ $cell_width = 30;  //define cell width
 $cell_height=10;    //define cell height
 	
 		    if(isset($dia['Lunes'])){
-		    	$pdf->MultiCell($cell_width,5,utf8_decode($dia['Lunes']),1);	    
+		    	$pdf->MultiCell($cell_width,10,utf8_decode($dia['Lunes']),1);	    
 	 	    	$current_x+=$cell_width;
 			$pdf->SetXY($current_x, $current_y);   
 		    }else{
