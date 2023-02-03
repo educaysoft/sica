70229bb74559844e9004567efe11dbba0fb2218b
Stain Francis <educaysoft@gmail.com>
2023-02-03 07:59:14 -0500

    modulo mejoras

--- application/views/distributivodocente_list_pdf.php
+++ application/views/distributivodocente_list_pdf.php
@@ -59,7 +59,7 @@
 
 	foreach ($horario2 as $hora=>$dia){  //Recorre todas la participaciones realizadas por los participantes
 		    $i=$i+1;
-		    $pdf->Cell(12,10,$hora,1,0,'R',0); 
+		    $pdf->Cell(12,10,$hora,1,0,'R',1); 
 $start_x=$pdf->GetX(); //initial x (start of column position)
 $current_y = $pdf->GetY();
 $current_x = $pdf->GetX();
@@ -68,7 +68,7 @@ $cell_width = 30;  //define cell width
 $cell_height=10;    //define cell height
 	
 		    if(isset($dia['Lunes'])){
-		    	$pdf->vcell($cell_width,10,utf8_decode($dia['Lunes']),1);	    
+		    	$pdf->MultiCell($cell_width,5,utf8_decode($dia['Lunes']),1);	    
 	 	    	$current_x+=$cell_width;
 			$pdf->SetXY($current_x, $current_y);   
 		    }else{
