573903695aed6f23eefd6583a6038bcf6fc24d6c
Stain Francis <educaysoft@gmail.com>
2023-02-05 16:29:35 -0500

    numero de horas a asignatura del docente

--- application/views/distributivodocente_record.php
+++ application/views/distributivodocente_record.php
@@ -192,7 +192,6 @@ echo form_input('eldocente',$options[$distributivodocente['iddocente']],array("i
 $(document).ready(function(){
 	var iddistributivodocente=document.getElementById("iddistributivodocente").value;
 	var iddocente=document.getElementById("iddocente").value;
-	alert(iddocente);
 	var mytablaf= $('#mydatac').DataTable({"ajax": {url: '<?php echo site_url('distributivodocente/asignaturadocente_data')?>', type: 'GET',data:{iddistributivodocente:iddistributivodocente}},});
 
 
