b683301d5a85aa6830a3488857a561a19f8d89c4
Stain Francis <educaysoft@gmail.com>
2023-02-05 21:42:17 -0500

    numero de horas a asignatura del docente

--- application/controllers/Sesionevento.php
+++ application/controllers/Sesionevento.php
@@ -88,7 +88,7 @@ class Sesionevento extends CI_Controller{
   		$data['unidadsilabos']= $this->unidadsilabo_model->unidadsilaboss($data['evento']['idsilabo'])->result();
 		$data['modoevaluacions']= $this->modoevaluacion_model->lista_modoevaluacions()->result();
   		$data['silabo']= $this->silabo_model->silabo($data['evento']['idsilabo'])->first_row('array');
-		$data['distributivodocente']=$this->distributivodocente_model->distributivo_pado($data['silabo']['idperiodoacademico'],$data['silabo']['iddocente'])->first_row('array');	
+		$data['distributivodocente']=$this->distributivodocente_model->distributivodocente_pado($data['silabo']['idperiodoacademico'],$data['silabo']['iddocente'])->first_row('array');	
 			$data['asignaturadocente']= $this->asignaturadocente_model->lista_asignaturadocentesA($data['distributivodocente']['iddistributivodocente'])->result();
 			$data['jornadadocente']= $this->jornadadocente_model->jornadadocente($data['asignaturadocente']['idasignaturadocente'])->result();
 	   }
