1af38c250dbf645887b548285054441cb6b9b56f
Stain Francis <educaysoft@gmail.com>
2023-02-10 13:54:50 -0500

    mejoras a evenot

--- application/models/Evento_model.php
+++ application/models/Evento_model.php
@@ -138,7 +138,9 @@ class Evento_model extends CI_model {
  	function lista_eventoP($id){
 
 		$this->db->where("idevento_estado=2 or idevento_estado=3");  //SOLO ESTADO INSCRIPCION OR EN EJECUCION
- 		$evento = $this->db->query('select * from eventoP where idevento="'. $id.'" order by elparticipante');
+		$this->db->where("idevento",$id); 
+
+		$evento=$this->db->order_by("elparticipante")->get('eventoP');
  		return $evento;
  	}
 
