9af54e0392d0d85ce38e811091abb7d6b7796232
Stain Francis <educaysoft@gmail.com>
2023-02-11 09:22:00 -0500

    mejoras a evenot

--- application/views/sesionevento_form.php
+++ application/views/sesionevento_form.php
@@ -33,7 +33,7 @@ foreach ($eventos as $row){
 <div class="col-md-10">
 <?php
 
- $dias = array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado');
+ $dias = array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado');
    date_default_timezone_set('America/Guayaquil');
     $fecha = date("Y-m-d");
     $horai= date("H:i:s");
