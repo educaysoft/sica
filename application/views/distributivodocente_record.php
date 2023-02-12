0ebe4424a0639a17516ab2733c11abf0b9bc9c0a
Stain Francis <educaysoft@gmail.com>
2023-02-12 13:05:56 -0500

    Ubicacion docente

--- application/views/distributivodocente_record.php
+++ application/views/distributivodocente_record.php
@@ -86,7 +86,15 @@ echo form_input(array('name'=>'iddocente',"type"=>"hidden","value"=>$distributiv
 
 
 <div class="form-group row">
- <div class="row">
+  
+
+
+	<div class="col-md-10">
+	<div class="row justify-content-left">
+      	<!-- Page Heading -->
+ 	<div class="row">
+  	<div class="col-12">
+<div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
             <h2>Codeigniter 3 CRUD Example from scratch</h2>
@@ -95,14 +103,9 @@ echo form_input(array('name'=>'iddocente',"type"=>"hidden","value"=>$distributiv
             <a class="btn btn-success" href="<?php echo base_url('itemCRUD/create') ?>"> Create New Item</a>
         </div>
     </div>
-</div> 
+</div>
 
 
-	<div class="col-md-10">
-	<div class="row justify-content-left">
-      	<!-- Page Heading -->
- 	<div class="row">
-  	<div class="col-12">
 	<table class="table table-striped table-bordered table-hover" id="mydatac">
 	 <thead>
 	 <tr>
