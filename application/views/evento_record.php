c32d5b153502071658c875c31313cf2282f46433
Stain Francis <educaysoft@gmail.com>
2023-02-12 23:21:18 -0500

    Evento mejoras

--- application/views/evento_record.php
+++ application/views/evento_record.php
@@ -316,12 +316,25 @@ echo form_input($arrdatos) ?>
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Sesiones ( <?php echo anchor('sesionevento/add/'.$evento['idevento'], 'New'); ?>):</label>
 	<div class="col-md-10">
 	<div class="row justify-content-left">
       	<!-- Page Heading -->
  	<div class="row">
-  	<div class="col-12">
+  	<div class="col-12" style="border:solid;">
+
+<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <b>Sesiones dictadas: </b>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-success" href="<?php echo base_url('sesionevento/add/'.$evento['idevento']) ?>">Nueva sesion</a>
+        </div>
+    </div>
+</div>
+
+
+
 	<table class="table table-striped table-bordered table-hover" id="mydatac">
 	 <thead>
 	 <tr>
@@ -349,14 +362,29 @@ echo form_input($arrdatos) ?>
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> <?php echo anchor('participante/add/'.$evento['idevento'], 'Participante'); ?>:</label>
 	<div class="col-md-10">
  
 <div class="row justify-content-left">
       <!-- Page Heading -->
  <div class="row">
-  <div class="col-12">
-             
+
+  	<div class="col-12" style="border:solid;">
+
+<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <b>Los participantes: </b>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-success" href="<?php echo base_url('participante/add/'.$evento['idevento']) ?>">Agregar participante</a>
+        </div>
+    </div>
+</div>
+
+
+
+
+
 
 <table class="table table-striped table-bordered table-hover" id="mydatap">
  <thead>
