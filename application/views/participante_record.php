2162ed0458c6d67eba3e50ed074fb6bf888d176d
Stain Francis <educaysoft@gmail.com>
2023-02-17 19:51:59 -0500

    emoji en pariticipante_recor.php

--- application/views/participante_record.php
+++ application/views/participante_record.php
@@ -182,6 +182,15 @@ echo form_input('idnivelparticipante',$options[$participante['idnivelparticipant
  	<div class="row">
   	<div class="col-12" style="border:solid;">
 
+<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <b>Sesioner recibidas y Calificaciones del estudiante: </b>
+        </div>
+        
+    </div>
+</div>
+
 
 	<table class="table table-striped table-bordered table-hover" id="mydatac">
 	 <thead>
