b25008389730580958967d85b16579f6be43c9a5
Stain Francis <educaysoft@gmail.com>
2023-02-18 19:26:45 -0500

    creando el modulo de horasasignatura

--- application/views/asignatura_record.php
+++ application/views/asignatura_record.php
@@ -177,7 +177,6 @@ if(isset($asignatura))
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Silabos( <?php echo anchor('silabo/add', 'new'); ?>) : </label>
 
 	<div class="col-md-10">
 	<div class="row justify-content-left">
