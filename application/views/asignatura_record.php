643283d541a40c9ab3bea3eb2c09d830f5cadb8c
Stain Francis <educaysoft@gmail.com>
2023-02-18 19:22:55 -0500

    creando el modulo de horasasignatura

--- application/views/asignatura_record.php
+++ application/views/asignatura_record.php
@@ -183,7 +183,18 @@ if(isset($asignatura))
 	<div class="row justify-content-left">
       	<!-- Page Heading -->
  	<div class="row">
-  	<div class="col-12">
+  	<div class="col-12" style="border:solid;" >
+
+<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <b>Silabos presentados: </b>
+        </div>
+        
+    </div>
+</div>
+
+
 	<table class="table table-striped table-bordered table-hover" id="mydatas">
 	 <thead>
 	 <tr>
