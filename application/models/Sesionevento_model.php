de097f459a7f924d59ceed60d1a5d58a5d0a1b55
Stain Francis <educaysoft@gmail.com>
2023-02-22 00:52:39 -0500

    working with permisos

--- application/models/Sesionevento_model.php
+++ application/models/Sesionevento_model.php
@@ -62,7 +62,7 @@ class Sesionevento_model extends CI_model {
 
 
 	function sesionevento_sesiones($idevento){
-        $sesiones=$this->db->query('select count(idsesionevento) as nsesion from sesionevento where idevento='.$idevento.';');
+        $sesiones=$this->db->query('select * from sesionevento where idevento='.$idevento.';');
 
 	return $sesiones;
 }
