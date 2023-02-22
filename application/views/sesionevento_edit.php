8ad5b9f1a93d9107868567a23842d6c9ef044481
Stain Francis <educaysoft@gmail.com>
2023-02-22 03:31:46 -0500

    sesion

--- application/views/sesionevento_edit.php
+++ application/views/sesionevento_edit.php
@@ -112,7 +112,7 @@ foreach ($modoevaluacions as $row){
 
 
  <tr>
- <td colspan="2"> <hr><?php echo form_submit('submit', 'Guardar'); ?> <?php echo anchor('sesionevento','Atras') ?></td>
+ <td colspan="2"> <hr><?php echo form_submit('submit', 'Guardar'); ?> <input type="button" onclick="history.back()" name="volver atrás" value="volver atrás">  </td>
  </tr>
 </table>
 <?php echo form_close(); ?>
