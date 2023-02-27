fbbb1cebcd790ad8b0054813b8e8bc903aaf886f
Stain Francis <educaysoft@gmail.com>
2023-02-26 19:16:20 -0500

    mejoras en asistencia_form

--- application/controllers/Unidad.php
+++ application/controllers/Unidad.php
@@ -4,7 +4,7 @@ class Unidad extends CI_Controller{
 
   public function __construct(){
       parent::__construct();
-      $this->load->model('_model');
+      $this->load->model('unidad_model');
   	$this->load->model('institucion_model');
 }
 
