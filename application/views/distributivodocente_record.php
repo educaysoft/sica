bd5e4f12a617a9dce91fa44f9ec929215688252a
Stain Francis <educaysoft@gmail.com>
2023-02-27 22:18:07 -0500

    mejoras en distributivodocente

--- application/views/distributivodocente_record.php
+++ application/views/distributivodocente_record.php
@@ -260,6 +260,31 @@ $.ajax({url: '<?php echo site_url('silabo/save')?>',
 	dataType: 'json',
 	successs: function(data){
 
+	var idtipoevento=2; // CURSOS DE MALLA
+	var idevento_estado=2; //INSCRIPCION
+	var idinstitucion=1;  //Universidad Tecnica Luis Vargas Torres
+	var duracion=0;
+	var costo=0;	
+	var idsilabo=0;
+	var codigoclassroom="";	
+	var idasignaturadocente=0;	
+	var idcalendarioacademico=0;
+
+$.ajax({url: '<?php echo site_url('evento/save')?>',
+	method: 'POST',
+	data:{idtipoevento:idtipoevento,idevento_estado:idevento_estado,idinstitucion:idinstitucion,titulo:titulo,fechainicia:fechainicia,fechafinaliza:fechafinaliza,detalle:detalle,idusuario:idusuario,fecha:fecha,duracion:duracion,costo:costo,idsilabo:idsilabo,codigoclassroom:codigoclassroom,idasignaturadocente:idasignaturadocente,idcalendarioacademico:idcalendarioacademico},
+	async : true,
+	dataType: 'json',
+	successs: function(data){
+	
+	
+	},
+	      error: function (xhr, ajaxOptions, thrownError) {
+		alert(xhr.status);
+		alert(thrownError);
+	      }
+	    })
+
 
 
 
