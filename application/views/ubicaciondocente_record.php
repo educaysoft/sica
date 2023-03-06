a67868c44169954d4c55fe4edd6f73d15dc5143f
Stain Francis <educaysoft@gmail.com>
2023-03-06 00:52:50 -0500

    modulo de tramites

--- application/views/ubicaciondocente_record.php
+++ application/views/ubicaciondocente_record.php
@@ -60,15 +60,15 @@ echo form_input('idunidad',$options[$ubicaciondocente['idunidad']],array("disabl
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> <?php echo anchor('articulo/actual/'.$ubicaciondocente['idarticulo'], 'El artículo:'); ?> </label>
+    <label class="col-md-2 col-form-label"> <?php echo anchor('tramite/actual/'.$ubicaciondocente['idtramite'], 'El artículo:'); ?> </label>
 	<div class="col-md-10">
      <?php 
 $options= array("NADA");
-foreach ($articulos as $row){
-	$options[$row->idarticulo]= $row->nombre;
+foreach ($tramites as $row){
+	$options[$row->idtramite]= $row->nombre;
 }
 
-echo form_input('idarticulo',$options[$ubicaciondocente['idarticulo']],array("disabled"=>"disabled",'style'=>'width:500px;')); 
+echo form_input('idtramite',$options[$ubicaciondocente['idtramite']],array("disabled"=>"disabled",'style'=>'width:500px;')); 
 		?>
 	</div> 
 </div>
