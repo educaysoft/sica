23f11775c0ff178b6a6b045ffe1afecd48d99285
Stain Francis <educaysoft@gmail.com>
2023-03-06 21:12:05 -0500

    mejorando tramite

--- application/models/sql/sesionevento1.sql
+++ application/models/sql/sesionevento1.sql
@@ -1,15 +1,15 @@
 
 use educayso_facae;
-drop view sesionevento1;
 /*
+drop view sesionevento1;
 create view sesionevento1 as select seev.idsesionevento,seev.idevento,moev.nombre as evaluacion,  seev.fecha,seev.tema, even.titulo as elevento,(select count(asis.idtipoasistencia) from asistencia asis where asis.idevento=seev.idevento and asis.fecha=seev.fecha and asis.idtipoasistencia=1) as puntual,(select count(asis.idtipoasistencia) from asistencia asis where asis.idevento=seev.idevento and asis.fecha=seev.fecha and asis.idtipoasistencia=2) as atrasado   from sesionevento seev,evento even,modoevaluacion moev  where seev.idevento=even.idevento and seev.idmodoevaluacion=moev.idmodoevaluacion;
 */
 
 
 
-create view sesionevento1 as select seev.idsesionevento,seev.idevento,moev.nombre as evaluacion, moev.ponderacion, seev.fecha,seev.horainicio,seev.horafin,seev.tema, even.titulo as elevento,seev.idtema,tesi.numerosesion,(select count(idasistencia) from asistencia asis where asis.fecha=seev.fecha and asis.idtipoasistencia in (1,2) and asis.idevento=seev.idevento) as numeasis    from sesionevento seev,evento even,modoevaluacion moev,tema tesi  where seev.idevento=even.idevento and seev.idmodoevaluacion=moev.idmodoevaluacion and seev.idtema=tesi.idtema 
+create view sesionevento1 as select seev.idsesionevento,seev.idevento,moev.nombre as evaluacion, moev.ponderacion, seev.fecha,seev.horainicio,seev.horafin,seev.tema, even.titulo as elevento,seev.idtema,tesi.numerosesion,(select count(idasistencia) from asistencia asis where asis.fecha=seev.fecha and asis.idtipoasistencia in (1,2) and asis.idevento=seev.idevento) as numeasis, (select count(part.idparticipante) from participante part where part.idevento=seev.idevento) as numalum    from sesionevento seev,evento even,modoevaluacion moev,tema tesi  where seev.idevento=even.idevento and seev.idmodoevaluacion=moev.idmodoevaluacion and seev.idtema=tesi.idtema 
 
 
 UNION
 
- select seev.idsesionevento,seev.idevento,moev.nombre as evaluacion,  moev.ponderacion, seev.fecha,seev.horainicio,seev.horafin,seev.tema, even.titulo as elevento,seev.idtema, " " as numerosesion,  (select count(idasistencia) from asistencia asis where asis.fecha=seev.fecha and asis.idtipoasistencia in (1,2) and asis.idevento=seev.idevento) as numeasis  from sesionevento seev,evento even,modoevaluacion moev,tema tesi  where seev.idevento=even.idevento and seev.idmodoevaluacion=moev.idmodoevaluacion and seev.idtema is null; 
+			    select seev.idsesionevento,seev.idevento,moev.nombre as evaluacion,  moev.ponderacion, seev.fecha,seev.horainicio,seev.horafin,seev.tema, even.titulo as elevento,seev.idtema, " " as numerosesion,  (select count(idasistencia) from asistencia asis where asis.fecha=seev.fecha and asis.idtipoasistencia in (1,2) and asis.idevento=seev.idevento) as numeasis,(select count(part.idparticipante) from participante part where part.idevento=seev.idevento) as numalum   from sesionevento seev,evento even,modoevaluacion moev,tema tesi  where seev.idevento=even.idevento and seev.idmodoevaluacion=moev.idmodoevaluacion and seev.idtema is null; 
