45f1fbc25de1213f353f85dea4009e72388b76c9
Stain Francis <educaysoft@gmail.com>
2023-03-10 18:15:25 -0500

    mejoras a evaluacion

--- application/models/sql/respuesta1.sql
+++ application/models/sql/respuesta1.sql
@@ -1,3 +1,4 @@
 
 use educayso_facae;
-create view respuesta1 as select resp.idrespuesta,resp.respuesta,resp.acierto,preg.idpregunta,preg.idevaluacion from respuesta resp,pregunta preg where resp.idpregunta=preg.idpregunta;
+drop view respuesta1;
+create view respuesta1 as select resp.idrespuesta,resp.respuesta,resp.acierto,preg.idpregunta,preg.idreactivo from respuesta resp,pregunta preg where resp.idpregunta=preg.idpregunta;
