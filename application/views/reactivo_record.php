959a020f430ac219d4cbbfe3b4b1ab7702adcba9
Stain Francis <educaysoft@gmail.com>
2023-03-10 21:15:55 -0500

    mejoras a evaluacion

--- application/views/reactivo_record.php
+++ application/views/reactivo_record.php
@@ -39,33 +39,179 @@ if(isset($reactivo))
 <?php echo form_hidden('idreactivo',$reactivo['idreactivo']) ?>
 
 
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> id Evaluación:</label>
+	<div class="col-md-10">
+		<?php
+  $eys_arrctl=array("name"=>'idreactivo','value'=>$reactivo['idreactivo'],"disabled"=>"disabled",'placeholder'=>'Idreactivos','style'=>'width:500px;');
+ echo form_input($eys_arrctl) ;
+		?>
+	</div> 
+</div> 
+
+
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Nombre:</label>
+	<div class="col-md-10">
+		<?php
+
+
+  $eys_arrctl=array("name"=>'nombre','value'=>$reactivo['nombre'],"disabled"=>"disabled",'placeholder'=>'Inombre','style'=>'width:500px;');
+ echo form_input($eys_arrctl); 
+
+		?>
+	</div> 
+</div> 
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Detalle:</label>
+	<div class="col-md-10">
+		<?php
+
+  $eys_arrctl=array("name"=>'detalle','value'=>$reactivo['detalle'],"disabled"=>"disabled",'placeholder'=>'Detalle','style'=>'width:500px;');
+ echo form_textarea($eys_arrctl);
+		?>
+	</div> 
+</div> 
+  
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Id evento:</label>
+	<div class="col-md-10">
+		<?php
+
+    echo form_input('idevento',$reactivo['idevento'],array("disabled"=>"disabled",'placeholder'=>'Ideventos')); 
+
+		?>
+	</div> 
+</div>
+
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Evento </label>
+	<div class="col-md-10">
+     <?php 
+$options= array("NADA");
+foreach ($eventos as $row){
+	$options[$row->idevento]= $row->titulo;
+}
+
+echo form_input('idevento',$options[$reactivo['idevento']],array("disabled"=>"disabled",'style'=>'width:500px;')); 
+		?>
+	</div> 
+</div>
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Fecha:</label>
+	<div class="col-md-10">
+		<?php
+
+  $eys_arrctl=array("name"=>'fecha','value'=>$reactivo['fecha'],"disabled"=>"disabled",'placeholder'=>'Inombre','style'=>'width:500px;');
+ echo form_input($eys_arrctl); 
+
+		?>
+	</div> 
+</div>
+
+
+
+
+<div class="form-group row">
+	<div class="col-md-10">
+	<div class="row justify-content-left">
+      	<!-- Page Heading -->
+ 	<div class="row">
+  	<div class="col-12" style="border:solid;">
+
+<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <b>Preguntas: </b>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-success" href="<?php echo base_url('pregunta/add/'.$reactivo['idreactivo']) ?>">Nueva peregunta</a><a class="btn btn-danger" href="<?php echo base_url('pregunta/reportepdf/'.$reactivo['idreactivo']) ?>">Reporte</a>
+        </div>
+    </div>
+</div>
+
+	<table class="table table-striped table-bordered table-hover" id="mydatap">
+	 <thead>
+	 <tr>
+	 <th>idreactivo</th>
+	 <th>idpregunta</th>
+	 <th>pregunta</th>
+	 <th style="text-align: right;">Actions</th>
+	 </tr>
+	 </thead>
+	 <tbody id="show_data">
+	 </tbody>
+	</table>
+	</div>
+	</div>
+	</div>
+	</div> 
+</div>
+
+
+
+
+
+
+<div class="form-group row">
+	<div class="col-md-10">
+	<div class="row justify-content-left">
+      	<!-- Page Heading -->
+ 	<div class="row">
+  	<div class="col-12" style="border:solid;">
+
+<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <b>Respuestas: </b>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-success" href="<?php echo base_url('respuesta/add/'.$reactivo['idreactivo']) ?>">Nueva respuesta</a><a class="btn btn-danger" href="<?php echo base_url('pregunta/reportepdf/'.$reactivo['idreactivo']) ?>">Reporte</a>
+        </div>
+    </div>
+</div>
+
+	<table class="table table-striped table-bordered table-hover" id="mydatar">
+	 <thead>
+	 <tr>
+	 <th>idpregunta</th>
+	 <th>idrespuesta</th>
+	 <th>respuesta</th>
+	 <th>acieto</th>
+	 <th style="text-align: right;">Actions</th>
+	 </tr>
+	 </thead>
+	 <tbody id="show_data">
+	 </tbody>
+	</table>
+	</div>
+	</div>
+	</div>
+	</div> 
+</div>
+
+
+
+
+
+
+
+
 
 
 
 <?php echo form_close(); ?>
 
-<script type="text/javascript">
 
-$(document).ready(function(){
-	var idreactivo=document.getElementById("idreactivo").innerHTML;
-	var mytablaf= $('#mydatap').DataTable({"ajax": {url: '<?php echo site_url('reactivo/reactivo_pregunta')?>', type: 'GET',data:{idreactivo:idreactivo}},});
-
-	var mytablaf= $('#mydatar').DataTable({"ajax": {url: '<?php echo site_url('reactivo/reactivo_respuesta')?>', type: 'GET',data:{idreactivo:idreactivo}},});
-
-
-
-});
-
-$('#show_data').on('click','.item_ver',function(){
-var id= $(this).data('idsesionevento');
-var retorno= $(this).data('retorno');
-window.location.href = retorno+'/'+id;
-
-});
-
-
-
-</script>
 
 
 
