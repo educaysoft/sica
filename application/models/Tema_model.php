d2ad7366a2773db0925bd39884d6048cd171e517
Stain Francis <educaysoft@gmail.com>
2023-03-13 17:27:39 -0400

    evaluacion

--- application/models/Tema_model.php
+++ application/models/Tema_model.php
@@ -32,7 +32,7 @@ function lista_temass($idsilabo){
 	{
 	$this->db->where('idsilabo='.$idsilabo);
 	}
-	$query=$this->db->order_by("idsilabo","unidad asc","numerosesion asc")->get('tema1');
+	$query=$this->db->order_by("unidad asc","numerosesion asc")->get('tema1');
 	 return $query;
 	}
 
