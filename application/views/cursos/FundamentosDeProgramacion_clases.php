3e00913ac87822a022463eaa097ca2a6c4aa1cc9
Stain Francis <educaysoft@gmail.com>
2023-03-15 19:44:01 -0400

    evaluacion

--- application/views/cursos/FundamentosDeProgramacion_clases.php
+++ application/views/cursos/FundamentosDeProgramacion_clases.php
@@ -493,14 +493,14 @@ function get_pregunta(idpregunta,idx) {
 		if(data[i].idrespuesta==idrespuesta && acierto==0)
 		{
 		html += '<div>';
-		html += '  <input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+')">';
+		html += '  <input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+')"  checked>';
 		html += '  <label for="huey" style="color:red">'+data[i].respuesta+'</label>';
 		html += '</div>';
 		}
 		else if(data[i].idrespuesta==idrespuesta && acierto==1)
 		{
 		html += '<div>';
-		html += '  <input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+')">';
+		html += '  <input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+'  )"  checked>';
 		html += '  <label for="huey" style="color:green">'+data[i].respuesta+'</label>';
 		html += '</div>';
 		}
