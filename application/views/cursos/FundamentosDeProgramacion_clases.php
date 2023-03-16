ebfe9abd90601946e552544085f396769e6bcafe
Stain Francis <educaysoft@gmail.com>
2023-03-15 22:56:46 -0400

    evaluacion

--- application/views/cursos/FundamentosDeProgramacion_clases.php
+++ application/views/cursos/FundamentosDeProgramacion_clases.php
@@ -503,7 +503,7 @@ function get_pregunta(idpregunta,idx) {
 		j=i+1;
 		if(data[i].idrespuesta==idrespuesta && acierto==0)
 		{
-		html += '<input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+')"  checked style="position:absolute; margin-left:0 !important;"><span for="huey" style="color:red">   '+data[i].respuesta+'</span></input>';
+		html += '<input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+')"  checked style="position:absolute; margin-left:0 !important;"><span " style="color:red">   '+data[i].respuesta+'</span></input>';
 		}
 		else if(data[i].idrespuesta==idrespuesta && acierto==1)
 		{
