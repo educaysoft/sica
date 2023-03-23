256ebf2e3727e835c3d3563ff1d2b50c100af173
Stain Francis <educaysoft@gmail.com>
2023-03-23 16:36:50 -0400

    elimimirn participante

--- application/models/Participante_model.php
+++ application/models/Participante_model.php
@@ -128,7 +128,7 @@ return $participante;
 		if ($query->num_rows() != 0) {
 
 			$js="<script> 
-			confirm('Los datos se eliminaran ¿esta seguro?');
+			window.confirm('Los datos se eliminaran ¿esta seguro?');
 			</script>";
 
 			echo $js;
