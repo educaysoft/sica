02a47ca706659973a64a0136a3ea45ec273d281a
Stain Francis <educaysoft@gmail.com>
2023-03-26 10:39:44 -0400

    mejorando los modeolos de certificado

--- application/views/registration_form.php
+++ application/views/registration_form.php
@@ -103,7 +103,7 @@ if(sizeof($eventos)>1){
     $arrdatos=array('name'=>'idevento','value'=>$eventos[0]->idevento,"type"=>"hidden", "style"=>"width:600px");
 				echo form_input($arrdatos) ;
 
-	$textarea_options = array('class' => 'form-control','rows' => '4',   'cols' => '10',"disabled"=>"disabled", 'style'=> 'width:200px;height:80px;');    
+	$textarea_options = array('class' => 'form-control','rows' => '4',   'cols' => '10',"disabled"=>"disabled",  'class'=>'form-control');    
 							echo form_textarea('titulo',$eventos[0]->titulo,$textarea_options);
 }
 ?>
