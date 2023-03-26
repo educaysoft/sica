96d698cbaf117455804a1d36453e63e06a4c0f91
Stain Francis <educaysoft@gmail.com>
2023-03-26 01:58:02 -0400

    mejorando el modulo de sesionevento

--- application/views/template/page_header.php
+++ application/views/template/page_header.php
@@ -618,7 +618,7 @@ span a {
 
       <div class="btn-group">
         	  <button class="btn btn-success">
-		            <img id="foto" src= "<?php echo base_url().$this->session->userdata['logged_in']['foto']; ?>" height="100%" class="avatar" onerror="this.onerror=null; this.src= \"<?php echo base_url().'fotos/perfil.jpg'; ?>\" ">
+		            <img id="foto" src= "<?php echo base_url().$this->session->userdata['logged_in']['foto']; ?>" height="100%" class="avatar" onerror="this.onerror=null; this.src='<?php echo base_url().'fotos/perfil.jpg'; ?>'">
 	          </button>
 
             <button class="btn btn-success dropdown-toggle" data-toggle="dropdown">
