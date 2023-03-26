bdb980e2f64819553bf32b40eb33ab176d2e4328
Stain Francis <educaysoft@gmail.com>
2023-03-26 09:34:42 -0400

    mejorando los modeolos de certificado

--- application/models/sql/certificado1.sql
+++ application/models/sql/certificado1.sql
@@ -1,6 +1,5 @@
 
 use educayso_facae;
-drop view certificado1;
 create view certificado1 as select certificado.idcertificado,certificado.archivo,certificado.propietario,certificado.iddocumento,documento.asunto as eldocumento, evento.titulo as elevento, certificado.ubicacion,certificado.idevento from certificado,documento,evento where certificado.iddocumento=documento.iddocumento and certificado.idevento=evento.idevento
 UNION
 select certificado.idcertificado,certificado.archivo,certificado.propietario,certificado.iddocumento,''  as eldocumento,''  as elevento, certificado.ubicacion,"" as idevento from certificado where certificado.iddocumento is null and certificado.idevento is null;
