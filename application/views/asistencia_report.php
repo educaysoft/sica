97cf7dadd766136ce034e887f3e3e85a30a7dfc7
Stain Francis <educaysoft@gmail.com>
2023-03-28 23:21:08 -0400

    evento

--- application/views/asistencia_report.php
+++ application/views/asistencia_report.php
@@ -161,7 +161,7 @@ echo "<td style='color:black;"." background-color:rgb(".$x.",".$k.", 255); '>". 
 
      if($arrasistencia[$row1->fecha][0]==2)   //atraseo
 		{
-echo "<td style='color:black;"." background-color:rgb("."10".","."20".", 30); '>". '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/><path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>'.$arrasistencia[$row1->fecha][1]."</td>";
+echo "<td style='color:black;"." background-color:rgb(".$x.",".$k.", 255); '>". '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/><path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>'.$arrasistencia[$row1->fecha][1]."</td>";
 		}
 
 
@@ -207,8 +207,8 @@ $j=0;
 	  $aus=$aus+1;
       }
     } 
-      $resu=round(($asi/($asi+$aus))*100,2);
-      echo "<td>".$resu."</td>";
+      $resu=round(($asi/($totalsesion))*100,2);
+      echo "<td>".$asi." = ".$resu."%</td>";
       echo "</tr>";
 $j=0;
 $asi=0;
