eb57515eba21888a34fdae484939ed59aa77c611
ElProfe <educaysoft@gmail.com>
2023-04-01 14:57:07 -0500

    sesionevento

--- application/controllers/Sesionevento.php
+++ application/controllers/Sesionevento.php
@@ -269,6 +269,10 @@ class Sesionevento extends CI_Controller{
 	public function  save_edit()
 	{
 		$id=$this->input->post('idsesionevento');
+
+
+
+
 	 	$array_item=array(
 		 	'idevento' => $this->input->post('idevento'),
 		 	'fecha' => $this->input->post('fecha'),
@@ -288,6 +292,48 @@ class Sesionevento extends CI_Controller{
 			echo "<script language='JavaScript'> window.history.go(-2);</script>";
 		}
  	}
+
+
+
+	public function  save_editx()
+	{
+		$id=$this->input->post('idsesionevento');
+		$idtema=$this->input->post('idtema');
+
+		$array_tema=array(
+	 		'nombrecorto' => $this->input->post('temacorto'),
+	 		'nombrelargo' => $this->input->post('tema'),
+	 		'idunidadsilabo' => $this->input->post('idunidadsilabo'),
+		 	'numerosesion' => $this->input->post('numerosesion'),
+	 	);
+	 	$idtema =$this->tema_model->update($idtema,$array_tema);
+
+
+
+
+	 	$array_item=array(
+		 	'idevento' => $this->input->post('idevento'),
+		 	'fecha' => $this->input->post('fecha'),
+		 	'idtema' => $this->input->post('idtema'),
+		 	'tema' => $this->input->post('tema'),
+		 	'temacorto' => $this->input->post('temacorto'),
+		 	'horainicio' => $this->input->post('horainicio'),
+		 	'horafin' => $this->input->post('horafin'),
+		 	'idmodoevaluacion' => $this->input->post('idmodoevaluacion'),
+	 	);
+	 	$result=$this->sesionevento_model->update($id,$array_item);
+	 	if(!$result)
+		{
+			echo "<script language='JavaScript'> alert('sesion no se pudo modificar'); </script>";
+			echo "<script language='JavaScript'> window.history.go(-2);</script>";
+		}else{
+			echo "<script language='JavaScript'> window.history.go(-2);</script>";
+		}
+ 	}
+
+
+
+
 
 
 
--- application/views/sesionevento_editx.php
+++ application/views/sesionevento_editx.php
@@ -1,4 +1,4 @@
-<?php echo form_open('sesionevento/save_edit') ?>
+<?php echo form_open('sesionevento/save_editx') ?>
 <?php echo form_hidden('idsesionevento',$sesionevento['idsesionevento']) ?>
 <h2> <?php echo $title; ?></h2>
 <hr />
