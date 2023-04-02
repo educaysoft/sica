b8e105a10adbe5b5007884cb1ef60e92248cd82c
ElProfe <educaysoft@gmail.com>
2023-04-02 02:27:21 -0500

    sesionevento

--- application/views/sesionevento_editx.php
+++ application/views/sesionevento_editx.php
@@ -321,8 +321,7 @@ echo form_dropdown("idmodoevaluacion",$options, $sesionevento['idmodoevaluacion'
 
 $(document).ready(function(){
   	var idsilabo=<?php echo $evento['idsilabo']; ?>;
-	var mytablat= $('#mydatac').DataTable(pageLength:50,"ajax":,url: '<?php echo site_url('tema/tema_silabo')?>', type: 'GET',data:{idsilabo:idsilabo}
-);
+	var mytablat= $('#mydatac').DataTable({pageLength:50,"ajax":{url: '<?php echo site_url('tema/tema_silabo')?>', type: 'GET',data:{idsilabo:idsilabo}}});
 
 
 
