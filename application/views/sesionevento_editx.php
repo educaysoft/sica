1e02140c75f19780297f00ec8304260a432ae166
ElProfe <educaysoft@gmail.com>
2023-04-01 23:36:16 -0500

    sesionevento

--- application/views/sesionevento_editx.php
+++ application/views/sesionevento_editx.php
@@ -133,7 +133,7 @@ foreach ($sesioneventos as $row){
 
 
 <div class="form-group row">
-<label class="col-md-2 col-form-label">Unidad:</label>
+<label class="col-md-2 col-form-label">idtema:</label>
 <div class="col-md-10">
 
 <?php
