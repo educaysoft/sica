6b80f273bbb199b9a69f6f30126f0b9a7dd27f7c
ElProfe <educaysoft@gmail.com>
2023-04-02 12:10:30 -0500

    sesionevento

--- application/controllers/Sesionevento.php
+++ application/controllers/Sesionevento.php
@@ -247,19 +247,23 @@ class Sesionevento extends CI_Controller{
 
 	public function edit()
 	{
-		$idevento=$this->uri->segment(3);
+		$idsesionevento=$this->uri->segment(3);
 	 	$data['sesioneventos'] = $this->sesionevento_model->sesionevento($this->uri->segment(3))->result();
-		$data['sesionevento'] = $this->sesionevento_model->sesionevento($idevento)->row_array();
+		$data['sesionevento'] = $this->sesionevento_model->sesionevento($idsesionevento)->row_array();
 		$data['evento'] = $this->evento_model->evento($data['sesionevento']['idevento'])->row_array();
+  		$data['unidadsilabos']= $this->unidadsilabo_model->unidadsilaboss($data['evento']['idsilabo'])->result();
 		$data['jornadadocente']= $this->jornadadocente_model->jornadadocentes($data['evento']['idasignaturadocente'])->result();
 
-		$data['eventos']= $this->evento_model->lista_eventos()->result();
+	//	$data['eventos']= $this->evento_model->lista_eventos()->result();
+		$data['eventos'] = $this->evento_model->evento($data['sesionevento']['idevento'])->result();
   		$data['temas']= $this->tema_model->lista_temass($data['evento']['idsilabo'])->result();
 		$data['personas']= $this->persona_model->lista_personas()->result();
 		$data['modoevaluacions']= $this->modoevaluacion_model->lista_modoevaluacions()->result();
 		$data['calendarioacademico'] = $this->calendarioacademico_model->lista_calendarioacademicosA($data['evento']['idcalendarioacademico'])->result();
   		$data['documentos']= $this->documento_model->lista_documentos()->result();
- 	 	$data['title'] = "Actualizar Sesionevento";
+ 
+
+	 	$data['title'] = "Actualizar Sesionevento";
  	 	$this->load->view('template/page_header');		
  	 	$this->load->view('sesionevento_edit',$data);
 	 	$this->load->view('template/page_footer');
