e2f5eea317226280c10a1bdafbb5209c3ac9358c
Stain Francis <educaysoft@gmail.com>
2023-04-04 00:04:56 -0400

    reporete pdf

--- application/views/cursos/FundamentosDeProgramacion_clases.php
+++ application/views/cursos/FundamentosDeProgramacion_clases.php
@@ -539,21 +539,21 @@ function get_pregunta(idpregunta,idx) {
 		if(data[i].idrespuesta==idrespuesta && acierto==0)
 		{
 		html+="<div>";
-		html += '<input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+','+"x"+')"  checked >';
+		html += '<input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+','+'x'+')"  checked >';
 		html += '  <label for="huey" style="color:red">'+data[i].respuesta+'</label>';
 		html+="</div>";
 		}
 		else if(data[i].idrespuesta==idrespuesta && acierto==1)
 		{
 		html+="<div>";
-		html += '<input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+','+"x"+'  )"  checked>';
+		html += '<input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+','+'x'+'  )"  checked>';
 		html += '  <label for="huey" style="color:green">'+data[i].respuesta+'</label>';
 		html+="</div>";
 		}
 		else
 		{
 		html+="<div>";
-		html += '<input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+','+"x"')">';
+		html += '<input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+','+'x'+')">';
 		html += '  <label for="huey" >'+data[i].respuesta+'</label>';
 		html+="</div>";
 		}
