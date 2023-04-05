7cd806e3c07ac04cd7a5ea41139cdaa59a63b4fd
Stain Francis <educaysoft@gmail.com>
2023-04-04 21:24:40 -0400

    reporete pdf

--- application/views/silabo_record.php
+++ application/views/silabo_record.php
@@ -150,7 +150,20 @@ if(isset($silabo))
 	<div class="row justify-content-left">
       	<!-- Page Heading -->
  	<div class="row">
-  	<div class="col-12">
+  	<div class="col-12" style="border:solid;" >
+
+<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <b>Sesiones dictadas: </b>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-success" href="<?php echo base_url('sesionevento/add/'.$evento['idevento']) ?>">Nueva sesion</a><a class="btn btn-danger" onclick='reportepdf()' >Reporte</a>
+        </div>
+    </div>
+</div>
+
+
 	<table class="table table-striped table-bordered table-hover" id="mydatac">
 	 <thead>
 	 <tr>
