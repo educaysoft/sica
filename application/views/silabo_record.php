84778fa19792c5492656e67321f898098afc7933
Stain Francis <educaysoft@gmail.com>
2023-04-04 21:52:32 -0400

    reporete pdf

--- application/views/silabo_record.php
+++ application/views/silabo_record.php
@@ -190,7 +190,17 @@ if(isset($silabo))
 	<div class="row justify-content-left">
       	<!-- Page Heading -->
  	<div class="row">
-  	<div class="col-12">
+  	<div class="col-12" style="border:solid;" >
+
+<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+    <div class="col-lg-12 margin-tb">
+	<div class="pull-left"> 
+	 <b>Eventos dictados: ( <?php echo anchor('evento/add', 'New'); ?>):</b>
+        </div>
+        
+    </div>
+</div>
+
 	<table class="table table-striped table-bordered table-hover" id="mydatae">
 	 <thead>
 	 <tr>
