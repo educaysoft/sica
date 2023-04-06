72525e7e24772902633f6ca47a2b060d37dbda97
Stain Francis <educaysoft@gmail.com>
2023-04-06 00:53:38 -0400

    reporete pdf

--- application/controllers/Evento.php
+++ application/controllers/Evento.php
@@ -338,7 +338,7 @@ public function index(){
 		$result = $this->participante_model->esinstructor($id,$r->idevento);
 		if($result)
 		{
-			$data[]=array($r->idevento,$r->titulo,$r->fechainicia,$r->fechafinaliza,$r->estado,$r->eltutor,
+			$data[]=array($r->idevento,$r->titulo,$r->fechainicia,$r->fechafinaliza,$r->eltutor,
 					$r->href='<a href="javascript:void(0);" class="btn btn-info btn-sm item_ver"  data-retorno="'.site_url('evento/actual').'"    data-idevento="'.$r->idevento.'">Edit</a><a href="javascript:void(0);" class="btn btn-info btn-sm item_ver2"  data-retorno2="'.site_url('evento/detalle').'"    data-idevento2="'.$r->idevento.'">Ver</a>');
 				
 			}else{
