8997a88870bae12c8d000bc2ba2569f5dd1fcf69
ElProfe <educaysoft@gmail.com>
2023-04-08 12:08:13 -0500

    visitantes

--- application/views/visitante_form.php
+++ application/views/visitante_form.php
@@ -195,7 +195,7 @@ foreach ($personas as $row){
 
 
 
-	<div class="col-md-10" style="height:10px">
+	<div class="col-md-10" style="height:60px">
  <div data-role="page" id="2">
 			<div data-role="header">
 				<h1>Signature Pad - HTML5 - jQuery Mobile</h1>
