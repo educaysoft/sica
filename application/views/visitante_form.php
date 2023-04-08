e4d749654551691ee604bc0686ad82989413ad59
ElProfe <educaysoft@gmail.com>
2023-04-08 09:56:11 -0500

    visitantes

--- application/views/visitante_form.php
+++ application/views/visitante_form.php
@@ -4,143 +4,70 @@
 	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
 	<style type="text/css">
-		#div_signcontract{ width: 99%; }
-		.popupHeader{ margin: 10px; }
+		canvas#signature {
+  		border: 2px solid black;
+		}
+
+		form>* {
+  		margin: 10px;
+		}
 	</style>
 	<script type="text/javascript">
-		var isSign = false;
-		var leftMButtonDown = false;
-		
-		jQuery(function(){
-			//Initialize sign pad
-			init_Sign_Canvas();
-		});
-		
-		function fun_submit() {
-			if(isSign) {
-				var canvas = $("#canvas").get(0);
-				var imgData = canvas.toDataURL();
-				jQuery('#page').find('p').remove();
-				jQuery('#page').find('img').remove();
-				jQuery('#page').append(jQuery('<p>Your Sign:</p>'));
-				jQuery('#page').append($('<img/>').attr('src',imgData));
-				
-				closePopUp();
-			} else {
-				alert('Please sign');
-			}
-		}
-		
-		function closePopUp() {
-			jQuery('#divPopUpSignContract').popup('close');
-			jQuery('#divPopUpSignContract').popup('close');
-		}
-		
-		function init_Sign_Canvas() {
-			isSign = false;
-			leftMButtonDown = false;
-			
-			//Set Canvas width
-			var sizedWindowWidth = $(window).width();
-			if(sizedWindowWidth > 700)
-				sizedWindowWidth = $(window).width() / 2;
-			else if(sizedWindowWidth > 400)
-				sizedWindowWidth = sizedWindowWidth - 100;
-			else
-				sizedWindowWidth = sizedWindowWidth - 50;
-			 
-			 $("#canvas").width(sizedWindowWidth);
-			 $("#canvas").height(200);
-			 $("#canvas").css("border","1px solid #000");
-			
-			 var canvas = $("#canvas").get(0);
-			
-			 canvasContext = canvas.getContext('2d');
 
-			 if(canvasContext)
-			 {
-				 canvasContext.canvas.width  = sizedWindowWidth;
-				 canvasContext.canvas.height = 200;
+var canvas = document.getElementById('signature');
+var ctx = canvas.getContext("2d");
+var drawing = false;
+var prevX, prevY;
+var currX, currY;
+var signature = document.getElementsByName('signature')[0];
 
-				 canvasContext.fillStyle = "#fff";
-				 canvasContext.fillRect(0,0,sizedWindowWidth,200);
-				 
-				 canvasContext.moveTo(50,150);
-				 canvasContext.lineTo(sizedWindowWidth-50,150);
-				 canvasContext.stroke();
-				
-				 canvasContext.fillStyle = "#000";
-				 canvasContext.font="20px Arial";
-				 canvasContext.fillText("x",40,155);
-			 }
-			 // Bind Mouse events
-			 $(canvas).on('mousedown', function (e) {
-				 if(e.which === 1) { 
-					 leftMButtonDown = true;
-					 canvasContext.fillStyle = "#000";
-					 var x = e.pageX - $(e.target).offset().left;
-					 var y = e.pageY - $(e.target).offset().top;
-					 canvasContext.moveTo(x, y);
-				 }
-				 e.preventDefault();
-				 return false;
-			 });
-			
-			 $(canvas).on('mouseup', function (e) {
-				 if(leftMButtonDown && e.which === 1) {
-					 leftMButtonDown = false;
-					 isSign = true;
-				 }
-				 e.preventDefault();
-				 return false;
-			 });
-			
-			 // draw a line from the last point to this one
-			 $(canvas).on('mousemove', function (e) {
-				 if(leftMButtonDown == true) {
-					 canvasContext.fillStyle = "#000";
-					 var x = e.pageX - $(e.target).offset().left;
-					 var y = e.pageY - $(e.target).offset().top;
-					 canvasContext.lineTo(x,y);
-					 canvasContext.stroke();
-				 }
-				 e.preventDefault();
-				 return false;
-			 });
-			 
-			 //bind touch events
-			 $(canvas).on('touchstart', function (e) {
-				leftMButtonDown = true;
-				canvasContext.fillStyle = "#000";
-				var t = e.originalEvent.touches[0];
-				var x = t.pageX - $(e.target).offset().left;
-				var y = t.pageY - $(e.target).offset().top;
-				canvasContext.moveTo(x, y);
-				
-				e.preventDefault();
-				return false;
-			 });
-			 
-			 $(canvas).on('touchmove', function (e) {
-				canvasContext.fillStyle = "#000";
-				var t = e.originalEvent.touches[0];
-				var x = t.pageX - $(e.target).offset().left;
-				var y = t.pageY - $(e.target).offset().top;
-				canvasContext.lineTo(x,y);
-				canvasContext.stroke();
-				
-				e.preventDefault();
-				return false;
-			 });
-			 
-			 $(canvas).on('touchend', function (e) {
-				if(leftMButtonDown) {
-					leftMButtonDown = false;
-					isSign = true;
-				}
-			 
-			 });
-		}
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", stop);
+canvas.addEventListener("mousedown", start);
+
+function start(e) {
+  drawing = true;
+}
+
+function stop() {
+  drawing = false;
+  prevX = prevY = null;
+  signature.value = canvas.toDataURL();
+}
+
+function draw(e) {
+  if (!drawing) {
+    return;
+  }
+  // Test for touchmove event, this requires another property.
+  var clientX = e.type === 'touchmove' ? e.touches[0].clientX : e.clientX;
+  var clientY = e.type === 'touchmove' ? e.touches[0].clientY : e.clientY;
+  currX = clientX - canvas.offsetLeft;
+  currY = clientY - canvas.offsetTop;
+  if (!prevX && !prevY) {
+    prevX = currX;
+    prevY = currY;
+  }
+
+  ctx.beginPath();
+  ctx.moveTo(prevX, prevY);
+  ctx.lineTo(currX, currY);
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.closePath();
+
+  prevX = currX;
+  prevY = currY;
+}
+
+function onSubmit(e) {
+  console.log({
+    'name': document.getElementsByName('name')[0].value,
+    'signature': signature.value,
+  });
+  return false;
+}
 	</script>
 
 
@@ -209,29 +136,12 @@ foreach ($personas as $row){
   <label class="col-md-2 col-form-label">Grupo</label>
 	<div class="col-md-10">
 
-<div data-role="page">
-		<div data-role="header">
-			<h1>Signature Pad - HTML5 - jQuery Mobile</h1>
-		</div><!-- /header -->
-		<div id="page" data-role="content">
-			<a href="#divPopUpSignContract" data-rel="popup" data-position-to="window" data-role="button" data-inline="true">Open Sign Pad</a>
-		</div>	
-		<div data-role="popup" id="divPopUpSignContract">
-			<div data-role="header" data-theme="b">
-				<a data-role="button" data-rel="back" data-transition="slide" class="ui-btn-right" onclick="closePopUp()"> Close </a>
-				<p class="popupHeader">Sign Pad</p>
-			</div>
-			<div class="ui-content popUpHeight">
-				<div id="div_signcontract">
-					<canvas id="canvas">Canvas is not supported</canvas>
-					<div>
-						<input id="btnSubmitSign" type="button" data-inline="true" data-mini="true" data-theme="b" value="Submit Sign" onclick="fun_submit()" />
-						<input id="btnClearSign" type="button" data-inline="true" data-mini="true" data-theme="b" value="Clear" onclick="init_Sign_Canvas()" />
-					</div>
-				</div>	
-			</div>
-		</div>
-	</div>	
+ <div>
+    <canvas id="signature" width="300" height="100"></canvas>
+  </div>
+  <div>
+    <input type="hidden" name="signature" />
+  </div>	
 
 
 
