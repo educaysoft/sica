8395b1638ff9811723f6fe06450874c50f27f9f9
Stain Francis <educaysoft@gmail.com>
2023-04-10 00:24:09 -0400

    informe de distributivo

--- application/views/visitante_edit.php
+++ application/views/visitante_edit.php
@@ -1,4 +1,4 @@
-<?php echo form_open('https://repositorioutlvte.org/firmadigital.php',array('method'=>'post')); ?>
+<?php echo form_open('https://repositorioutlvte.org/firmadigital.php',array('method'=>'GET')); ?>
 <?php echo form_hidden('idvisitante',$visitante['idvisitante']) ?>
 <h2> <?php echo $title; ?></h2>
 <hr />
