3f396e21eb786499bc3b6fb473e83ce934be3f50
Stain Francis <educaysoft@gmail.com>
2023-04-13 03:50:53 -0400

    mejoras en evento

--- application/views/cursos/2023/index.php
+++ application/views/cursos/2023/index.php
@@ -114,7 +114,7 @@
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
         <h1 class="fw-light">Cursos de Perfeccionamiento Académico y Ética</h1>
-        <p class="lead text-muted">Inscribete y da seguimeinto a los cursos  .</p>
+        <p class="lead text-muted">Inscribete y da seguimiento al  curso  .</p>
       <!---  <p>
           <a href="#" class="btn btn-primary my-2">Main call to action</a>
           <a href="#" class="btn btn-secondary my-2">Secondary action</a>
