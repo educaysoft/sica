37a8b7b1f8142d864c3d3733e536af38e27cfd71
Stain Francis <educaysoft@gmail.com>
2023-04-14 18:32:35 -0400

    cursos

--- application/views/cursos/2023/index.php
+++ application/views/cursos/2023/index.php
@@ -31,6 +31,24 @@
 width:100%;
 }
 
+.img-contenedor:hover img {
+-webkit-transform:scale(1.25);
+-moz-transform:scale(1.25);
+-ms-transform:scale(1.25);
+-o-transform:scale(1.25);
+transform:scale(1.25);
+}
+
+.img-contenedor {/*Ancho y altura son modificables al requerimiento de cada uno*/
+/*width:300px;*/
+/*height:180px;*/
+overflow:hidden;
+}
+
+
+
+
+
 
 
       .bd-placeholder-img {
@@ -195,6 +213,10 @@ width:100%;
 <div class="col">
           <div class="card shadow-sm">
 		  <a  href='https://congresoutlvte.org/facped/'><svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/> <image href="<?php echo base_url(); ?>images/cursos/estadistica.jpg" height="100%" width="100%"/> </svg></a>
+
+<div class='img-contenedor w3-card-4' style='position:absolute; top:0px;right:0px; border: 2px solid green; border-radius: 50%; width: 30%; display:flex; justify-content: center; align-items: center;'>
+<img src='https://repositorioutlvte.org/Repositorio/fotos/0960576361.jpg' width='100%' height='100%' style='border-radius:50px;'>
+</div>
 
             <div class="card-body">
               <p class="card-text">Estadística Aplicada y Análisis Estadístico.</p>
