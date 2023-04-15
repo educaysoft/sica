5088bf1482fdc331119a57ac4f76f55cdf32dbea
Stain Francis <educaysoft@gmail.com>
2023-04-14 23:49:49 -0400

    cursos

--- application/views/registration_form.php
+++ application/views/registration_form.php
@@ -34,7 +34,7 @@
 
 
 		<div style="width: 40%;  padding:5px;   display: inline-flex; " >
-			<div class="w3-card-2"  style="width:100%; height:900px; ">
+			<div class="w3-card-2"  style="width:100%; height:1000px; ">
 				<header class="w3-container" style="background-color:#4CAF50;">
 					<p style="font-variant: small-caps; font-weight:bold; font-family:'Times New Roman'; font-size:25px; color:white;
                							 text-align:center;">Registrate Aquí </p>
