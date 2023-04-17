fd1fcd36f1063d11734e3aa6f3dd2a942b32eb68
Stain Francis <educaysoft@gmail.com>
2023-04-17 01:19:43 -0400

    mejorando funcionario

--- application/views/funcionario_form.php
+++ application/views/funcionario_form.php
@@ -2,34 +2,77 @@
 <hr/>
 <?php echo form_open("funcionario/save") ?>
 <?php echo form_hidden("idfuncionario")  ?>
-<table>
 
 
 
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Departamento:</label>
+	<div class="col-md-10">
+	<?php
+
+$options= array('--Select--');
+foreach ($departamentos as $row){
+	$options[$row->iddepartamento]= $row->nombre;
+}
+ echo form_dropdown("iddepartamento",$options, set_select('--Select--','default_value')); 
+		?>
+	</div> 
+</div>
 
 
-<tr>
-<td> Persona: </td>
-<td><?php 
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Persona:</label>
+	<div class="col-md-10">
+	<?php
 
 $options= array('--Select--');
 foreach ($personas as $row){
 	$options[$row->idpersona]= $row->apellidos." ".$row->nombres;
 }
 
- echo form_dropdown("idpersona",$options, set_select('--Select--','default_value'));  ?></td>
-</tr>
+ echo form_dropdown("idpersona",$options, set_select('--Select--','default_value'));  
+		?>
+	</div> 
+</div>
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Cargo:</label>
+	<div class="col-md-10">
+	<?php
+$options= array('--Select--');
+foreach ($cargos as $row){
+	$options[$row->idcargo]= $row->nombre;
+}
+ echo form_dropdown("idcargo",$options, set_select('--Select--','default_value')); 
+		?>
+	</div> 
+</div>
 
 
 
 
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Fecha de ingreso:</label>
+	<div class="col-md-10">
+	<?php
+ echo form_input(array("name"=>"fechaingreso","id"=>"fechaingreso","type"=>"date"));  
+		?>
+	</div> 
+</div>
 
 
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Fecha de salida:</label>
+	<div class="col-md-10">
+	<?php
+ echo form_input(array("name"=>"fechasalida","id"=>"fechasalida","type"=>"date"));  
+		?>
+	</div> 
+</div>
 
 
-
-
-
+<table>
 <tr>
 <td colspan="2"> <hr><?php echo form_submit("submit", "Guardar"); ?><?php echo anchor("funcionario","Atras") ?> </td>
 </tr>
