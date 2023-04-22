7849cc10d10f5b78b785173d6cf268d8a0589b2f
Stain Francis <educaysoft@gmail.com>
2023-04-22 08:04:17 -0400

    estado del correo

--- application/views/evento_record.php
+++ application/views/evento_record.php
@@ -389,7 +389,8 @@ echo form_input($arrdatos) ?>
             <b>Los participantes: </b>
         </div>
         <div class="pull-right">
-            <a class="btn btn-success" href="<?php echo base_url('asistencia/reportepdf/'.$evento['idevento']) ?>">Reporte</a><a class="btn btn-danger" href="<?php echo base_url('asistencia/add/'.$evento['idevento']) ?>">Tomar Asistencia</a>
+          <a class="btn btn-danger" href="<?php echo base_url('asistencia/add/'.$evento['idevento']) ?>">Tomar Asistencia</a>  <a class="btn btn-success" href="<?php echo base_url('asistencia/reportepdf/'.$evento['idevento']) ?>">Reporte</a><a class="btn btn-warning" href="<?php echo base_url('participante/add/'.$evento['idevento']) ?>">+</a>
+
         </div>
     </div>
 </div>
