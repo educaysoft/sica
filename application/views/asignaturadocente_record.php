35d37cc3fa6f08ce9b57f30297124451749291b4
Stain Francis <educaysoft@gmail.com>
2023-04-22 07:41:09 -0400

    estado del correo

--- application/views/asignaturadocente_record.php
+++ application/views/asignaturadocente_record.php
@@ -54,7 +54,8 @@ if(isset($asignaturadocente))
 		$options[$row->iddistributivodocente]= $row->eldistributivodocente;
 	}
 
-echo form_input('iddistributivodocente',$options[$asignaturadocente['iddistributivodocente']],array("disabled"=>"disabled",'style'=>'width:500px;'));
+echo form_input('eldistributivodocente',$options[$asignaturadocente['iddistributivodocente']],array("disabled"=>"disabled",'style'=>'width:500px;'));
+echo form_input('iddistributivodocente',$asignaturadocente['iddistributivodocente'],array("display"=>"none",'style'=>'width:500px;'));
 
 		?>
 	</div> 
@@ -154,6 +155,47 @@ echo form_input('iddistributivodocente',$options[$asignaturadocente['iddistribut
 
 
 
+<div class="form-group row">
+
+	<div class="col-md-10">
+	<div class="row justify-content-left">
+      	<!-- Page Heading -->
+ 	<div class="row">
+  	<div class="col-12" style="border:solid;">
+
+<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <b>Eventos-cursos:</b>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-success" href="<?php echo base_url('evento/add/') ?>">Crear un evento</a>
+        </div>
+    </div>
+</div>
+
+	<table class="table table-striped table-bordered table-hover" id="mydatae">
+	 <thead>
+	 <tr>
+	 <th>idsilabo</th>
+	 <th>idevento</th>
+	 <th>evento</th>
+	 <th>Classroom</th>
+	 <th style="text-align: right;">Actions</th>
+	 </tr>
+	 </thead>
+	 <tbody id="show_data1">
+	 </tbody>
+	</table>
+	</div>
+	</div>
+	</div>
+	</div> 
+</div>
+
+
+
+
 
 
 <?php echo form_close(); ?>
@@ -162,8 +204,14 @@ echo form_input('iddistributivodocente',$options[$asignaturadocente['iddistribut
 <script type="text/javascript">
 
 $(document).ready(function(){
+	var iddistributivodocente=document.getElementById("iddistributivodocente").value;
 	var idasignaturadocente=document.getElementById("idasignaturadocente").value;
 	var mytablaf= $('#mydatac').DataTable({"ajax": {url: '<?php echo site_url('asignaturadocente/jornadadocente_data')?>', type: 'GET',data:{idasignaturadocente:idasignaturadocente}},});
+
+
+	var mytablaf= $('#mydatae').DataTable({"ajax": {url: '<?php echo site_url('distributivodocente/evento_data')?>', type: 'GET',data:{iddistributivodocente:iddistributivodocente}},});
+
+
 });
 
 
