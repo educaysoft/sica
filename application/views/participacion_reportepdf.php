dd46c254bb6c1a9d555ddc2b5d2bbccaf72560c6
Stain Francis <educaysoft@gmail.com>
2023-05-03 20:43:13 -0400

    sesino evernto

--- application/views/participacion_reportepdf.php
+++ application/views/participacion_reportepdf.php
@@ -177,11 +177,11 @@ if(checkdate($m,$d,$a)){
 		$pdf->Cell(8,5,$row->temacorto,1,0,'C',1);
 	}
 	$pdf->Cell(10,5,'P1',1,0,'C',1);
-	$pdf->Cell(11,5,'As1('.$sesiontotal[0].')',0,'C',1);
+	$pdf->Cell(12,5,'As1('.$sesiontotal[0].')',0,'C',1);
 	$pdf->Cell(10,5,'P2',1,0,'C',1);
-	$pdf->Cell(11,5,'As2('.$sesiontotal[1].')',0,'C',1);
+	$pdf->Cell(12,5,'As2('.$sesiontotal[1].')',0,'C',1);
 	$pdf->Cell(10,5,'Prom',1,0,'C',1);
-	$pdf->Cell(11,5,'Asis',1,1,'C',1);
+	$pdf->Cell(12,5,'As('.$sesiontotal[0]+$sesiontotal[1].')',1,1,'C',1);
  
 
 
@@ -274,7 +274,7 @@ if(checkdate($m,$d,$a)){
 	       if($nnotas[$k]>=1){
 		$sum=$sum+round($sp,0);
     		$pdf->Cell(10,5,round($sp,0),1,0,'R',0);
-    		$pdf->Cell(11,5,round((100*$arrasistencia[$id][$k]/$sesiontotal[$k]),0).'%',1,0,'R',0);
+    		$pdf->Cell(12,5,round((100*$arrasistencia[$id][$k]/$sesiontotal[$k]),0).'%',1,0,'R',0);
 		$k=$k+1;
 	       }else{
 		 if($sp>0){
@@ -284,10 +284,10 @@ if(checkdate($m,$d,$a)){
 			 }
 	       }
 	    } 
-		if($k=1){
+		if($k==1){
 
     		$pdf->Cell(10,5,0,1,0,'R',0);
-    		$pdf->Cell(11,5,round((100*$arrasistencia[$id][$k]/$sesiontotal[$k]),0).'%',1,0,'R',0);
+    		$pdf->Cell(12,5,round((100*$arrasistencia[$id][$k]/$sesiontotal[$k]),0).'%',1,0,'R',0);
 
 
 		}
@@ -311,7 +311,7 @@ if(checkdate($m,$d,$a)){
 			$aprobados=$aprobados+1;
 		}
 		//--Imprime la asistencia
-		$pdf->Cell(11,5,round(100*(($arrasistencia[0]+$arrasistencia[1])/($sesiontotal[0]+$sesiontotal[1])),0),1,1,'R',0);
+		$pdf->Cell(12,5,round(100*(($arrasistencia[0]+$arrasistencia[1])/($sesiontotal[0]+$sesiontotal[1])),0),1,1,'R',0);
 		//--INicializa para el proximo participante
 		foreach($fechacorte as $p=>$fc)
 		{
