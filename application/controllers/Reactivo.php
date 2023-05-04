7ed98834ef61398e97ef3d1b3b24d7189e36b5d8
Stain Francis <educaysoft@gmail.com>
2023-05-03 23:02:31 -0400

    sesino evernto

--- application/controllers/Reactivo.php
+++ application/controllers/Reactivo.php
@@ -168,6 +168,21 @@ function reactivo_respuesta()
 
 
 
+	public function reportepdf()
+	{
+	 	$data['reactivo'] = $this->reactivo_model->reactivo($this->uri->segment(3))->row_array();
+	 	$data['pregunta'] = $this->pregunta_model->pregunta($data['reactivo']['idreactivo'])->row_array();
+	 	$data['preguntas'] = $this->pregunta_model->pregunta($data['reactivo']['idreactivo'])->result();
+	 	$data['respuesta'] = $this->respuesta_model->respuesta($data['pregunta']['idpregunta'])->row_array();
+	 	$data['respuestas'] = $this->respuesta_model->reactivo($data['pregunta']['idpregunta'])->result();
+		$data['title']="Reactivo";
+		$this->load->view('reactivo_list_pdf',$data);
+	}
+
+
+
+
+
 
 
 
---
+++ application/views/reactivo_list_pdf.php
@@ -0,0 +1,79 @@
+<?php
+
+	include 'plantilla.php';
+        
+	$pdf = new PDF();
+	$pdf->SetMargins(23, 10, 11.7);
+
+
+	$pdf->institucion='UNIVERSIDAD TÉCNICA LUIS VARGAS TORRES DE ESMERALDAS';
+	$pdf->unidad='FACULTAD DE INGENIERIAS (FACI)';
+	$pdf->departamento='CARRERA EN TECNOLOGÍA DE LA INFORMACIÓN';
+	$pdf->titulo="Reactivo";
+	
+
+
+	$pdf->AliasNbPages();
+	$pdf->AddPage();
+
+
+
+	$pdf->SetFont('Arial','',12);
+	$pdf->SetTextColor(0, 0,0);
+    	$pdf->Text(20,40,"Asignatura:  ".$reactivo[0]->nombre); 
+
+	$pdf->SetFillColor(232,232,232);
+	$pdf->SetFont('Arial','B',8);
+
+
+	$pdf->Cell(10,5,'idestu',1,0,'C',1);
+	$pdf->Cell(60,5,'institucion',1,0,'C',1);
+	$pdf->Cell(10,5,'nivel',1,0,'C',1);
+	$pdf->Cell(70,5,'titulo',1,1,'C',1);
+ 
+
+	$pdf->SetFont('Arial','',7);
+	$id=0;
+	$persona="";
+	$i=0;
+	foreach ($preguntas as $row){  //Recorre todas la participaciones realiadas por los participantes
+	       
+		    $i=$i+1;
+		    $pdf->Cell(10,5,$i,1,0,'R',0); 
+		    $pdf->Cell(60,5,utf8_decode($row->pregunta),1,1,'L',0);
+
+		foreach ($respuestas as $row1){  //Recorre todas la participaciones realiadas por los participantes
+
+		    $pdf->Cell(60,5,utf8_decode($row1->respuesta),1,1,'L',0);
+		}
+
+    }
+
+    
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	$pdf->Output();
+?>
