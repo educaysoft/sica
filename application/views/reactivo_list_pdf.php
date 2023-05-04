89544e5dd5cad6104a75e6f32eb248e2cde9822e
Stain Francis <educaysoft@gmail.com>
2023-05-04 01:53:37 -0400

    sesino evernto

--- application/views/reactivo_list_pdf.php
+++ application/views/reactivo_list_pdf.php
@@ -25,9 +25,7 @@
 
     	$pdf->Text(20,45,"Instrucciones : "); 
 
-$current_y = $pdf->GetY();
-$current_x = $pdf->GetX();
-		    $pdf->MultiCell(20,5,utf8_decode($reactivos[0]->detalle),1);
+		    $pdf->MultiCell(40,5,utf8_decode($reactivos[0]->detalle),0);
 	$pdf->SetFillColor(232,232,232);
 	$pdf->SetFont('Arial','B',8);
 
