f38e9619fbeb45d42df967e9269bee12a9b4aba1
Stain Francis <educaysoft@gmail.com>
2023-05-05 18:09:28 -0400

    habilidad linkroto

--- application/views/evento_cumplimiento.php
+++ application/views/evento_cumplimiento.php
@@ -27,7 +27,7 @@
 
 
 
-<div style="display:flex; flex-direction:row; padding:10px; border:2px red solid;">
+<div style="display:flex; flex-direction:row; padding:10px; border:2px red solid;justify-content: space-around;">
 
 
 <div style="padding:20px;">
@@ -77,7 +77,7 @@
 
 
 </div>
-<div>
+<div >
 
 
 
