f41393b7b0f38227324909fadbef3ec547c3962d
Stain Francis <educaysoft@gmail.com>
2023-05-05 04:01:09 -0400

    habilidad linkroto

--- application/views/pregunta_record.php
+++ application/views/pregunta_record.php
@@ -121,8 +121,7 @@ echo form_input('idreactivo',$options[$pregunta['idreactivo']],array("disabled"=
 <script type="text/javascript">
 
 $(document).ready(function(){
-	var idpregunta=document.getElementById("idpregunta").innerHTML;
-alert(idpregunta);
+	var idpregunta=document.getElementById("idpregunta").value;
 	var mytablaf= $('#mydatar').DataTable({"ajax": {url: '<?php echo site_url('reactivo/reactivo_respuesta2')?>', type: 'GET',data:{idpregunta:idpregunta}},});
 
 
