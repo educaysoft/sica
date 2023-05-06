1fce609f0cb70f6fe9c6ffc7773c0be1ae0bfd7f
Stain Francis <educaysoft@gmail.com>
2023-05-05 20:55:29 -0400

    habilidad linkroto

--- application/views/cursos/FundamentosDeProgramacion_clases.php
+++ application/views/cursos/FundamentosDeProgramacion_clases.php
@@ -540,14 +540,16 @@ function get_pregunta(idpregunta,idx) {
 		{
 		html+="<div>";
 		html += '<input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+')"  checked >';
-		html += '  <label for="huey" style="color:red">'+data[i].respuesta+'</label>';
+		//html += '  <label for="huey" style="color:red">'+data[i].respuesta+'</label>';
+		html += '  <label for="huey" style="color:black">'+data[i].respuesta+'</label>';
 		html+="</div>";
 		}
 		else if(data[i].idrespuesta==idrespuesta && acierto==1)
 		{
 		html+="<div>";
 		html += '<input type="radio" id="'+j+'" name="respuesta" id="'+i+'" value="'+data[i].respuesta+'" onclick="evaluado('+data[i].idreactivo+','+data[i].acierto+','+data[i].idpregunta+','+idpersona+','+data[i].idrespuesta+')"  checked>';
-		html += '  <label for="huey" style="color:green">'+data[i].respuesta+'</label>';
+		//html += '  <label for="huey" style="color:green">'+data[i].respuesta+'</label>';
+		html += '  <label for="huey" style="color:black">'+data[i].respuesta+'</label>';
 		html+="</div>";
 		}
 		else
