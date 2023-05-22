60b6538dec0b4eeb35b0c2622806cda72cd9207e
Stain Francis <educaysoft@gmail.com>
2023-05-21 22:56:35 -0400

    mejora a paginas educaysoft

--- application/controllers/Silabo.php
+++ application/controllers/Silabo.php
@@ -14,6 +14,7 @@ class Silabo extends CI_Controller{
   	$this->load->model('evento_model');
   	$this->load->model('docente_model');
   	$this->load->model('malla_model');
+  	$this->load->model('departamento_model');
   }
 
 //=========================================================
