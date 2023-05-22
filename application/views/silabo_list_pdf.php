ca44d83c251a9adf2a0ed769d2e08202dcb19f77
Stain Francis <educaysoft@gmail.com>
2023-05-21 23:07:36 -0400

    mejora a paginas educaysoft

--- application/views/silabo_list_pdf.php
+++ application/views/silabo_list_pdf.php
@@ -116,7 +116,7 @@ $pdf->Ln(20);
 
 			$pdf->SetXY($current_x, $current_y);   
 			$cell_width=70;
-		    $pdf->MultiCell($cell_width,5,utf8_decode($row->nombrecorto),1);
+		    $pdf->MultiCell($cell_width,5,utf8_decode($row->nombrecorto),1,'L',$fill);
 	 	 	$current_x+=$cell_width;
 			$current_y2 = $pdf->GetY();
 			if($current_y==$pdf->GetY()-5){
@@ -127,7 +127,7 @@ $pdf->Ln(20);
 			$pdf->SetXY($current_x, $current_y);   
 			$cell_width=150;
 			$l=strlen($row->nombrelargo);
-		    $pdf->MultiCell($cell_width,5,utf8_decode($row->nombrelargo),1);
+		    $pdf->MultiCell($cell_width,5,utf8_decode($row->nombrelargo),1,'L',$fill);
 	 	 	$current_x+=$cell_width;
 			if($current_y2< $pdf->GetY()){
 				$current_y2 = $pdf->GetY();
