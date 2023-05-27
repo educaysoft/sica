a361ea48862d52a2560949e883bf56dcab11e2fd
Stain Francis <educaysoft@gmail.com>
2023-05-26 20:38:51 -0400

    departamento del funcionario

--- application/controllers/Funcionario.php
+++ application/controllers/Funcionario.php
@@ -8,8 +8,7 @@ class Funcionario extends CI_Controller{
   	  $this->load->model('departamento_model');
   	  $this->load->model('cargo_model');
   	  $this->load->model('funcionario_model');
-  	  $this->load->model('estudio_model');
-  	  $this->load->model('silabo_model');
+  	  $this->load->model('departamentofuncionario_model');
 }
 
 public function index(){
@@ -157,7 +156,7 @@ public function actual(){
 
 
 
-	function funcionario_data()
+	function departamento_data()
 	{
 			$draw= intval($this->input->get("draw"));
 			$draw= intval($this->input->get("start"));
@@ -183,52 +182,8 @@ public function actual(){
 
 
 
-	function silabo_data()
-	{
-			$draw= intval($this->input->get("draw"));
-			$draw= intval($this->input->get("start"));
-			$draw= intval($this->input->get("length"));
-
-			$idfuncionario=$this->input->get('idfuncionario');
-			$data0 =$this->silabo_model->silaboss($idfuncionario);
-			$data=array();
-			foreach($data0->result() as $r){
-				$data[]=array($r->idfuncionario,$r->idsilabo,$r->elsilabo,$r->elperiodo,
-				$r->href='<a href="javascript:void(0);" class="btn btn-info btn-sm item_ver"  data-retornos="'.site_url('silabo/actual').'"    data-idsilabo="'.$r->idsilabo.'">Ver</a>');
-			}	
-			$output=array( "draw"=>$draw,
-				"recordsTotal"=> $data0->num_rows(),
-				"recordsFiltered"=> $data0->num_rows(),
-				"data"=>$data
-			);
-			echo json_encode($output);
-			exit();
-	}
 
 
-
-
-	function estudio_data()
-	{
-			$draw= intval($this->input->get("draw"));
-			$draw= intval($this->input->get("start"));
-			$draw= intval($this->input->get("length"));
-
-			$idpersona=$this->input->get('idpersona');
-			$data0 =$this->estudio_model->estudios($idpersona);
-			$data=array();
-			foreach($data0->result() as $r){
-				$data[]=array($r->idpersona,$r->idestudio,$r->lainstitucion,$r->nivel,$r->titulo,
-				$r->href='<a href="javascript:void(0);" class="btn btn-info btn-sm item_vere"  data-retornoe="'.site_url('estudio/actual').'"    data-idestudio="'.$r->idestudio.'">Ver</a>');
-			}	
-			$output=array( "draw"=>$draw,
-				"recordsTotal"=> $data0->num_rows(),
-				"recordsFiltered"=> $data0->num_rows(),
-				"data"=>$data
-			);
-			echo json_encode($output);
-			exit();
-	}
 
 
 
