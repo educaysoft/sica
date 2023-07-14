5035ffcba4c20c025ea69fe7dd2c0bcd42182245
Stain Francis <educaysoft@gmail.com>
2023-07-14 00:17:51 -0400

    nuevos cursos

--- application/views/cursos/2023/cursosparadocentes2023-2.php
+++ application/views/cursos/2023/cursosparadocentes2023-2.php
@@ -159,7 +159,7 @@ overflow:hidden;
           <div class="card shadow-sm">
 		<a  href='https://educaysoft.org/sica/evento/detalle/332'><svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><image href="<?php echo base_url(); ?>images/cursos/didactica.jpg" height="100%" width="100%"/>  </svg></a>
 <div class='img-contenedor w3-card-4' style='position:absolute; top:0px;right:0px; border: 2px solid green; border-radius: 50%; width: 30%; display:flex; justify-content: center; align-items: center;'>
-<img src='https://repositorioutlvte.org/Repositorio/fotos/1757529738.jpg' width='100%' height='100%' style='border-radius:50px;'>
+<img src='https://repositorioutlvte.org/Repositorio/fotos/0801215328.jpg' width='100%' height='100%' style='border-radius:50px;'>
 </div>
 
             <div class="card-body">
