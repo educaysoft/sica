4bd7284d4da67f19cb7fc04fb59ff4faf3d57354
Stain Francis <educaysoft@gmail.com>
2023-07-24 03:26:17 -0400

    mejoras

--- application/controllers/Evento.php
+++ application/controllers/Evento.php
@@ -372,10 +372,6 @@ public function cumplimiento(){
 			$data[]=array($r->idevento,$r->titulo,$r->fechainicia,$r->fechafinaliza,$r->eltutor,
 			$r->href='<a href="javascript:void(0);" class="btn btn-info btn-sm item_ver"  data-retorno="'.site_url('evento/actual').'"    data-idevento="'.$r->idevento.'">Edit</a><a href="javascript:void(0);" class="btn btn-info btn-sm item_ver2"  data-retorno2="'.site_url('evento/detalle').'"    data-idevento2="'.$r->idevento.'">Ver</a>');
 				
-			}else{
-
-			$data[]=array($r->idevento,$r->titulo,$r->fechainicia,$r->fechafinaliza,$r->eltutor,
-			$r->href='<a href="javascript:void(0);"  class="btn btn-info btn-sm item_ver2"  data-retorno2="'.site_url('evento/detalle').'"    data-idevento2="'.$r->idevento.'">Ver</a>');
 			}	
 			}	
 			}
--- application/views/evento_xpersona.php
+++ application/views/evento_xpersona.php
@@ -38,7 +38,7 @@ body {font-family: Arial, Helvetica, sans-serif;}
 <div class="row" style="background-color:green; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-	     <b>Lista de eventos activos dictados por:  <?php echo $persona[0]->apellidos; ?> <?php echo "  "; ?>  <?php echo $persona[0]->nombres; ?>    	</b>
+	     <b style="color:white">Lista de eventos activos dictados por:  <?php echo $persona[0]->apellidos; ?> <?php echo "  "; ?>  <?php echo $persona[0]->nombres; ?>    	</b>
         </div>
         
     </div>
