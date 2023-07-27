a6ea46ecadc845e7a9f3884ddadac2372506e3eb
Stain Francis <educaysoft@gmail.com>
2023-07-27 17:26:27 -0400

    mejoras

--- application/views/calendarioacademico_record.php
+++ application/views/calendarioacademico_record.php
@@ -122,11 +122,22 @@ if(isset($calendarioacademico))
 
 <div class="form-group row">
     <label class="col-md-2 col-form-label"> <?php echo anchor('fechacalendario/add', 'Fechas'); ?>: </label>
+
 	<div class="col-md-10">
 	<div class="row justify-content-left">
       	<!-- Page Heading -->
  	<div class="row">
-  	<div class="col-12">
+  	<div class="col-12" style="border:solid;">
+
+	<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+	    <div class="col-lg-12 margin-tb">
+		<div class="pull-left">
+		    <b>Fecha planificadas: </b>
+
+		</div>
+	    </div>
+	</div>
+
 	<table class="table table-striped table-bordered table-hover" id="mydatac">
 	 <thead>
 	 <tr>
--- application/views/persona_record.php
+++ application/views/persona_record.php
@@ -290,18 +290,14 @@ if(isset($persona))
  	<div class="row">
   	<div class="col-12" style="border:solid;">
 
-<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <b>Documentos subidos: </b>
+	<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
+	    <div class="col-lg-12 margin-tb">
+		<div class="pull-left">
+		    <b>Documentos subidos: </b>
 
-        </div>
-    </div>
-</div>
-
-
-
-
+		</div>
+	    </div>
+	</div>
 
 
 	<table class="table table-striped table-bordered table-hover" id="mydatac">
