1df16ddb7e0ef26281cf002f80263249e29d6ffd
Stain Francis <educaysoft@gmail.com>
2023-08-13 18:49:35 -0400

    mejoras

--- application/views/distributivodocente_genpagina.php
+++ application/views/distributivodocente_genpagina.php
@@ -1,9 +1,188 @@
 <?php
 
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
+$myfile = fopen("application/view/cursos/2023-1S.txt", "w") or die("Unable to open file!");
+$txt =`
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Stalin Francis Quinde">
+    <meta name="generator" content="Hugo 0.101.0">
+        <meta property="og:site_name" content="Educación Continua-UTLVTE" />
+        <meta property="og:image" content="https://educaysoft.org/sica/images/LogoEducacionContinua.png" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="400" />
+    <title>Cursos de Perfeccionamiento Académico y Ética</title>
+
+    <link rel="educaysoft" href="https://congresoutlvte.org/faci/">
+    
+<link rel="stylesheet" type="text/css"  href="<?php echo base_url(); ?>assets/dist/css/bootstrap.min.css" />
+    <style>
+
+.img-contenedor img {
+-webkit-transition:all .9s ease; /* Safari y Chrome */
+-moz-transition:all .9s ease; /* Firefox */
+-o-transition:all .9s ease; /* IE 9 */
+-ms-transition:all .9s ease; /* Opera */
+width:100%;
+}
+
+.img-contenedor:hover img {
+-webkit-transform:scale(1.25);
+-moz-transform:scale(1.25);
+-ms-transform:scale(1.25);
+-o-transform:scale(1.25);
+transform:scale(1.25);
+}
+
+.img-contenedor {/*Ancho y altura son modificables al requerimiento de cada uno*/
+/*width:300px;*/
+/*height:180px;*/
+overflow:hidden;
+}
+
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+  .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
+    
+  </head>
+
+ <body>
+
+<header>
+  <div class="collapse bg-secondary" id="navbarHeader">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 col-md-7 py-4">
+          <h4 class="text-white">Acerca de Educación Continua</h4>
+          <p class="text-light"> Artículo 48:La educación continua hace referencia a procesos de capacitación, actualización y certificación de competencias laborales específicas. Se ejecuta en forma de cursos, seminarios, talleres u otras actividades académicas.</p>
+
+        </div>
+        <div class="col-sm-4 offset-md-1 py-4">
+          <h4 class="text-white">Contactanos</h4>
+          <ul class="list-unstyled">
+            <li><a href="https://twitter.com/educaysoft1" class="text-white">Siguenos en Twitter</a></li>
+            <li><a href="https://www.facebook.com/stalin.francis.7/" class="text-white">Siguenos en  Facebook</a></li>
+            <li><a href="mailto:educaysoft@educaysoft.org" class="text-white">Mensajeame</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="navbar navbar-light bg-light shadow-sm" aria-label="Light offcanvas navbar">
+    <div class="container">
+
+<a class="navbar-brand" href="https://www.utelvt.edu.ec/site/" target="_blank">
+      <img src="https://congresoutlvte.org/images/logoutlvte.png" alt="..." height="45">
+    </a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </div>
+</header>
+
+\n`;
+
+
+
+
+
 fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
+$txt = `
+
+<footer class="text-muted py-5">
+  <div class="container">
+    <p class="float-end mb-1">
+      <a href="#">Back to top</a>
+    </p>
+    <p class="mb-1">Cursos de Educación Continua UTLVTE fue creado por &copy; EDUCAYSOFT, como parte de un proyecto de desarrollo de software universitario</p>
+    <p class="mb-0">Lo nuevo de Educación Continua? <a href="https://educaysoft.org/">Visit the homepage</a> cursos gratis para desarrolladores <a href="https://educaysoft.org/cursos">Cursos gratis</a>.</p>
+  </div>
+</footer>
+
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.4.0/mdb.min.js"
+></script>
+
+<script type="text/javascript">
+
+$(".submenu").click(function(){
+  $(this).children("ul").slideToggle();
+})
+
+$("ul").click(function(ev){
+  ev.stopPropagation();
+})
+
+function cargarVideo(url){
+        document.getElementById('slider').src=url;
+}
+
+</script>
+    <script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
+      
+  </body>
+</html>
+                                   
+\n`;
+
+
+
 fwrite($myfile, $txt);
 fclose($myfile);
 
