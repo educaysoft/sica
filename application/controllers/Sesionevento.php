8b7236cf26f133074fc6a0ae001310636281ac50
Stain Francis <educaysoft@gmail.com>
2023-08-19 12:14:28 -0400

    mejoras

--- application/controllers/Sesionevento.php
+++ application/controllers/Sesionevento.php
@@ -118,7 +118,7 @@ class Sesionevento extends CI_Controller{
 		$puede= $this->fechacalendario_model->existe($data['evento']['idsilabo'],$date);
 		$data['calendarioacademico'] = $this->calendarioacademico_model->lista_calendarioacademicosA($data['evento']['idcalendarioacademico'])->result();
 		$data['sesionevento'] = $this->sesionevento_model->sesionevento_sesiones($idevento)->result();
-		$data['title']="<div style='padding:30px; text-align:left; background:black; color:white; font-size:30px;'> <h1>Nueva sesion del evento</h1><p>En este formulario se ingresa el plan a seguir para este dia de sesión del evento</p> </div>";
+		$data['title']="<div style='padding:30px; text-align:left; background:black; color:white; font-size:30px;'> <p style='font-size:50px; font-weight:bold; text-transform:uppercase;' >Nueva sesión del evento</h1><p>En este formulario se ingresa el plan a seguir para este dia de sesión del evento</p> </div>";
 	 	$this->load->view('template/page_header');		
 	 	$this->load->view('sesionevento_form',$data);
 	 	$this->load->view('template/page_footer');
