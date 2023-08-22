c9beee2881db5c70cfabd8b665fdc1e46d1a0af5
Stain Francis <educaysoft@gmail.com>
2023-08-22 10:40:50 -0400

    mejkoras

--- application/views/sesionevento_edit.php
+++ application/views/sesionevento_edit.php
@@ -38,6 +38,7 @@ echo form_dropdown("idevento",$options, $sesionevento['idevento']);
 
 $sesiondictada= array();
 	$numerosesiondictada= array();
+	$k=1;
 foreach ($sesioneventos as $row){
 	$sesiondictada[$row->fecha]= $row->idsesionevento;
 	$numerosesiondictada[$k]= $row->idsesionevento;
