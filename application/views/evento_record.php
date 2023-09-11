39261025697100170566c54f041baca25a8b9e72
Stain Francis <educaysoft@gmail.com>
2023-09-10 22:20:45 -0400

    mejoras:

--- application/views/evento_record.php
+++ application/views/evento_record.php
@@ -330,7 +330,7 @@ echo form_input($arrdatos) ?>
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> <?php echo anchor('horasasignatura/add/'.$asignatura['idasignatura'], 'Harario:'); ?>:</label>
+    <label class="col-md-2 col-form-label"> Horario:</label>
 	<div class="col-md-10">
 	<?php
  	$options = array();
