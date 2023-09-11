cc7b332f6eb69b6d90e6ab75986dcf2e81e886cd
Stain Francis <educaysoft@gmail.com>
2023-09-10 22:14:20 -0400

    mejoras:

--- application/controllers/Evento.php
+++ application/controllers/Evento.php
@@ -279,6 +279,7 @@ public function cumplimiento(){
 		$data['paginas']= $this->pagina_model->lista_paginas()->result();
 		$data['asignaturadocentes'] = $this->asignaturadocente_model->lista_asignaturadocentesA(0)->result();
 		$data['calendarioacademicos'] = $this->calendarioacademico_model->lista_calendarioacademicosA(0)->result();
+		$data['jornadadocente'] =$this->jornadadocente_model->jornadadocentes($data['evento']['idasignaturadocente']);
 		$data['title']="<div style='padding:30px; text-align:left; background:black; color:white; font-size:30px;'> <p style='font-size:40px; font-weight:bold; text-transform:uppercase;' >Detalle del evento # ". $data['evento']['idevento']."</p>
 		<p>En este formulario esta toda la información de evento que este dirigiendo</p> </div>";
 
