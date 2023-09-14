3d973e24e893dff2f37182d416805356268e573c
Stain Francis <educaysoft@gmail.com>
2023-09-14 18:02:25 -0400

    mejoras

--- application/controllers/Evento.php
+++ application/controllers/Evento.php
@@ -584,8 +584,8 @@ public function genpagina()
 		$data['participacionB1'] = $this->participacion_model->ParticipacionxPersonaB1($data['evento']['idevento'])->result();
 		$data['participacionC1'] = $this->participacion_model->ParticipacionxPersonaC1($data['evento']['idevento'])->result();
 
-
-
+		print_r('participacionesp');
+		die();
 
 		$data['title']="Evento";
 		$this->load->view('evento_genpagina',$data);
