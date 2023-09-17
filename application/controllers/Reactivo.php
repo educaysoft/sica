283bcaa900c2fce834821d9ded06997b407a46f8
Stain Francis <educaysoft@gmail.com>
2023-09-17 17:16:13 -0400

    mejoras

--- application/controllers/Reactivo.php
+++ application/controllers/Reactivo.php
@@ -112,7 +112,7 @@ function reactivo_data()
 		$data=array();
 		foreach($data0->result() as $r){
 			$data[]=array($r->idreactivo,$r->laasignatura,$r->nombre,
-			$r->href='<a href="javascript:void(0);" class="btn btn-info btn-sm item_ver" data-retorno="'.site_url('ractivo/actual').'"   data-idreactivo="'.$r->idreactivo.'">Ver</a>');
+			$r->href='<a href="javascript:void(0);" class="btn btn-info btn-sm item_ver" data-retorno="'.site_url('reactivo/actual').'"   data-idreactivo="'.$r->idreactivo.'">Ver</a>');
 		}	
 		$output=array( "draw"=>$draw,
 			"recordsTotal"=> $data0->num_rows(),
