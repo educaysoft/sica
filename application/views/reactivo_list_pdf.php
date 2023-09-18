6f1479d83fdc8f224b2b49a60df9fadc9db4d85e
Stain Francis <educaysoft@gmail.com>
2023-09-17 23:35:20 -0400

    mejoras

--- application/views/reactivo_list_pdf.php
+++ application/views/reactivo_list_pdf.php
@@ -43,9 +43,11 @@
 		    $i=$i+1;
 		    $pdf->Cell(10,5,$i,1,0,'R',0); 
 		//	$pdf->MultiCell(150,5,utf8_decode($row->pregunta),1);
-$pdf->WriteHTML($row->pregunta);
+		$pdf->WriteHTML($row->pregunta);
 		  //  $pdf->Cell(150,5,utf8_decode($row->pregunta),1,1,'L',0);
-		    $pdf->Ln();
+	    	$pdf->Ln();
+		$pdf->Cell( 5, 5, $pdf->Image($row->linkimagen, $pdf->GetX(), $pdf->GetY(), 4), 0, 0, 'R', false );
+	    	$pdf->Ln();
 
 		foreach ($respuestas as $row1){  //Recorre todas la participaciones realiadas por los participantes
 	            if($row1->idreactivo==$row->idreactivo && $row1->idpregunta==$row->idpregunta)
