afed7c33338dd142f457212f262b9eca87d13209
Stain Francis <educaysoft@gmail.com>
2023-09-18 15:01:12 -0400

    mejoras

--- application/views/evento_list_pdf.php
+++ application/views/evento_list_pdf.php
@@ -1,77 +1,79 @@
 <?php
-ini_set ('error_reporting', E_ALL);
+
 
 	include 'plantilla.php';
-//	require 'conexion.php';
 	
-//	$query = "SELECT e.estado, m.id_municipio, m.municipio FROM t_municipio AS m INNER JOIN t_estado AS e ON m.id_estado=e.id_estado";
-//	$resultado = $mysqli->query($query);
 
-/*
 
-	if(isset($_GET["idparticipanteestado"]))
-	{
-		$idparticipanteestado=$_GET["idparticipanteestado"];
-	}else{
-		$idparticipanteestado=0;
-	}
         
-
-
-	if(isset($_GET["idpersona"]))
-	{
-		$idpersona=$_GET["idpersona"];
-	}else{
-		$idpersona=0;
-	}
- */      
 	$pdf = new PDF();
 	$pdf->SetMargins(23, 10, 11.7);
+
+
 	$pdf->institucion='UNIVERSIDAD TÉCNICA LUIS VARGAS TORRES DE ESMERALDAS';
 	$pdf->unidad='FACULTAD DE INGENIERIAS (FACI)';
 	$pdf->departamento='CARRERA EN TECNOLOGÍA DE LA INFORMACIÓN';
-//	$pdf->titulo=$evento['titulo'];
+	$pdf->titulo="Plan de clase";
 	
 
 
 	$pdf->AliasNbPages();
 	$pdf->AddPage();
-	
+
+
+
+	$pdf->SetFont('Arial','',14);
+	$pdf->SetTextColor(0, 0,0);
+    	$pdf->Text(20,40,"  "); 
+
 	$pdf->SetFillColor(232,232,232);
-	$pdf->SetFont('Arial','B',8);
+	$pdf->SetFont('Arial','B',12);
 
 
-//	$pdf->Cell(5,5,'#',1,0,'C',1);
-//	$pdf->Cell(50,5,'Participante',1,0,'C',1);
-//	$pdf->Cell(70,5,'Archivo PDF',1,1,'C',1);
- 
-	 
 
 
-//	$pdf->SetFont('Arial','',7);
 
+/*	 
+
+
+	$pdf->SetFont('Arial','',7);
 	$id=0;
 	$persona="";
 	$i=0;
-//	foreach ($participantes as $row){  //Recorre todas la participaciones realiadas por los participantes
+	foreach ($estudios as $row){  //Recorre todas la participaciones realiadas por los participantes
 	       
-//		    $i=$i+1;
-//		    $pdf->Cell(5,5,$i,1,0,'R',0); 
-		    //$pdf->Cell(60,5,utf8_decode($row->nombres),1,0,'L',0);
-//		    $pdf->Cell(50,5," ",1,0,'L',0);
-		   // if(isset($row->archivopdf)){
-		   //	 $pdf->Cell(90,5,"https://repositorioutlvte.org/Repositorio/".utf8_decode($row->archivopdf),1,1,'L',0);
-		   // }else{
-//		    	$pdf->Cell(70,5,"---",1,1,'L',0);
-		   // }
+		    $i=$i+1;
+		    $pdf->Cell(10,5,$row->idestudio,1,0,'R',0); 
+		    $pdf->Cell(60,5,utf8_decode($row->lainstitucion),1,0,'L',0);
+		    $pdf->Cell(10,5,utf8_decode($row->nivel),1,0,'L',0);
+		    $pdf->Cell(70,5,utf8_decode($row->titulo),1,1,'L',0);
 
-
-  //  }
+    }
+ */
+    
 
 
 
+ 
 
-//	header('Content-type: application/pdf');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	$pdf->Output();
