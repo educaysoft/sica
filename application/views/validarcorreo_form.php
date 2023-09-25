8ffc53deb3c5c32adae66f94020f64e4e8b3736c
Stalin Francis Quinde <educaysoft@gmail.com>
2023-09-25 14:38:06 -0500

    mejoras a validarcorreo_form.php
    se mejorar en en envio de correo

--- application/views/validarcorreo_form.php
+++ application/views/validarcorreo_form.php
@@ -201,11 +201,14 @@ $('#validarcorreo').click(function() {
 		 var idevento=<?php echo $idevento ?>;
 		 var titulo=document.getElementById("titulo").value; //   "stalin.francis@utelvt.edu.ec";
 		 var email="educacioncontinua@utelvt.edu.ec";
+		 var correode="educacioncontinua@utelvt.edu.ec";
 		 var nome= 'Stalin Francis Q.'; // document.getElementById("lapersona_edit").value; 		
                  var msg="Ingresa al siguiente link para terminar tu registro y poder recibir tu certificado al culminar el evento <br><br><a href='https://educaysoft.org/sica/index.php/login/registro?idevento="+idevento+"' style='text-align:center;'><b>Evento :</b>"+titulo+"</a><br><br>";  //tinyMCE.activeEditor.getContent({format:'text'});
 		 var mailto=document.getElementById("email").value; //   "stalin.francis@utelvt.edu.ec";
+		 var correopara=mailto;
 		 var secure="siteform";
 		 var head="";
+		  var asunto="Completar registro a SIGECA";
 			
 		var foot0="<br><div style='text-align:center; background-color:lightgrey;'> Aprovechamos la oportunidad para informarte que la Universidad Técnica Luis Vargas Torres esta ejecutando los siguientes programas de capacitación para el perfeccionamiento de nuestros docentes.<br><br> <a href='https://educaysoft.org/sica/curso/CursosEducacionContinua2023'>Cursos de Educación Continua</a><br><br></div>" ;
 		 var foot=" <div style='text-align:center; background-color:lightgrey; font-size:12px;'> Este correo ha sido enviado a "+mailto+ ", de acuerdo a la Ley Orgánica de Protección de datos, usted tiene el derecho a solicitar a la Universidad Técnica Luis Vargas Torres, la actualización, inclusión, supresión y/o tratamiento de los datos personales incluidos en sus bases de datos, con este correo electrónico usted acepta recibir información de las actividades académicas que realiza el Alma Mater así como nuestra propuestas académicas <br><br> Este correo fue generado y enviado automáticamente desde el sistema cloud elaborado de la Maestría en Tecnología de la Información</div> ";
@@ -214,7 +217,7 @@ $('#validarcorreo').click(function() {
 
 	    $.ajax({
 		url: "<?php echo site_url('seguimiento/send') ?>",
-		data: {nome:nome, email:email, msg:msg, mailto:mailto, secure:secure},
+		data: {nome:nome, correode:correode,correopara:correopara,email:email, msg:msg, mailto:mailto, secure:secure,asunto:asunto},
 		method: 'POST',
 		async : false,
 		success: function(data){
