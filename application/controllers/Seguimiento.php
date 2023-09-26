f76a9bd1c26f170dc697c5c3484649151ba28a97
Stain Francis <educaysoft@gmail.com>
2023-09-26 19:22:00 -0400

    mejoras

--- application/controllers/Seguimiento.php
+++ application/controllers/Seguimiento.php
@@ -506,39 +506,39 @@ public function sendhotmail()
 	$this->load->database();
         $this->load->helper('form','language');
 
-    if ($this->input->post('secure') != 'siteform') {
-        echo lang('erro_no_js');
-    }else{
+//    if ($this->input->post('secure') != 'siteform') {
+  //      echo lang('erro_no_js');
+   // }else{
 
-       if($this->input->post('idpersona'))
-	{
-	$condition="idpersona=".$this->input->post('idpersona')." and idcorreo_estado=1";
-		$this->db->select('*');
-		$this->db->from('correo');
-		$this->db->where($condition);
-		$this->db->limit(1);
-		$query=$this->db->get();
-		if($query->num_rows() >0) {
-			$mailto=$query->result()[0]->nombre;
-
-		}else{
-        		$mailto = $this->input->post('mailto');
-		}
-	}else{
-       		$mailto = $this->input->post('mailto');
-	}
+     //  if($this->input->post('idpersona'))
+//	{
+//	$condition="idpersona=".$this->input->post('idpersona')." and idcorreo_estado=1";
+//		$this->db->select('*');
+//		$this->db->from('correo');
+//		$this->db->where($condition);
+///		$this->db->limit(1);
+//		$query=$this->db->get();
+//		if($query->num_rows() >0) {
+//			$mailto=$query->result()[0]->nombre;
+//
+//		}else{
+  //      		$mailto = $this->input->post('mailto');
+//		}
+//	}else{
+       		$mailto = $this->input->post('correopara');
+//	}
 
         $this->load->library('email');
         $nome = $this->input->post('nome');
         $msg = str_replace("stalin.francis@utelvt.edu.ec",$mailto, $this->input->post('msg'));
         $secure = $this->input->post('secure');
 	$email= $this->input->post('email');
-        $config['protocol'] = "ssmtp";
-        $config['smtp_host'] = "ssl://smtp.gmail.com";
-        $config['smtp_port'] = "465";
+        $config['protocol'] = "smtp";
+        $config['smtp_host'] = "smtp.office365.com";
+        $config['smtp_port'] = "587";
         $config['smtp_timeout'] = "7";
-        $config['smtp_user'] ="maestria.ti@utelvt.edu.ec"; // $this->settings['smtp_email'];
-        $config['smtp_pass'] ="PIwiIB2@3#"; //  $this->settings['smtp_password'];
+        $config['smtp_user'] =$email; // $this->settings['smtp_email'];
+        $config['smtp_pass'] ="SAfq1234"; //  $this->settings['smtp_password'];
         $config['charset'] = "utf-8";
         $config['mailtype'] = "html";
         $config['newline'] = "\r\n";
