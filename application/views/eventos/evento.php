a9054e0cd4962ba1d72899371220f9af85e64d6b
Stain Francis <educaysoft@gmail.com>
2023-10-05 01:30:02 -0400

    mejoras

--- application/views/eventos/evento.php
+++ application/views/eventos/evento.php
@@ -316,34 +316,18 @@
 		<?php } ?>
           </div>
 
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
+          <div class="col-sm-1">
+		<?php if($row->aprendizajeautonomo==""){ ?>
+			<?php echo ""; ?>
+		<?php }else{ 
+			echo '<button onclick="window.modal'.$row->numerosesion.'.showModal();">tarea</button>';
+  			echo '<dialog id="modal'.$row->numerosesion.'">';
+			echo '<h3>Tarea para aprendizaje autonomo<?php echo $row->idtema; ?></h3>';
+    			echo $row->aprendizajeautonomo.' <br>';
+  			echo '<center><button onclick="window.modal'.$row->numerosesion.'.close();">Cerrar</button></center>';
+			echo '</dialog>';
+			 } ?>
+          </div>
 
 <?php
 if($row->numerosesion==24){
@@ -351,7 +335,7 @@ if($row->numerosesion==24){
     die();
  }
   
-?>
+
 
 
 
