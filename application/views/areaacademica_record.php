541827c4edeccdc98e54e92563b9e545492f0a9b
Stalin Francis <educaysoft@gmail.com>
2023-10-18 03:20:04 -0500

    mejoras

--- application/views/areaacademica_record.php
+++ application/views/areaacademica_record.php
@@ -93,6 +93,7 @@
 
 $(document).ready(function(){
 	var idareaacademica=document.getElementById("idareaacademica").innerHTML;
+	alert(idareaacademica);
 	var mytablaf= $('#mydatae').DataTable({"ajax": {url: '<?php echo site_url('miembroareaacademica/miembroareaacademica_data')?>', type: 'GET',data:{idareaacademica:idareaacademica}},});
 
 
