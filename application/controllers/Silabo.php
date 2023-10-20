89d56e89542f9e2d9581faf7d8ca6b44e70e8488
Stalin Francis <educaysoft@gmail.com>
2023-10-19 22:41:43 -0500

    mejoras

--- application/controllers/Silabo.php
+++ application/controllers/Silabo.php
@@ -169,7 +169,7 @@ function silabo_data()
 			$data0 =$this->unidadsilabo_model->lista_unidades($idsilabo);
 			$data=array();
 			foreach($data0->result() as $r){
-				$data[]=array($r->idsilabo,$r->idunidadsilabo,$r->unidad,$r->nombre,$r->sesiones,
+				$data[]=array($r->idsilabo,$r->idunidadsilabo,$r->unidad,$r->launidad,$r->sesiones,
 				$r->href='<a href="javascript:void(0);" class="btn btn-info btn-sm item_ver"  data-retorno="'.site_url('unidadsilabo/actual').'"    data-idunidadsilabo="'.$r->idunidadsilabo.'">Ver</a>');
 			}	
 			$output=array( "draw"=>$draw,
