52036df328267280123e6751864f961e20ecbc6f
Stalin Francis <educaysoft@gmail.com>
2023-10-20 17:18:07 -0500

    mejkora

--- application/views/seguimiento_form.php
+++ application/views/seguimiento_form.php
@@ -269,8 +269,10 @@ echo '<a class="btn"  onclick="enviar_correo(`<img src=\'data:image/jpg;base64,'
 
 	 tinymce.init({
 		selector:'#comentario_edit',
-		plugins: 'advcode',
+		plugins: 'code',
   		toolbar: 'code'
+		height: 300,
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
 	});
 
 
