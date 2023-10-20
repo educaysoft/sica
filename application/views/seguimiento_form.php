5c4906da0837745feed0605386b0f58bff2c405b
Stalin Francis <educaysoft@gmail.com>
2023-10-20 17:02:23 -0500

    mejoras

--- application/views/seguimiento_form.php
+++ application/views/seguimiento_form.php
@@ -268,14 +268,9 @@ echo '<a class="btn"  onclick="enviar_correo(`<img src=\'data:image/jpg;base64,'
 		}
 
 	 tinymce.init({
-		 selector:'#comentario_edit',
- plugins:
-    "casechange advcode advlist lists spellchecker formatpainter autoresize visualblocks tinydrive image link media imagetools wordcount code",
-  min_height: 380,
-  menubar: false,
-  toolbar: [
-    "bold italic underline | forecolor | formatselect | alignleft aligncenter alignright | bullist numlist",
-    "cut copy paste formatpainter removeformat | casechange visualblocks | spellchecker | link image media | code" ]
+		selector:'#comentario_edit',
+		plugins: 'advcode',
+  		toolbar: 'code'
 	});
 
 
