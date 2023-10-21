bbc424f2be65e65016cf1d0a7b753a17234199cc
Stalin Francis <educaysoft@gmail.com>
2023-10-21 16:54:16 -0500

    mejoras

--- application/controllers/Docenteactividadacademica.php
+++ application/controllers/Docenteactividadacademica.php
@@ -190,6 +190,7 @@ public function elultimo()
   if(!empty($data))
   {
   	$data['distributivodocentes']=$this->distributivodocente_model->lista_distributivodocentesA()->result();
+  	$data['actividadacademicas']= $this->actividadacademica_model->lista_actividadacademicas()->result();
     	$data['title']="Docenteactividadacademica";
     	$this->load->view('template/page_header');		
     	$this->load->view('docenteactividadacademica_record',$data);
