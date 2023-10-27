7de3b039114d1fbe98bd3abec24b5fd55696a89e
Stalin Francis <educaysoft@gmail.com>
2023-10-26 23:14:51 -0500

    mejoras

--- application/views/asignaturadeldocente_form.php
+++ application/views/asignaturadeldocente_form.php
@@ -2,47 +2,76 @@
 <hr/>
 <?php echo form_open("asignaturadeldocente/save") ?>
 <?php echo form_hidden("idasignaturadeldocente")  ?>
-<table>
 
 
 
-<tr>
-<td> Asignatura: </td>
-<td><?php 
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Asignatura:</label>
+	<div class="col-md-10">
+	<?php
 
 $options= array('--Select--');
 foreach ($asignaturas as $row){
 	$options[$row->idasignatura]= $row->nombre;
 }
  echo form_dropdown("idasignatura",$options, set_select('--Select--','default_value'));  ?></td>
-</tr>
+		?>
+	</div> 
+</div> 
 
 
 
-
-<tr>
-<td> Docente: </td>
-<td><?php 
-
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Docente:</label>
+	<div class="col-md-10">
+	<?php
 $options= array('--Select--');
 foreach ($docentes as $row){
 	$options[$row->iddocente]= $row->eldocente;
 }
- echo form_dropdown("iddocente",$options, set_select('--Select--','default_value'));  ?></td>
-</tr>
+ echo form_dropdown("iddocente",$options, set_select('--Select--','default_value'));  
+		?>
+	</div> 
+</div> 
 
 
 
-<tr>
-<td> Documento: </td>
-<td><?php 
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Fecha de desde:</label>
+	<div class="col-md-10">
+	<?php
+
 
 $options= array('--Select--');
 foreach ($documentos as $row){
 	$options[$row->iddocumento]= $row->asunto;
 }
- echo form_dropdown("iddocumento",$options, set_select('--Select--','default_value'));  ?></td>
-</tr>
+ echo form_dropdown("iddocumento",$options, set_select('--Select--','default_value'));  
+		?>
+	</div> 
+</div> 
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Fecha de inicio:</label>
+	<div class="col-md-10">
+	<?php
+ echo form_input(array("name"=>"fechadesde","id"=>"fechadesde","type"=>"date"));  
+		?>
+	</div> 
+</div> 
+
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Fecha hasta:</label>
+	<div class="col-md-10">
+	<?php
+ echo form_input(array("name"=>"fechahasta","id"=>"fechahasta","type"=>"date"));  
+		?>
+	</div> 
+</div> 
 
 
 
@@ -54,8 +83,7 @@ foreach ($documentos as $row){
 
 
 
-
-
+<table>
 <tr>
 <td colspan="2"> <hr><?php echo form_submit("submit", "Guardar"); ?><?php echo anchor("asignaturadeldocente","Atras") ?> </td>
 </tr>
