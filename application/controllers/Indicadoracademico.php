7973bd694388e73412434a9f013204777ec87276
Stalin Francis <educaysoft@gmail.com>
2023-10-28 18:01:07 -0500

    mejoras

--- application/controllers/Indicadoracademico.php
+++ application/controllers/Indicadoracademico.php
@@ -5,6 +5,7 @@ class Indicadoracademico extends CI_Controller{
   public function __construct(){
       parent::__construct();
       $this->load->model('indicadoracademico_model');
+      $this->load->model('distributivo_model');
 }
 
 public function index(){
