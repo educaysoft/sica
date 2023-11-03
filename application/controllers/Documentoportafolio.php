bebac42fb268aa992e47a213a59335bbf5676c06
Stalin Francis <educaysoft@gmail.com>
2023-11-02 22:00:25 -0500

    mejoras

--- application/controllers/Documentoportafolio.php
+++ application/controllers/Documentoportafolio.php
@@ -21,7 +21,8 @@ public function index(){
 	$data['docente']= $this->docente_model->docentespersona($data['documentoportafolio']['idpersona'])->row_array();
   	$data['portafolio']= $this->portafolio_model->lista_portafoliosA($data['documentoportafolio']['idportafolio'])->row_array();
   	$data['portafolios']= $this->portafolio_model->lista_portafoliosA(0)->result();
-
+	echo $data['portafolio']['idperiodoacademico'];
+	echo $data['docente']['iddocente'];
 
 	$data['distributivodocente']=$this->distributivodocente_model->distributivodocente_pado($data['portafolio']['idperiodoacademico'],$data['docente']['iddocente'])->row_array();
 	$data['docenteactividadacademicas']=$this->docenteactividadacademica_model->lista_docenteactividadacademicasA($data['distributivodocente']['iddistributivodocente'])->result();
