bedb5e63ce268b19a2a3e5ff36e22dfe9c64d45e
Stalin Francis <educaysoft@gmail.com>
2023-11-08 22:26:47 -0500

    mejoras

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '108.170.10.210',
+	'hostname' => '38.46.218.114',
 	'username' => 'educayso_facae',
 	'password' => 'kSxMP1!DWkl{',
 	'database' => 'educayso_facae',
