74a4eb3c23d988b5a5c9d34703dd9a7bafaa55a2
Stalin Francis <educaysoft@gmail.com>
2023-11-08 23:43:40 -0500

    mejoreas

--- application/models/sql/periodoacademico1.sql
+++ application/models/sql/periodoacademico1.sql
@@ -1,7 +1,6 @@
 
 use educayso_facae;
-drop view periodoacademico1;
-create view periodoacademico1 as select peac.idperiodoacademico,depa.iddepartamento,  concat(COALESCE(depa.iniciales,'')," - ", COALESCE(peac.nombrecorto,'')) as elperiodoacademico  from periodoacademico peac,departamento depa,departamentoperiodoacademico depe where peac.idperiodoacademico=depe.idperiodoacademico and depe.iddepartamento=depa.iddepartamento
+create view periodoacademico1 as select peac.idperiodoacademico,depa.iddepartamento,  concat(COALESCE('TIC','')," - ", COALESCE(peac.nombrecorto,'')) as elperiodoacademico  from periodoacademico peac,departamento depa,departamentoperiodoacademico depe where peac.idperiodoacademico=depe.idperiodoacademico and depe.iddepartamento=depa.iddepartamento
 UNION
 
-select peac.idperiodoacademico,0 as iddepartamento,  concat("GENERAL"," - ", COALESCE(peac.nombrecorto,'')) as elperiodoacademico  from periodoacademico peac,departamento depa where peac.idperiodoacademico not in (select idperiodoacademico from departamentoperiodoacademico);
+select peac.idperiodoacademico,0 as iddepartamento,  concat(COALESCE("GENERAL",' ')," - ", COALESCE(peac.nombrecorto,'')) as elperiodoacademico   from periodoacademico peac where peac.idperiodoacademico not in (select idperiodoacademico from departamentoperiodoacademico);
