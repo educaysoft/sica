a17b3ebbae15f21ca8d9ec2bdcb3c4c7b86aa6f9
Stalin Francis <educaysoft@gmail.com>
2023-11-22 08:17:51 -0500

    mejoras

--- application/views/departamentodestino_record.php
+++ application/views/departamentodestino_record.php
@@ -47,29 +47,11 @@ if(isset($departamentodestino))
 </div> 
 
 
-<div class="form-group row">
-    <label class="col-md-2 col-form-label"> Id estudio:</label>
-	<div class="col-md-10">
-	<?php
-     	 echo form_input('idestudio',$departamentodestino['idestudio'],array("disabled"=>"disabled",'placeholder'=>'Iddepartamentodestinoes','style'=>'width:600px;'));
-	?>
-	</div> 
-</div> 
+ 
 
 
 
-<div class="form-group row">
-    <label class="col-md-2 col-form-label"> Estudio:</label>
-	<div class="col-md-10">
-	<?php
-$options= array("NADA");
-foreach ($estudios as $row){
-	$options[$row->idestudio]= $row->lapersona." -  ".$row->titulo;
-}
-echo form_input('idestudio',$options[$departamentodestino['idestudio']],array("disabled"=>"disabled",'style'=>'width:600px;'));
-	?>
-	</div> 
-</div> 
+ 
 
 
 
