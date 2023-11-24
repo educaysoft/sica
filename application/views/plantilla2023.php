00e0a0763bd090d1fdc9c05e24507e592ecf9aa4
Stalin Francis <educaysoft@gmail.com>
2023-11-24 05:35:00 -0500

    mejoras

--- application/views/plantilla2023.php
+++ application/views/plantilla2023.php
@@ -38,6 +38,28 @@ class PDF extends PDF_Sector
 //		$this->Cell(40,5,utf8_decode('B'),0,1,'L');
 //		$this->Cell(40,5,utf8_decode('DOCENTE:'),0,0,'L');
 //		$this->Cell(40,5,utf8_decode('Ing. Stalin Francis Q. M.Sc.'),0,1,'L');
+
+  $this->SetFont('Arial','',10);
+        $this->SetTextColor(0, 0,0);
+        $this->Cell(30,5,"Asignatura: ",1,0,'L',0);
+        $this->Cell(120,5,utf8_decode($this->laasignatura),1,1,'L',0);
+    
+        $this->Cell(30,5,"Docente: ",1,0,'L',0);
+        $this->Cell(120,5,utf8_decode($this->eldocente),1,1,'L',0);
+    $this->Ln();
+
+        $this->Cell(30,5,"Instrucciones : ",1,0,'L',0);   $this->WriteHTML($this->detalle);
+   //   $this->Cell(30,5,"Instrucciones : ",1,0,'L',0);   $this->MultiCell(100,5,utf8_decode($this->detalle),0);
+        $this->SetFillColor(232,232,232);
+        $this->SetFont('Arial','B',8);
+            
+    $this->Ln();
+
+
+
+
+
+
 	}
 	function Footer()
 	{
@@ -181,6 +203,142 @@ class PDF extends PDF_Sector
 			$this->wLegend=max($this->GetStringWidth($legend),$this->wLegend);
 		}
 	}
+
+
+
+
+function WriteHTML($html)
+{
+    //HTML parser
+    //$html=strip_tags($html,"<b><u><i><a><img><p><br><strong><em><font><tr><blockquote>"); //supprime tous les tags sauf ceux reconnus
+    $html=strip_tags($html); //supprime tous les tags sauf ceux reconnus
+    $html=str_replace("\n",' ',$html); //remplace retour à la ligne par un espace
+    $a=array(2=>$html); // preg_split('/<(.*)>/U',$html,-1,PREG_SPLIT_DELIM_CAPTURE); //éclate la chaîne avec les balises
+    foreach($a as $i=>$e)
+    {
+        if($i%2==0)
+        {
+            //Text
+            if($this->HREF)
+                $this->PutLink($this->HREF,$e);
+            else
+                $this->MultiCell(130,5,utf8_decode(txtentities($e)),1);
+               // $this->Write(5,txtentities($e));
+        }
+        else
+        {
+            //Tag
+            if($e[0]=='/')
+                $this->CloseTag(strtoupper(substr($e,1)));
+            else
+            {
+                //Extract attributes
+                $a2=explode(' ',$e);
+                $tag=strtoupper(array_shift($a2));
+                $attr=array();
+                foreach($a2 as $v)
+                {
+                    if(preg_match('/([^=]*)=["\']?([^"\']*)/',$v,$a3))
+                        $attr[strtoupper($a3[1])]=$a3[2];
+                }
+                $this->OpenTag($tag,$attr);
+            }
+        }
+    }
+}
+function OpenTag($tag, $attr)
+{
+    //Opening tag
+    switch($tag){
+        case 'STRONG':
+            $this->SetStyle('B',true);
+            break;
+        case 'EM':
+            $this->SetStyle('I',true);
+            break;
+        case 'B':
+        case 'I':
+        case 'U':
+            $this->SetStyle($tag,true);
+            break;
+        case 'A':
+            $this->HREF=$attr['HREF'];
+            break;
+        case 'IMG':
+            if(isset($attr['SRC']) && (isset($attr['WIDTH']) || isset($attr['HEIGHT']))) {
+                if(!isset($attr['WIDTH']))
+                    $attr['WIDTH'] = 0;
+                if(!isset($attr['HEIGHT']))
+                    $attr['HEIGHT'] = 0;
+                $this->Image($attr['SRC'], $this->GetX(), $this->GetY(), px2mm($attr['WIDTH']), px2mm($attr['HEIGHT']));
+            }
+            break;
+        case 'TR':
+        case 'BLOCKQUOTE':
+        case 'BR':
+            $this->Ln(5);
+            break;
+        case 'P':
+            $this->Ln(10);
+            break;
+        case 'FONT':
+            if (isset($attr['COLOR']) && $attr['COLOR']!='') {
+                $coul=hex2dec($attr['COLOR']);
+                $this->SetTextColor($coul['R'],$coul['V'],$coul['B']);
+                $this->issetcolor=true;
+            }
+            if (isset($attr['FACE']) && in_array(strtolower($attr['FACE']), $this->fontlist)) {
+                $this->SetFont(strtolower($attr['FACE']));
+                $this->issetfont=true;
+            }
+            break;
+    }
+}
+
+unction CloseTag($tag)
+{
+    //Closing tag
+    if($tag=='STRONG')
+        $tag='B';
+    if($tag=='EM')
+        $tag='I';
+    if($tag=='B' || $tag=='I' || $tag=='U')
+        $this->SetStyle($tag,false);
+    if($tag=='A')
+        $this->HREF='';
+    if($tag=='FONT'){
+        if ($this->issetcolor==true) {
+            $this->SetTextColor(0);
+        }
+        if ($this->issetfont) {
+            $this->SetFont('arial');
+            $this->issetfont=false;
+        }
+    }
+}
+
+function SetStyle($tag, $enable)
+{
+    //Modify style and select corresponding font
+    $this->$tag+=($enable ? 1 : -1);
+    $style='';
+    foreach(array('B','I','U') as $s)
+    {
+        if($this->$s>0)
+            $style.=$s;
+    }
+    $this->SetFont('',$style);
+}
+
+function PutLink($URL, $txt)
+{
+    //Put a hyperlink
+    $this->SetTextColor(0,0,255);
+    $this->SetStyle('U',true);
+    $this->Write(5,$txt,$URL);
+    $this->SetStyle('U',false);
+    $this->SetTextColor(0);
+}
 
 
 
