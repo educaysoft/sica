5be507209aec781102972485fb2d9a44b859fcfe
Stalin Francis <educaysoft@gmail.com>
2023-11-24 05:42:14 -0500

    mejoras

--- application/views/plantilla2023.php
+++ application/views/plantilla2023.php
@@ -4,6 +4,39 @@ require 'fpdf/fpdf.php';
 //require_once($v);
 require_once('sector.php');
 
+//function hex2dec
+//returns an associative array (keys: R,G,B) from
+//a hex html code (e.g. #3FE5AA)
+function hex2dec($couleur = "#000000"){
+    $R = substr($couleur, 1, 2);
+    $rouge = hexdec($R);
+    $V = substr($couleur, 3, 2);
+    $vert = hexdec($V);
+    $B = substr($couleur, 5, 2);
+    $bleu = hexdec($B);
+    $tbl_couleur = array();
+    $tbl_couleur['R']=$rouge;
+    $tbl_couleur['V']=$vert;
+    $tbl_couleur['B']=$bleu;
+    return $tbl_couleur;
+}
+
+//conversion pixel -> millimeter at 72 dpi
+function px2mm($px){
+    return $px*25.4/72;
+}
+
+function txtentities($html){
+    $trans = get_html_translation_table(HTML_ENTITIES);
+    $trans = array_flip($trans);
+    return strtr($html, $trans);
+}
+////////////////////////////////////
+
+
+
+
+
 class PDF extends PDF_Sector
 {
 	public $institucion = "";
