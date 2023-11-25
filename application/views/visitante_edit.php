2295b412400093ccf9e75e200c1498d76002fa63
Stalin Francis <educaysoft@gmail.com>
2023-11-25 06:45:20 -0500

    mejoras

--- application/views/visitante_edit.php
+++ application/views/visitante_edit.php
@@ -152,7 +152,7 @@ $textarea_options = array('class' => 'form-control','rows' => '4','id'=>'motivo'
    fecha =document.getElementById('fecha');   
 
 //window.location.href="https://repositorioutlvte.org/firmadigital.php?idvisitante="+idvisitante+"&motivo='"+motivo+"'";
-	window.location.href="https://repositorioutlvte.org/firmadigital.php?idvisitante="+idvisitante+"&motivo='"+motivo+"&fecha='"+fecha+"&idpersona='"+idpersona+"'";
+	window.location.href="https://repositorioutlvte.org/firmadigital.php?idvisitante="+idvisitante+"&motivo='"+motivo+"'&fecha='"+fecha+"'&idpersona="+idpersona;
 
  }
 
