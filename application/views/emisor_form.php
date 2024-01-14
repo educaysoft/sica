b4238f2858b21c9536a053c1e991f2c765df5268
Stalin Francis <educaysoft@gmail.com>
2024-01-14 10:19:49 -0500

    mejoras

--- application/views/emisor_form.php
+++ application/views/emisor_form.php
@@ -13,7 +13,7 @@
 
 $options= array('--Select--');
 foreach ($documentos as $row){
-	$options[$row->iddocumento]= $row->asunto;
+	$options[$row->iddocumento]= $row->iddocumento." - ".$row->asunto;
 }
 
  echo form_dropdown("iddocumento",$options, set_select('--Select--','default_value'));  ?></td>
