b3c55eea59bc011a775180b063cb2bac2629dfd8
Stalin Francis <educaysoft@gmail.com>
2024-01-29 18:33:49 -0500

    mejoras

--- application/views/asignatura_record.php
+++ application/views/asignatura_record.php
@@ -16,7 +16,9 @@ if(isset($asignatura))
 	<li> <?php echo anchor('asignatura/reportepdf/'.$asignatura['idmalla'],'reportepdf'); ?></li>
 <?php 
 }else{
-   <li> <?php echo anchor('asignatura/add', 'Nuevo'); ?></li>
+?>
+
+        <li> <?php echo anchor('asignatura/add', 'Nuevo'); ?></li>
 <?php
 }
 ?>
