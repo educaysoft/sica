ea954f934b9c6c9d61405d1d1ba5457699c4fd31
Stalin Francis <educaysoft@gmail.com>
2024-02-28 11:28:06 -0500

    mejoras

--- application/views/distributivo_list_pdf2.php
+++ application/views/distributivo_list_pdf2.php
@@ -43,7 +43,7 @@
 	$pdf->Cell(10,5,'Ses',1,0,'C',1);
 	$pdf->Cell(35,5,'Silabopdf',1,0,'C',1);
 	$pdf->Cell(35,5,'Planpdf',1,0,'C',1);
-	$pdf->Cell(35,5,'Calificacionpdf',1,1,'C',1);
+	$pdf->Cell(35,5,'Distib. Indiv',1,1,'C',1);
  
 	 
 
@@ -94,7 +94,7 @@
 		    $pdf->Cell(10,5,utf8_decode($row->cantidadtemas),1,0,'L',0);
 		    $pdf->Cell(35,5,utf8_decode($row->archivopdf),1,0,'L',0);
 		    $pdf->Cell(35,5,utf8_decode($row->planpdf),1,0,'L',0);
-		    $pdf->Cell(35,5,utf8_decode($row->calificacionpdf),1,1,'L',0);
+		    $pdf->Cell(35,5,utf8_decode($row->distributivoindividualpdf),1,1,'L',0);
     }
 
 
