e91bd22e75b931c5bb0c4b6e8a9e7e5fd72a245c
Stalin Francis <educaysoft@gmail.com>
2024-03-01 20:39:14 -0500

    mejoras

---
+++ application/views/genero_list.php
@@ -0,0 +1,50 @@
+<html>
+
+
+<body>
+
+<h2> <?php echo $title;  ?></h2>
+<hr/>
+<?php echo anchor('genero/add', 'NUEVO ORDENADOR'); ?>
+<br>
+<br>
+
+<table border="1">
+<tr>
+<th> ID ORDENADOR</th>
+<th> DESCRIPCION</th>
+<th> cantidad</th>
+</tr>
+<tbody>
+<?php  foreach($genero_list as $list) { ?>
+<tr>
+
+<td> <?php echo $list->idgenero ?></td>
+<td> <?php echo $list->nombre?></td>
+<td> <?php echo anchor('documento/listarxgenero/'.$list->idgenero,'ver') ?> || <?php echo anchor('documento/add/'.$list->idgenero,'Nuevo') ?></td>
+ </tr>
+
+
+<?php } ?>
+
+</tbody>
+</table>
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+</html>
