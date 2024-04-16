89eb3c956eca75c1d9b5ca5232e82acb8b680e41
Stalin Francis <educaysoft@gmail.com>
2024-04-15 19:51:34 -0500

    mejoras

--- application/models/Documentoportafolio_model.php
+++ application/models/Documentoportafolio_model.php
@@ -35,7 +35,7 @@ class Documentoportafolio_model extends CI_model {
 
 
 
- 	function documentoportafolioestudiante( $idestudiante,$idperiodoacademico){
+ 	function documentoportafolioalumno( $idestudiante,$idperiodoacademico){
 
  		$documentoportafolio = $this->db->query('select * from documentoportafolio3 where idestudiante="'. $idestudiante.'" and idperiodoacademico="'. $idperiodoacademico.' "  order by idestudiante  ');
  		return $documentoportafolio;
