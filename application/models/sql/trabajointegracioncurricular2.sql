f346312a5e742448943b0266fe230890a5036a2f
Stalin francis <educaysoft@gmail.com>
2024-05-01 01:51:30 -0500

    meoras

--- application/models/sql/trabajointegracioncurricular2.sql
+++ application/models/sql/trabajointegracioncurricular2.sql
@@ -1,4 +1,5 @@
 
 use educayso_facae;
-create view trabajointegracioncurricular2 as select ticu.idtrabajointegracioncurricular, (select elegresado from egresado1 egre where egre.idtrabajointegracioncurricular= ticu.idtrabajointegracioncurricular) as elegresado, ticu.nombre, ticu.resumen, (select iddocente from lector lect where lect.idtrabajointegracioncurricular= ticu.idtrabajointegracioncurricular and lect.idtipolector=1) as iddocente,(select ellector from lector1 lect where lect.idtrabajointegracioncurricular= ticu.idtrabajointegracioncurricular and lect.idtipolector=1) as ellector ,ticu.fechacreacion, ticu.horacreacion from trabajointegracioncurricular ticu     
+drop view trabajointegracioncurricular2;
+create view trabajointegracioncurricular2 as select ticu.idtrabajointegracioncurricular,ticu.idestadotrabajointegracioncurricular, (select elegresado from egresado1 egre where egre.idtrabajointegracioncurricular= ticu.idtrabajointegracioncurricular) as elegresado, ticu.nombre, ticu.resumen, (select iddocente from lector lect where lect.idtrabajointegracioncurricular= ticu.idtrabajointegracioncurricular and lect.idtipolector=1) as iddocente,(select ellector from lector1 lect where lect.idtrabajointegracioncurricular= ticu.idtrabajointegracioncurricular and lect.idtipolector=1) as ellector ,ticu.fechacreacion, ticu.horacreacion from trabajointegracioncurricular ticu     
 
