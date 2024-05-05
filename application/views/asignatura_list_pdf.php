5a6715e15a122e5a5842f978dd8ac8cf909e39ea
Stalin francis <educaysoft@gmail.com>
2024-05-05 16:52:52 -0500

    mejoas

--- application/views/asignatura_list_pdf.php
+++ application/views/asignatura_list_pdf.php
@@ -1,6 +1,6 @@
 <?php
 
-	include 'plantilla.php';
+	include 'plantilla2.php';
 //	require 'conexion.php';
 	
 //	$query = "SELECT e.estado, m.id_municipio, m.municipio FROM t_municipio AS m INNER JOIN t_estado AS e ON m.id_estado=e.id_estado";
