d7e5dd8430b351e5967e465829084878f834c963
Stalin francis <educaysoft@gmail.com>
2024-05-06 05:36:56 -0500

    mejora

--- application/views/portafolio_form.php
+++ application/views/portafolio_form.php
@@ -2,34 +2,35 @@
 <hr/>
 <?php echo form_open("portafolio/save") ?>
 <?php echo form_hidden("idportafolio")  ?>
-<table>
 
 
-<tr>
-<td> Persona: </td>
-<td><?php 
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Persona:</label>
+	<div class="col-md-10">
+    <?php 
+    $options= array('--Select--');
+    foreach ($personas as $row){
+	    $options[$row->idpersona]= $row->lapersona;
+    }
+     echo form_dropdown("idpersona",$options, set_select('--Select--','default_value'));  ?>
+	</div> 
+</div>
 
-$options= array('--Select--');
-foreach ($personas as $row){
-	$options[$row->idpersona]= $row->lapersona;
-}
 
- echo form_dropdown("idpersona",$options, set_select('--Select--','default_value'));  ?></td>
-</tr>
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Periodo académico:</label>
+	<div class="col-md-10">
+    <?php 
+
+    $options= array('--Select--');
+    foreach ($periodoacademicos as $row){
+	    $options[$row->idperiodoacademico]= $row->nombrecorto;
+    }
+    echo form_dropdown("idperiodoacademico",$options, set_select('--Select--','default_value'));  ?></td>
+	</div> 
+</div>
 
 
-
-<tr>
-<td> Periodo académico: </td>
-<td><?php 
-
-$options= array('--Select--');
-foreach ($periodoacademicos as $row){
-	$options[$row->idperiodoacademico]= $row->nombrecorto;
-}
-
- echo form_dropdown("idperiodoacademico",$options, set_select('--Select--','default_value'));  ?></td>
-</tr>
 
 <div class="form-group row">
 <label class="col-md-2 col-form-label">Ordenador destino:</label>
@@ -59,15 +60,14 @@ foreach ($periodoacademicos as $row){
 
 
 
+<div id="eys-nav-i">
+	<ul>
+   	 	<li> <a href="javascript:{}" onclick="document.getElementById('eys-form').submit(); return false;">Guardar</a></li>
+    		<li> <?php echo anchor('portafolio', 'Cancelar'); ?></li>
+	</ul>
+</div>
 
 
 
-
-
-<tr>
-<td colspan="2"> <hr><?php echo form_submit("submit", "Guardar"); ?><?php echo anchor("portafolio","Atras") ?> </td>
-</tr>
-
-</table>
 <?php echo form_close();?>
 
