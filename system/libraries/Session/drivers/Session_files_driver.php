1b62a9aff2fc3a629e9c9f88b25c25d63d241a87
Stalin francis <educaysoft@gmail.com>
2024-05-13 17:17:49 -0500

    mejoras

--- system/libraries/Session/drivers/Session_files_driver.php
+++ system/libraries/Session/drivers/Session_files_driver.php
@@ -356,7 +356,7 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 	 * @param	int 	$maxlifetime	Maximum lifetime of sessions
 	 * @return	bool
 	 */
-	 function gc($maxlifetime): int:false
+	 function gc($maxlifetime): int|false
 	{
 		if ( ! is_dir($this->_config['save_path']) OR ($directory = opendir($this->_config['save_path'])) === FALSE)
 		{
