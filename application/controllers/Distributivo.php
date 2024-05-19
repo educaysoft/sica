3a22e07b46bc297c62dc689312ef8aeb2f30e7b7
Stalin francis <educaysoft@gmail.com>
2024-05-19 12:39:30 -0500

    mejoras

--- application/controllers/Distributivo.php
+++ application/controllers/Distributivo.php
@@ -478,7 +478,7 @@ public function generahorario()
     $horainiciovespertino = "13:00:00";
     $horafinalmatutino = "13:00:00";
     $horafinalvespertino = "16:00:00";
-    print_r($data0);
+    print_r($data0->result());
     die();
     foreach ($data0->result() as $r) {
         if ($inicio == 0) {
