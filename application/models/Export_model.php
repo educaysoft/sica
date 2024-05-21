7cf96c09f329db004a6c4805102c636f982d6caf
Stalin francis <educaysoft@gmail.com>
2024-05-21 06:54:17 -0500

    mejoras

--- application/models/Export_model.php
+++ application/models/Export_model.php
@@ -16,6 +16,28 @@ class Export_model extends CI_Model {
         // Escribir los datos en la hoja de cálculo
         $sheet->fromArray($data, null, 'A1');
 
+
+
+    // Enviar el archivo al navegador para descarga
+    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    header('Content-Disposition: attachment;filename="' . $filename . '"');
+    header('Cache-Control: max-age=0');
+    header('Cache-Control: max-age=1');
+    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Fecha en el pasado
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // Siempre modificado
+    header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+    header('Pragma: public'); // HTTP/1.0
+
+
+
+
+
+
+
+
+
+
+
         // Crear un objeto Writer para guardar la hoja de cálculo
         $writer = new Xlsx($spreadsheet);
 
