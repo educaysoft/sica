f5904efeb8514eef7a3d1910d164c3460cfc84f0
Stalin Francis <educaysoft@gmail.com>
2024-06-06 22:40:10 -0500

    mejoras

--- application/views/referenciasasignatura_pdf.php
+++ application/views/referenciasasignatura_pdf.php
@@ -26,7 +26,7 @@
 	$pdf->SetTextColor(0, 0,0);
 //    	$pdf->Text(20,40,"Docente:  ".$estudios[0]->cedula." - ". $estudios[0]->lapersona); 
 
-    	$pdf->Text(20,40,"Distributivo : ". $distributivo[0]->eldistributivo); 
+//    	$pdf->Text(20,40,"Distributivo : ". $distributivo[0]->eldistributivo); 
 	$pdf->SetFillColor(232,232,232);
 	$pdf->SetFont('Arial','B',8);
 
