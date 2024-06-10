082ede744dbd242e029b6368d142c22552f40af0
Stalin francis <educaysoft@gmail.com>
2024-06-09 22:51:58 -0500

    mejoras

--- application/views/validarcorreo_form.php
+++ application/views/validarcorreo_form.php
@@ -205,13 +205,21 @@ function enviar_correo() {
     var correode = "educaysoft@hotmail.com";
     var nome = 'Stalin Francis Q.';
     var msg = "Ingresa al siguiente link para terminar tu registro y poder recibir tu certificado al culminar el evento: <b>";
+     var msg="Ingresa al siguiente link para terminar tu registro y poder recibir tu certificado al culminar el evento <br><br><a href='https://educaysoft.org/sica/index.php/login/registro?idevento="+idevento+"' style='text-align:center;'><b>Evento :</b>"+titulo+"</a><br><br>";  //tinyMCE.activeEditor.getContent({format:'text'});
+		 
+ 
+ 
     var mailto = document.getElementById("email").value;
     var correopara = mailto;
     var secure = "siteform";
     var head = "";
     var asunto = "Completar registro a SIGECA";
     var foot0 = "";
-    var foot = "<div style='text-align:center; background-color:lightgrey; font-size:12px;'> Este correo ha sido enviado a " + mailto + ".</div>";
+   // var foot = "<div style='text-align:center; background-color:lightgrey; font-size:12px;'> Este correo ha sido enviado a " + mailto + ".</div>";
+
+	var foot=" <div style='text-align:center; background-color:lightgrey; font-size:12px;'> Este correo ha sido enviado a "+mailto+ ", de acuerdo a la Ley Orgánica de Protección de datos, usted tiene el derecho a solicitar a la Universidad Técnica Luis Vargas Torres, la actualización, inclusión, supresión y/o tratamiento de los datos personales incluidos en sus bases de datos, con este correo electrónico usted acepta recibir información de las actividades académicas que realiza el Alma Mater así como nuestra propuestas académicas <br><br> Este correo fue generado y enviado automáticamente desde el sistema cloud elaborado de la Maestría en Tecnología de la Información</div> ";
+
+	
 
     msg = head + msg + foot0 + foot;
 
