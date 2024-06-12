bda2aa3f7fde98baa56397cee9b2e8d984caa950
Stalin francis <educaysoft@gmail.com>
2024-06-11 22:17:36 -0500

    mejoras

--- application/views/evento_xpersona.php
+++ application/views/evento_xpersona.php
@@ -25,7 +25,7 @@ body {font-family: Arial, Helvetica, sans-serif;}
 }
 
 </style>
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
 
