49c9a6a4dcee024dc34e01b67c5f3a61553e8502
Stalin francis <educaysoft@gmail.com>
2024-07-10 23:40:49 -0500

    mejoras

--- application/models/sql/formatoinstitucional1.sql
+++ application/models/sql/formatoinstitucional1.sql
@@ -1,4 +1,4 @@
 
 use educayso_facae;
-
-create view formatoinstitucional1 as select arti.idformatoinstitucional, arti.nombre as elformatoinstitucional, arti.detalle, arti.idinstitucion,inst.nombre as lainstitucion  from formatoinstitucional arti,institucion inst where arti.idinstitucion=inst.idinstitucion; 
+drop view formatoinstitucional1;
+create view formatoinstitucional1 as select arti.idformatoinstitucional, arti.nombre as elformatoinstitucional, arti.detalle, arti.idinstitucion,inst.nombre as lainstitucion,arti.archivo  from formatoinstitucional arti,institucion inst where arti.idinstitucion=inst.idinstitucion; 
