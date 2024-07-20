92116363086d777dd605934fe9e206c59dedece9
Stalin francis <educaysoft@gmail.com>
2024-07-20 16:10:34 -0500

    majoras

--- application/views/publicacion_genpagina.php
+++ application/views/publicacion_genpagina.php
@@ -271,7 +271,7 @@ foreach($publicaciondocentes as $row){
 
 $data=$data.'<div class="col">
           <div class="card shadow-sm">
-		  <a  href="https://educaysoft.org/sica/evento/detalle/'.$inicio.'"><svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/>';
+		  <a  href="#"><svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/>';
 
 // Remote file url
 $remoteFile = "https://repositorioutlvte.org/Repositorio/eventos/AreaConocimiento/".trim($inicio).".jpg";
