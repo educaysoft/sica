6476bfa3f22f0bf3b18f95c0cfc8bd5d3a94f4c7
Stalin francis <educaysoft@gmail.com>
2024-07-21 09:31:32 -0500

    mejoras

--- application/views/publicacion_genpagina.php
+++ application/views/publicacion_genpagina.php
@@ -41,13 +41,6 @@ overflow:hidden;
         }
       }
 
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
-
       .b-example-vr {
         flex-shrink: 0;
         width: 1.5rem;
