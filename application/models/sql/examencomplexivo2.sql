e3273358fc3a07ab47d3176b811216899ecde8b3
Stalin francis <educaysoft@gmail.com>
2024-07-29 00:57:22 -0500

    mejoras

--- application/models/sql/examencomplexivo2.sql
+++ application/models/sql/examencomplexivo2.sql
@@ -1,4 +1,5 @@
 
 use educayso_facae;
-create view examencomplexivo2 as select ticu.idexamencomplexivo, (select elegresado from egresado1 egre where egre.idexamencomplexivo= ticu.idexamencomplexivo) as elegresado,(select idegresado from egresado1 egre where egre.idexamencomplexivo= ticu.idexamencomplexivo) as idegresado,  ticu.nombre, ticu.resumen, lect.iddocente,  lect.eltutorexamencomplexivo,  ticu.fechacreacion, ticu.horacreacion from examencomplexivo ticu, tutorexamencomplexivo1 lect   where ticu.idexamencomplexivo=lect.idexamencomplexivo;    
+drop view examencomplexivo2;
+create view examencomplexivo2 as select ticu.idexamencomplexivo, egre.elegresado,egre.idegresado,  ticu.nombre, ticu.resumen,     ticu.fechacreacion, ticu.horacreacion from examencomplexivo ticu, egresado1 egre   where ticu.idexamencomplexivo=egre.idexamencomplexivo;    
 
