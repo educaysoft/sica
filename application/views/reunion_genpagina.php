1a63e7ceece91efb71138ece9930fbada82ffa4d
Stalin francis <educaysoft@gmail.com>
2024-08-12 08:19:48 -0500

    mejoras

--- application/views/reunion_genpagina.php
+++ application/views/reunion_genpagina.php
@@ -453,7 +453,7 @@ $data=$data.'
 
 	 	$data=$data.$data1;
 
-			$file='application/views/reunions/reunion-'.$row->idinstitucion.'.php';
+			$file='application/views/reuniones/reunion-'.$row->idinstitucion.'.php';
 
 
 	if ( !write_file($file, $data)){
