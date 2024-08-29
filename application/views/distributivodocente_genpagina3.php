40198ee801a4988ec299bec7e1c01291876d2d9a
Stalin francis <educaysoft@gmail.com>
2024-08-28 19:55:05 -0500

    mejoras

--- application/views/distributivodocente_genpagina3.php
+++ application/views/distributivodocente_genpagina3.php
@@ -594,7 +594,6 @@ $data=$data.'</div>
 			}
 			$data=$data.'<br><br>';
 
-			$data=$data.'[<a href="'.$row->portafoliodrive.'"  '.$disable1.'><i class="fas fa-file-pdf" style="font-size:24px" ></i> <span style="color:'.$color1.'" >Portafolio Drive</span></a>] - ';
 
 
 $data=$data.'<div class="tabla-container">
