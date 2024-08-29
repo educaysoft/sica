98e61256a8d1c6307ba25b3175bd953bfae6dd1b
Stalin francis <educaysoft@gmail.com>
2024-08-28 20:16:36 -0500

    mejoras

--- application/views/distributivodocente_edit.php
+++ application/views/distributivodocente_edit.php
@@ -80,7 +80,7 @@ foreach ($relaciondependencias as $row){
      <td><?php 
 
 
-$eys_arrinput=array('name'=>'portafoliodrive','value'=>$distributivodocente['portafoliodrive'],'readonly'=>'true', "style"=>"width:500px");
+$eys_arrinput=array('name'=>'portafoliodrive','value'=>$distributivodocente['portafoliodrive'], "style"=>"width:500px");
 echo form_input($eys_arrinput); ?></td>
   </tr>
 
