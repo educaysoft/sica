8812c19b8a78ca3702ea1676ae210694f8f8da31
Stalin francis <educaysoft@gmail.com>
2024-09-01 00:21:19 -0500

    merjoas

--- application/views/calidadcarrera_list_pdf.php
+++ application/views/calidadcarrera_list_pdf.php
@@ -42,9 +42,9 @@
 
 
 	$pdf->Cell(5,5,'#',1,0,'C',1);
-	$pdf->Cell(20,5,'Codigo',1,0,'C',1);
+	$pdf->Cell(15,5,'Codigo',1,0,'C',1);
 	$pdf->Cell(120,5,utf8_decode('Fuente de Información'),1,0,'C',1);
-	$pdf->Cell(10,5,'evidencia',1,1,'C',1);
+	$pdf->Cell(15,5,'evidencia',1,1,'C',1);
  
 	 
 
@@ -67,7 +67,7 @@
 
 		    $i=$i+1;
 		    $pdf->Cell(5,$h,$i,1,0,'R',0); 
-		    $pdf->Cell(20,$h,utf8_decode($row->codigo),1,0,'L',0);
+		    $pdf->Cell(15,$h,utf8_decode($row->codigo),1,0,'L',0);
 		 $current_x = $pdf->GetX();
 		 $current_y = $pdf->GetY();
 
@@ -81,13 +81,13 @@
             $y = $pdf->GetY(); // Obtén la posición Y actual
 
             $pdf->Image($url_base.'sindocumento.png', $x+2, $y+2,8,8); // Coloca la imagen dentro de la celda
-		    $pdf->Cell(10,$h,"        ",1,0,'L');
+		    $pdf->Cell(15,$h,"        ",1,0,'L');
         }else{
             $x = $pdf->GetX(); // Obtén la posición X actual
             $y = $pdf->GetY(); // Obtén la posición Y actual
 
             $pdf->Image($url_base.'documento.png', $x+2, $y+2, 8,8); // Coloca la imagen dentro de la celda
-		    $pdf->Cell(10,$h,"[           ]",1,0,'C',0,$row->archivo);
+		    $pdf->Cell(15,$h,"[           ]",1,0,'C',0,$row->archivo);
         }
             $pdf->Ln();
 
