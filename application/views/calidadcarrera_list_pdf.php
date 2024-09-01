17f29bc77a15582c0d78afc93a468adc720e20a5
Stalin francis <educaysoft@gmail.com>
2024-09-01 00:15:56 -0500

    merjoas

--- application/views/calidadcarrera_list_pdf.php
+++ application/views/calidadcarrera_list_pdf.php
@@ -1,7 +1,7 @@
 <?php
 
 //	include 'plantilla.php';
-	include 'plantilla2023-1.php';
+	include 'plantilla2024-1.php';
 //	require 'conexion.php';
 	
 //	$query = "SELECT e.estado, m.id_municipio, m.municipio FROM t_municipio AS m INNER JOIN t_estado AS e ON m.id_estado=e.id_estado";
@@ -43,8 +43,8 @@
 
 	$pdf->Cell(5,5,'#',1,0,'C',1);
 	$pdf->Cell(20,5,'Codigo',1,0,'C',1);
-	$pdf->Cell(100,5,utf8_decode('Fuente de Información'),1,0,'C',1);
-	$pdf->Cell(20,5,'evidencia',1,1,'C',1);
+	$pdf->Cell(120,5,utf8_decode('Fuente de Información'),1,0,'C',1);
+	$pdf->Cell(10,5,'evidencia',1,1,'C',1);
  
 	 
 
@@ -72,8 +72,8 @@
 		 $current_y = $pdf->GetY();
 
 		 //$pdf->Cell(80,5,utf8_decode($row->asunto),1,0,'L',0);
-		 $pdf->MultiCell(100,5,utf8_decode($row->nombre),1,'L',1);
-		 $pdf->SetXY($current_x+100, $current_y);
+		 $pdf->MultiCell(120,5,utf8_decode($row->nombre),1,'L',1);
+		 $pdf->SetXY($current_x+120, $current_y);
          $url_base = "https://repositorioutlvte.org/Repositorio/iconos/";
          if(empty($row->archivo))
          {
@@ -81,13 +81,13 @@
             $y = $pdf->GetY(); // Obtén la posición Y actual
 
             $pdf->Image($url_base.'sindocumento.png', $x+2, $y+2,8,8); // Coloca la imagen dentro de la celda
-		    $pdf->Cell(20,$h,"        ",1,0,'L');
+		    $pdf->Cell(10,$h,"        ",1,0,'L');
         }else{
             $x = $pdf->GetX(); // Obtén la posición X actual
             $y = $pdf->GetY(); // Obtén la posición Y actual
 
             $pdf->Image($url_base.'documento.png', $x+2, $y+2, 8,8); // Coloca la imagen dentro de la celda
-		    $pdf->Cell(20,$h,"[        ]",1,0,'L',0,$row->archivo);
+		    $pdf->Cell(10,$h,"[           ]",1,0,'C',0,$row->archivo);
         }
             $pdf->Ln();
 
