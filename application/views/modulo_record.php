6b58bca8d3dd2f93284bc25c416597a234d98ef3
Stalin francis <educaysoft@gmail.com>
2024-09-01 06:22:39 -0500

    mejoras

--- application/views/modulo_record.php
+++ application/views/modulo_record.php
@@ -13,7 +13,7 @@ if(isset($modulo))
         <li> <?php echo anchor('modulo/edit/'.$modulo['idmodulo'],'Edit'); ?></li>
         <li style="border-right:1px solid green"> <?php echo anchor('modulo/delete/'.$modulo['idmodulo'],'Delete'); ?></li>
         <li> <?php echo anchor('modulo/listar/','Listar'); ?></li>
-		<li> <?php echo anchor('modulo/generaweb/'.$modulo['idmodulo'],'generaweb'); ?></li>
+		<li> <?php echo anchor('modulo/generapaginaweb/'.$modulo['idmodulo'],'generaweb'); ?></li>
 		<li> <?php echo anchor('modulo/paginaweb/'.$modulo['idmodulo'],' web'); ?></li>
 <?php 
 }else{
