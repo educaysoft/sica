bc81d08f9f4bcb2f46508e1bb1fb26efd27c8c5e
Stalin francis <educaysoft@gmail.com>
2024-09-04 21:53:19 -0500

    mejoras

--- application/views/calidadcarrera_list_pdf.php
+++ application/views/calidadcarrera_list_pdf.php
@@ -42,10 +42,11 @@
 
 
 	$pdf->Cell(5,5,'#',1,0,'C',1);
-	$pdf->Cell(12,5,'Codigo',1,0,'C',1);
-	$pdf->Cell(12,5,'CRIT',1,0,'C',1);
-	$pdf->Cell(12,5,'SUCR',1,0,'C',1);
-	$pdf->Cell(12,5,'INDI',1,0,'C',1);
+	$pdf->Cell(10,5,'id',1,0,'C',1);
+	$pdf->Cell(10,5,'Codigo',1,0,'C',1);
+	$pdf->Cell(10,5,'CRIT',1,0,'C',1);
+	$pdf->Cell(10,5,'SUCR',1,0,'C',1);
+	$pdf->Cell(10,5,'INDI',1,0,'C',1);
 	$pdf->Cell(100,5,utf8_decode('Fuente de Información'),1,0,'C',1);
 	$pdf->Cell(15,5,'evidencia',1,1,'C',1);
  
@@ -70,19 +71,25 @@
 
 		    $i=$i+1;
 		    $pdf->Cell(5,$h,$i,1,0,'R',0); 
-		    $pdf->Cell(12,$h,utf8_decode($row->codigo),1,0,'L',0);
+
+		    $pdf->Cell(10,$h,utf8_decode($row->idcalidadcarrera),1,0,'L',0);
 		 $current_x = $pdf->GetX();
 		 $current_y = $pdf->GetY();
 
-		    $pdf->Cell(12,$h,utf8_decode($row->inicialcriterio),1,0,'L',0);
+
+		    $pdf->Cell(10,$h,utf8_decode($row->codigo),1,0,'L',0);
 		 $current_x = $pdf->GetX();
 		 $current_y = $pdf->GetY();
 
-		    $pdf->Cell(12,$h,utf8_decode($row->inicialsubcriterio),1,0,'L',0);
+		    $pdf->Cell(10,$h,utf8_decode($row->inicialcriterio),1,0,'L',0);
 		 $current_x = $pdf->GetX();
 		 $current_y = $pdf->GetY();
 
-		    $pdf->Cell(12,$h,utf8_decode($row->inicialindicador),1,0,'L',0);
+		    $pdf->Cell(10,$h,utf8_decode($row->inicialsubcriterio),1,0,'L',0);
+		 $current_x = $pdf->GetX();
+		 $current_y = $pdf->GetY();
+
+		    $pdf->Cell(10,$h,utf8_decode($row->inicialindicador),1,0,'L',0);
 		 $current_x = $pdf->GetX();
 		 $current_y = $pdf->GetY();
 
