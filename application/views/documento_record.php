2625fe645fef4f6fcef64dd5a1cac1118670df73
Stalin francis <educaysoft@gmail.com>
2024-09-10 06:32:45 -0500

    mejoras

--- application/views/documento_record.php
+++ application/views/documento_record.php
@@ -93,7 +93,7 @@ if(isset($documento))
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> <?php echo anchor('tipodocumentodocumento/add/'.$documento['idtipodocumento'], 'Tipodocumento:'); ?>:</label>
+    <label class="col-md-2 col-form-label"> <?php echo anchor('tipodocumentodocumento/add/'.$documento['iddocumento'], 'Tipodocumento:'); ?>:</label>
 	<div class="col-md-10">
 	<?php
  	$options = array();
