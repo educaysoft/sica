e6b12594cd75fe8dfa0c6a47e487c61796f70d5a
Stalin francis <educaysoft@gmail.com>
2024-09-21 00:00:31 -0500

    mejoras

--- application/views/seguimientosilabo_record.php
+++ application/views/seguimientosilabo_record.php
@@ -52,12 +52,12 @@ if(isset($seguimientosilabo))
 
 
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Nombre del silabo:</label>
+    <label class="col-md-2 col-form-label"> Nombre del evento:</label>
 	<div class="col-md-10">
 	<?php
 $options= array("NADA");
-foreach ($silabos as $row){
-	$options[$row->idevento]= $row->nombre;
+foreach ($eventos as $row){
+	$options[$row->idevento]= $row->titulo;
 }
          echo form_input('idevento',$options[$seguimientosilabo['idevento']],array("disabled"=>"disabled",'style'=>'width:500px;'));
 		?>
@@ -65,87 +65,44 @@ foreach ($silabos as $row){
 </div> 
 
 
-
-
-
-
-
 <div class="form-group row">
-    <label class="col-md-2 col-form-label"> id Unidad:</label>
+    <label class="col-md-2 col-form-label"> Criterio seguimiento:</label>
 	<div class="col-md-10">
 	<?php
-      echo form_input('idseguimientosilabo',$seguimientosilabo['idseguimientosilabo'],array("id"=>"idseguimientosilabo","disabled"=>"disabled",'placeholder'=>'unidad','style'=>'width:500px;'));
-		?>
-	</div> 
-</div> 
-
-
-
-
-<div class="form-group row">
-    <label class="col-md-2 col-form-label"> No. de la unidad:</label>
-	<div class="col-md-10">
-	<?php
-      echo form_input('unidad',$seguimientosilabo['unidad'],array("disabled"=>"disabled",'placeholder'=>'unidad','style'=>'width:500px;'));
-		?>
-	</div> 
-</div>
-
-<div class="form-group row">
-    <label class="col-md-2 col-form-label"> Nombre Unidad:</label>
-	<div class="col-md-10">
-	<?php
-
-
-$textarea_options = array("disabled"=>"disabled",'class' => 'form-control','rows' => '4',   'cols' => '20', 'style'=> 'width:500px;height:100px;');    
- echo form_textarea('nombre',$seguimientosilabo['nombre'],$textarea_options); 
-
+$options= array("NADA");
+foreach ($criterioseguimientosilabo as $row){
+	$options[$row->idcriterioseguimientosilabo]= $row->nombre;
+}
+         echo form_input('idcriterioseguimientosilabo',$options[$seguimientosilabo['idcriterioseguimientosilabo']],array("disabled"=>"disabled",'style'=>'width:500px;'));
 		?>
 	</div> 
 </div>
 
 
 <div class="form-group row">
-
+    <label class="col-md-2 col-form-label"> Criterio seguimiento:</label>
 	<div class="col-md-10">
-	<div class="row justify-content-left">
-      	<!-- Page Heading -->
- 	<div class="row">
-  	<div class="col-12" style="border:solid;">
-
-<div class="row" style="background-color:lightgray; padding-top:0.5cm; padding-bottom:0.5cm; border-bottom:0.5cm solid white;">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <b>Temas de la Unidad: </b>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-success" href="<?php echo base_url('tema/add/'.$seguimientosilabo['idseguimientosilabo']); ?>">Nuevo tema</a><a class="btn btn-danger" onclick='reportepdf()' >Reporte</a>
-        </div>
-    </div>
-</div>
-
-
-	<table class="table table-striped table-bordered table-hover" id="mydatac">
-	 <thead>
-	 <tr>
-	 <th>idseguimientosilabo</th>
-	 <th>idtema</th>
-	 <th>unidad</th>
-	 <th>sesión</th>
-	 <th>tema(nombre corto)</th>
-	 <th>duracion</th>
-	 <th>idvideoturorial</th>
-	 <th style="text-align: right;">Actions</th>
-	 </tr>
-	 </thead>
-	 <tbody id="show_data">
-	 </tbody>
-	</table>
-	</div>
-	</div>
-	</div>
+	<?php
+$options= array("NADA");
+foreach ($valorcriteriioseguimientosilabo as $row){
+	$options[$row->idvalorcriteriioseguimientosilabo]= $row->nombre;
+}
+         echo form_input('idvalorcriteriioseguimientosilabo',$options[$seguimientosilabo['idvalorcriteriioseguimientosilabo']],array("disabled"=>"disabled",'style'=>'width:500px;'));
+		?>
 	</div> 
 </div>
+
+ 
+
+
+
+
+
+
+
+
+
+
 
  
  
